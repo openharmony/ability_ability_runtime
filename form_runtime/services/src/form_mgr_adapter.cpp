5967e4a3046bfb5440c18f56c195dfc6b1fd1c63
yangzk <yangzhongkai@huawei.com>
2022-06-07 21:28:34 +0800

    IssueNo: #I5B700 Description: FMS no longer caches formBindingData with images Sig: SIG_ApplicationFramework Feature or Bugfix: Bugfix Binary Source: No
    Signed-off-by: yangzk <yangzhongkai@huawei.com>
    Change-Id: I2fcd5a330ccebf0319c52d90066df35afa7de00e

--- form_runtime/services/src/form_mgr_adapter.cpp
+++ form_runtime/services/src/form_mgr_adapter.cpp
@@ -859,10 +859,8 @@ ErrCode FormMgrAdapter::AddExistFormRecord(const FormItemInfo &info, const sptr<
 
     // create form info for js
     std::string cacheData;
-    std::map<std::string, std::pair<sptr<FormAshmem>, int32_t>> imageMap;
-    if (FormCacheMgr::GetInstance().GetData(formId, cacheData, imageMap)) {
+    if (FormCacheMgr::GetInstance().GetData(formId, cacheData)) {
         formInfo.formData = cacheData;
-        formInfo.formProviderData.SetImageDataMap(imageMap);
     }
     FormDataMgr::GetInstance().CreateFormInfo(formId, record, formInfo);
 
@@ -1595,9 +1593,9 @@ ErrCode FormMgrAdapter::AddRequestPublishForm(const int64_t formId, const Want &
     if (formProviderData != nullptr) {
         formJsInfo.formData = formProviderData->GetDataString();
         formJsInfo.formProviderData = *formProviderData;
-        if (formJsInfo.formData.size() <= Constants::MAX_FORM_DATA_SIZE) {
-            HILOG_INFO("%{public}s, updateJsForm, data is less than 1k, cache data.", __func__);
-            FormCacheMgr::GetInstance().AddData(formId, formJsInfo.formData, formProviderData->GetImageDataMap());
+        if (formProviderData->NeedCache()) {
+            HILOG_INFO("%{public}s, data is less than 1k, cache data.", __func__);
+            FormCacheMgr::GetInstance().AddData(formId, formJsInfo.formData);
         }
     }
     // storage info
@@ -2032,11 +2030,9 @@ bool FormMgrAdapter::UpdateProviderInfoToHost(const int64_t matchedFormId, const
     // If the form need refrsh flag is true and form visibleType is FORM_VISIBLE, refresh the form host.
     if (formRecord.needRefresh && formVisibleType == Constants::FORM_VISIBLE) {
         std::string cacheData;
-        std::map<std::string, std::pair<sptr<FormAshmem>, int32_t>> imageMap;
         // If the form has business cache, refresh the form host.
-        if (FormCacheMgr::GetInstance().GetData(matchedFormId, cacheData, imageMap)) {
+        if (FormCacheMgr::GetInstance().GetData(matchedFormId, cacheData)) {
             formRecord.formProviderInfo.SetFormDataString(cacheData);
-            formRecord.formProviderInfo.SetImageDataMap(imageMap);
             formHostRecord.OnUpdate(matchedFormId, formRecord);
         }
     }
