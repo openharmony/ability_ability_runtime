c99a92384f174f908a90264c6fe0554b456b92e4
xuchenghua09 <xuchenghua09@huawei.com>
2022-06-30 15:30:30 +0800

    fix
    Signed-off-by: xuchenghua09 <xuchenghua09@huawei.com>

--- services/abilitymgr/src/implicit_start_processor.cpp
+++ services/abilitymgr/src/implicit_start_processor.cpp
@@ -106,7 +106,7 @@ int ImplicitStartProcessor::GenerateAbilityRequestByAction(int32_t userId,
         request.want, abilityInfoFlag, userId, abilityInfos, extensionInfos));
 
     HILOG_INFO("ImplicitQueryInfos, abilityInfo size : %{public}d, extensionInfos size: %{public}d",
-        static_cast<int>(abilityInfos.size()), static_cast<int>(extensionInfos.size());
+        static_cast<int>(abilityInfos.size()), static_cast<int>(extensionInfos.size()));
 
     auto isExtension = (request.callType == AbilityCallType::START_EXTENSION_TYPE ||
         request.callType == AbilityCallType::CONNECT_ABILITY_TYPE);
