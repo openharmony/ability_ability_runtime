98359010c258769b65cfb65c4b4c95a0df962830
fangJinliang1 <fangjinliang1@huawei.com>
2022-06-30 19:58:23 +0800

    wantAgent modify
    Signed-off-by: fangJinliang1 <fangjinliang1@huawei.com>
    Change-Id: I10cdae8df7653afbab4437a993bfd72bd8e3a472

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -1611,8 +1611,19 @@ sptr<IWantSender> AbilityManagerService::GetWantSender(
 
     int32_t callerUid = IPCSkeleton::GetCallingUid();
     int userId = wantSenderInfo.userId;
+    bool query = true;
+    if (wantSenderInfo.allWants.size() > 0) {
+        std::string deviceId = wantSenderInfo.allWants[0].want.GetDeviceId();
+        std::string localDeviceId;
+        if (GetLocalDeviceId(localDeviceId) &&
+            (!deviceId.empty() && localDeviceId != deviceId)) {
+            query = false;
+        }
+        HILOG_INFO("query = %{public}d, localDeviceId = %{public}s, deviceId = %{public}s", query, localDeviceId.c_str(), deviceId.c_str());
+    }
+
     AppExecFwk::BundleInfo bundleInfo;
-    if (!wantSenderInfo.bundleName.empty()) {
+    if (!wantSenderInfo.bundleName.empty() && query) {
         bool bundleMgrResult = false;
         if (wantSenderInfo.userId < 0) {
 #ifdef OS_ACCOUNT_PART_ENABLED
