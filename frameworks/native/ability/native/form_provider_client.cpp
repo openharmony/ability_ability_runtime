d0b1dcb85bb4789168d09eda0a9778503b2772f7
xuchenghua09 <xuchenghua09@huawei.com>
2022-08-04 18:11:47 +0800

    modify code
    Signed-off-by: xuchenghua09 <xuchenghua09@huawei.com>

--- frameworks/native/ability/native/form_provider_client.cpp
+++ frameworks/native/ability/native/form_provider_client.cpp
@@ -515,15 +515,7 @@ int FormProviderClient::HandleAcquireStateResult(FormState state, const std::str
     return ERR_OK;
 }
 
-/**
- * @brief Acquire to share form information data. This is sync API.
- * @param formId The Id of the from.
- * @param remoteDeviceId Indicates the device ID to share.
- * @param formSupplyCallback Indicates lifecycle callbacks.
- * @param requestCode Indicates the request code of this share form.
- * @return Returns ERR_OK on success, others on failure.
- */
-int32_t FormProviderClient::ShareAcquireProviderFormInfo(int64_t formId, const std::string &remoteDeviceId,
+int32_t FormProviderClient::AcquireShareFormData(int64_t formId, const std::string &remoteDeviceId,
     const sptr<IRemoteObject> &formSupplyCallback, int64_t requestCode)
 {
     HILOG_DEBUG("FormProviderClient::%{public}s called.", __func__);
