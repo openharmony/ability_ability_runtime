9ab774e56970ebc52542988ecd4377987609c4ba
unknown <sijunjie@huawei.com>
2022-08-08 09:47:42 +0800

    add user
    Signed-off-by: unknown <sijunjie@huawei.com>

--- frameworks/native/appkit/app/watchdog.cpp
+++ frameworks/native/appkit/app/watchdog.cpp
@@ -52,7 +52,7 @@ void WatchDog::ProcessEvent(const OHOS::AppExecFwk::InnerEvent::Pointer &event)
         if (currentHandler_ != nullptr) {
             currentHandler_->RemoveTask(MAIN_THREAD_TIMEOUT_TASK);
         }
-        timeOut_.store(false);
+        needReport_.store(true);
         isSixSecondEvent_.store(false);
     }
 }
@@ -142,31 +142,7 @@ bool WatchDog::Timer()
             HILOG_DEBUG("cvWatchDog2 is stopped");
             return true;
         }
-        auto timeoutTask = [&]() {
-            if (timeOut_) {
-                return;
-            }
-
-            if (isSixSecondEvent_) {
-                timeOut_.store(true);
-            }
-            appMainThreadIsAlive_ = false;
-            std::string eventType = isSixSecondEvent_ ? "THREAD_BLOCK_6S" : "THREAD_BLOCK_3S";
-            std::string msgContent = "App main thread is not response!";
-            MainHandlerDumper handlerDumper;
-            appMainHandler_->Dump(handlerDumper);
-            msgContent += handlerDumper.GetDumpInfo();
-            isSixSecondEvent_.store(true);
-            if (applicationInfo_ != nullptr) {
-                OHOS::HiviewDFX::HiSysEvent::Write(OHOS::HiviewDFX::HiSysEvent::Domain::AAFWK, eventType,
-                    OHOS::HiviewDFX::HiSysEvent::EventType::FAULT, EVENT_KEY_UID, applicationInfo_->uid,
-                    EVENT_KEY_PID, static_cast<int32_t>(getpid()), EVENT_KEY_PACKAGE_NAME, applicationInfo_->bundleName,
-                    EVENT_KEY_PROCESS_NAME, applicationInfo_->process,
-                    EVENT_KEY_MESSAGE, msgContent);
-            }
-            HILOG_INFO("%{public}zu %{public}s", msgContent.size(), msgContent.c_str());
-        };
-        if (timeOut_) {
+        if (!needReport_) {
             HILOG_ERROR("Watchdog timeout, wait for the handler to recover, and do not send event.");
             continue;
         }
@@ -177,7 +153,8 @@ bool WatchDog::Timer()
             const char *hook_mode = "startup:";
             int ret = GetParameter("libc.hook_mode", "", paramOutBuf, bufferLen);
             if (ret <= 0 || strncmp(paramOutBuf, hook_mode, strlen(hook_mode)) != 0) {
-                currentHandler_->PostTask(timeoutTask, MAIN_THREAD_TIMEOUT_TASK, CHECK_INTERVAL_TIME);
+                currentHandler_->PostTask(std::bind(&WatchDog::reportEvent),
+                    MAIN_THREAD_TIMEOUT_TASK, CHECK_INTERVAL_TIME);
             }
         }
         if (appMainHandler_ != nullptr) {
@@ -185,6 +162,38 @@ bool WatchDog::Timer()
         }
     }
     return true;
+}
+
+void WatchDog::reportEvent()
+{
+    if (applicationInfo_ == nullptr) {
+        HILOG_ERROR("reportEvent fail, applicationInfo_ is nullptr.");
+        return;
+    }
+    
+    if (!needReport_) {
+        return;
+    }
+
+    appMainThreadIsAlive_ = false;
+    std::string eventType;
+    if (isSixSecondEvent_) {
+        eventType = "THREAD_BLOCK_6S";
+        needReport_.store(false);
+    } else {
+        eventType = "THREAD_BLOCK_3S";
+        isSixSecondEvent_.store(true);
+    }
+    std::string msgContent = "App main thread is not response!";
+    MainHandlerDumper handlerDumper;
+    appMainHandler_->Dump(handlerDumper);
+    msgContent += handlerDumper.GetDumpInfo();
+
+    OHOS::HiviewDFX::HiSysEvent::Write(OHOS::HiviewDFX::HiSysEvent::Domain::AAFWK, eventType,
+        OHOS::HiviewDFX::HiSysEvent::EventType::FAULT, EVENT_KEY_UID, applicationInfo_->uid,
+        EVENT_KEY_PID, static_cast<int32_t>(getpid()), EVENT_KEY_PACKAGE_NAME, applicationInfo_->bundleName,
+        EVENT_KEY_PROCESS_NAME, applicationInfo_->process, EVENT_KEY_MESSAGE, msgContent);
+    HILOG_INFO("reportEvent success, %{public}zu %{public}s", msgContent.size(), msgContent.c_str());
 }
 
 void MainHandlerDumper::Dump(const std::string &message)
