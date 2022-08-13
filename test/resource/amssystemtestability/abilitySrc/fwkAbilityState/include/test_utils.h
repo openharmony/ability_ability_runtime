db5e76a83cf4ed43ccc445d9f90ee7399a5751f7
m00512953 <mingxihua@huawei.com>
2022-08-13 15:20:31 +0800

    mingxihua@huawei.com.cn
    Signed-off-by: m00512953 <mingxihua@huawei.com>

--- test/resource/amssystemtestability/abilitySrc/fwkAbilityState/include/test_utils.h
+++ test/resource/amssystemtestability/abilitySrc/fwkAbilityState/include/test_utils.h
@@ -12,8 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef RESOURCE_OHOS_ABILITY_RUNTIME_TEST_UTILS_H_
-#define RESOURCE_OHOS_ABILITY_RUNTIME_TEST_UTILS_H_
+#ifndef RESOURCE_OHOS_ABILITY_RUNTIME_TEST_UTILS_H
+#define RESOURCE_OHOS_ABILITY_RUNTIME_TEST_UTILS_H
 #include "want.h"
 
 namespace OHOS {
@@ -27,4 +27,4 @@ public:
 };
 }  // namespace AppExecFwk
 }  // namespace OHOS
-#endif  // RESOURCE_OHOS_ABILITY_RUNTIME_TEST_UTILS_H_
+#endif  // RESOURCE_OHOS_ABILITY_RUNTIME_TEST_UTILS_H
