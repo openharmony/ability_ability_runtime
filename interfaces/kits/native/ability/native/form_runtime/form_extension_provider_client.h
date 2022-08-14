9dd13c38727131c66968d85262cbd687b00ba9a5
liweifeng <liweifeng2@huawei.com>
2022-08-14 15:15:30 +0800

    fms创建form js info，传递给卡片提供方，提供方缓存form js info用于数据更新
    Signed-off-by: liweifeng <liweifeng2@huawei.com>
    Change-Id: If466fc70a1a15d26910ae3388e5b9fb649bd3202

--- interfaces/kits/native/ability/native/form_runtime/form_extension_provider_client.h
+++ interfaces/kits/native/ability/native/form_runtime/form_extension_provider_client.h
@@ -32,12 +32,12 @@ public:
 
     /**
      * @brief Acquire to give back an ProviderFormInfo. This is sync API.
-     * @param formId The Id of the form.
+     * @param formJsInfo The form js info.
      * @param want Indicates the {@link Want} structure containing form info.
      * @param callerToken Caller form extension token.
      * @return Returns ERR_OK on success, others on failure.
      */
-    virtual int AcquireProviderFormInfo(const int64_t formId, const Want &want,
+    virtual int AcquireProviderFormInfo(const AppExecFwk::FormJsInfo &formJsInfo, const Want &want,
         const sptr<IRemoteObject> &callerToken) override;
 
     /**
@@ -149,7 +149,7 @@ private:
     std::shared_ptr<FormExtension> GetOwner();
     std::pair<int, int> CheckParam(const Want &want, const sptr<IRemoteObject> &callerToken);
     int HandleResultCode(int errorCode, const Want &want, const sptr<IRemoteObject> &callerToken);
-    void AcquireFormExtensionProviderInfo(const int64_t formId, const Want &want,
+    void AcquireFormExtensionProviderInfo(const AppExecFwk::FormJsInfo &formJsInfo, const Want &want,
         const sptr<IRemoteObject> &callerToken);
     void NotifyFormExtensionDelete(const int64_t formId, const Want &want,
         const sptr<IRemoteObject> &callerToken);
