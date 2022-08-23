8280779cd7d9192d98ce05bca6d0db5b37c2ae52
dy_study <dingyao5@huawei.com>
2022-08-23 15:43:57 +0800

    IssueNo:#I5NPDC Description:   多次点击跨设备免安装按钮导致崩溃问题修复 Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source: No
    Signed-off-by: dy_study <dingyao5@huawei.com>
    Change-Id: I15750af60ea986122c4bafdff6c22d7b744c0a22

--- services/abilitymgr/include/free_install_manager.h
+++ services/abilitymgr/include/free_install_manager.h
@@ -117,6 +117,7 @@ private:
     std::vector<FreeInstallInfo> freeInstallList_;
     std::vector<FreeInstallInfo> dmsFreeInstallCbs_;
     std::map<std::string, std::time_t> timeStampMap_;
+    std::mutex distributedFreeInstallLock_;
 
     /**
      * Start remote free install.
