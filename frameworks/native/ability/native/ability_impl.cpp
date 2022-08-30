3ec49ae0ec534608a308ff04f9a40ce7cd4dae10
yuwenze <yuaqiang1@huawei.com>
2022-08-30 11:13:12 +0800

    Description:fix bug Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source:No
    Signed-off-by: yuwenze <yuaqiang1@huawei.com>
    Change-Id: I42dcf382b4ca6e4f704d0d9b6c2d05e18fc1efb7

--- frameworks/native/ability/native/ability_impl.cpp
+++ frameworks/native/ability/native/ability_impl.cpp
@@ -590,12 +590,6 @@ void AbilityImpl::WindowLifeCycleImpl::AfterUnfocused()
 void AbilityImpl::WindowLifeCycleImpl::ForegroundFailed()
 {
     HILOG_DEBUG("%{public}s begin.", __func__);
-    auto owner = owner_.lock();
-    if (owner && !owner->IsStageBasedModel()) {
-        return;
-    }
-
-    HILOG_DEBUG("The ability is stage mode, schedule foreground failed.");
     PacMap restoreData;
     AbilityManagerClient::GetInstance()->AbilityTransitionDone(token_,
         AbilityLifeCycleState::ABILITY_STATE_FOREGROUND_FAILED, restoreData);
