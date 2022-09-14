clipsheep6/ability_runtime

IssueNo:#I5PXW4
Signed-off-by: zhoushicheng <zhoushicheng2@huawei.com>
Change-Id: I182636ae694c1a17d13efcf3ad1129d06a3080fb

interfaces/inner_api/app_manager/src/appmgr/process_data.cpp
@@ -16,19 +16,27 @@
 #include "process_data.h"
 
 #include "hilog_wrapper.h"
+#include "parcel_macro_base.h"
 
 namespace OHOS {
 namespace AppExecFwk {
 bool ProcessData::Marshalling(Parcel &parcel) const
 {
-    return (parcel.WriteString(bundleName) && parcel.WriteInt32(pid) && parcel.WriteInt32(uid));
+    WRITE_PARCEL_AND_RETURN_FALSE_IF_FAIL(Int32, parcel, static_cast<int32_t>(state));
+    return (parcel.WriteString(bundleName) && parcel.WriteInt32(pid) && parcel.WriteInt32(uid) &&
+        parcel.WriteBool(isContinuounsTask) && parcel.WriteBool(isKeepAlive));
 }
 
 bool ProcessData::ReadFromParcel(Parcel &parcel)
 {
+    int32_t stateData;
+    READ_PARCEL_AND_RETURN_FALSE_IF_FAIL(Int32, parcel, stateData);
+    state = static_cast<AppProcessState>(stateData);
     bundleName = parcel.ReadString();
     pid = parcel.ReadInt32();
     uid = parcel.ReadInt32();
+    isContinuounsTask = parcel.ReadBool();
+    isKeepAlive = parcel.ReadBool();
 
     return true;
 }

interfaces/inner_api/app_manager/src/appmgr/running_process_info.cpp
@@ -41,6 +41,8 @@ bool RunningProcessInfo::ReadFromParcel(Parcel &parcel)
     int32_t stateData;
     READ_PARCEL_AND_RETURN_FALSE_IF_FAIL(Int32, parcel, stateData);
     state_ = static_cast<AppProcessState>(stateData);
+    isContinuounsTask = parcel.ReadBool();
+    isKeepAlive = parcel.ReadBool();
     if (!parcel.ReadStringVector(&bundleNames)) {
         HILOG_ERROR("read bundleNames failed.");
         return false;

services/appmgr/src/app_mgr_service_inner.cpp
@@ -155,6 +155,7 @@ void AppMgrServiceInner::LoadAbility(const sptr<IRemoteObject> &token, const spt
 
     auto appRecord =
         appRunningManager_->CheckAppRunningRecordIsExist(appInfo->name, processName, appInfo->uid, bundleInfo);
+    bool isContinuousTask = (want == nullptr) ? false : want->GetIntParam("isContinuousTask", false);
     if (!appRecord) {
         appRecord =
             CreateAppRunningRecord(token, preToken, appInfo, abilityInfo,
@@ -165,9 +166,11 @@ void AppMgrServiceInner::LoadAbility(const sptr<IRemoteObject> &token, const spt
         }
         uint32_t startFlags = (want == nullptr) ? 0 : BuildStartFlags(*want, *abilityInfo);
         int32_t bundleIndex = (want == nullptr) ? 0 : want->GetIntParam(DLP_PARAMS_INDEX, 0);
+        appRecord->SetContinuousTaskAppState(isContinuousTask);
         StartProcess(abilityInfo->applicationName, processName, startFlags, appRecord,
             appInfo->uid, appInfo->bundleName, bundleIndex);
     } else {
+        appRecord->SetContinuousTaskAppState(isContinuousTask);
         StartAbility(token, preToken, abilityInfo, appRecord, hapModuleInfo, want);
     }
     PerfProfile::GetInstance().SetAbilityLoadEndTime(GetTickCount());
@@ -366,6 +369,7 @@ void AppMgrServiceInner::ApplicationForegrounded(const int32_t recordId)
     if (appState == ApplicationState::APP_STATE_READY || appState == ApplicationState::APP_STATE_BACKGROUND) {
         appRecord->SetState(ApplicationState::APP_STATE_FOREGROUND);
         OnAppStateChanged(appRecord, ApplicationState::APP_STATE_FOREGROUND);
+        DelayedSingleton<AppStateObserverManager>::GetInstance()->OnProcessStateChanged(appRecord);
     } else {
         HILOG_WARN("app name(%{public}s), app state(%{public}d)!",
             appRecord->GetName().c_str(),
@@ -397,6 +401,7 @@ void AppMgrServiceInner::ApplicationBackgrounded(const int32_t recordId)
     if (appRecord->GetState() == ApplicationState::APP_STATE_FOREGROUND) {
         appRecord->SetState(ApplicationState::APP_STATE_BACKGROUND);
         OnAppStateChanged(appRecord, ApplicationState::APP_STATE_BACKGROUND);
+        DelayedSingleton<AppStateObserverManager>::GetInstance()->OnProcessStateChanged(appRecord);
     } else {
         HILOG_WARN("app name(%{public}s), app state(%{public}d)!",
             appRecord->GetName().c_str(),
@@ -749,6 +754,8 @@ void AppMgrServiceInner::GetRunningProcesses(const std::shared_ptr<AppRunningRec
     runningProcessInfo.pid_ = appRecord->GetPriorityObject()->GetPid();
     runningProcessInfo.uid_ = appRecord->GetUid();
     runningProcessInfo.state_ = static_cast<AppProcessState>(appRecord->GetState());
+    runningProcessInfo.isContinuounsTask = appRecord->IsContinuousTask();
+    runningProcessInfo.isKeepAlive = appRecord->IsKeepAliveApp();
     appRecord->GetBundleNames(runningProcessInfo.bundleNames);
     info.emplace_back(runningProcessInfo);
 }

services/appmgr/src/app_state_observer_manager.cpp
@@ -114,7 +114,7 @@ void AppStateObserverManager::OnProcessDied(const std::shared_ptr<AppRunningReco
             return;
         }
         HILOG_INFO("OnProcessDied come.");
-        self->HandleOnAppProcessDied(appRecord);
+    self->HandleOnAppProcessDied(appRecord);
     };
     handler_->PostTask(task);
 }
@@ -129,6 +129,20 @@ void AppStateObserverManager::OnRenderProcessDied(const std::shared_ptr<RenderRe
         }
         HILOG_INFO("OnRenderProcessDied come.");
         self->HandleOnRenderProcessDied(renderRecord);
+    };
+    handler_->PostTask(task);
+}
+
+void AppStateObserverManager::OnProcessStateChanged(const std::shared_ptr<AppRunningRecord> &appRecord)
+{
+    auto task = [weak = weak_from_this(), appRecord]() {
+        auto self = weak.lock();
+        if (self == nullptr) {
+            HILOG_ERROR("self is nullptr, OnProcessStateChanged failed.");
+            return;
+        }
+        HILOG_INFO("OnProcessStateChanged come.");
+        self->HandleOnAppProcessStateChanged(appRecord);
     };
     handler_->PostTask(task);
 }
@@ -264,6 +278,30 @@ void AppStateObserverManager::HandleOnProcessCreated(const ProcessData &data)
     }
 }
 
+void AppStateObserverManager::HandleOnAppProcessStateChanged(const std::shared_ptr<AppRunningRecord> &appRecord)
+{
+    if (!appRecord) {
+        HILOG_ERROR("app record is null");
+        return;
+    }
+    ProcessData data = WrapProcessData(appRecord);
+    HILOG_DEBUG("Process State Change, bundle:%{public}s, pid:%{public}d, uid:%{public}d, size:%{public}zu",
+        data.bundleName.c_str(), data.pid, data.uid, appStateObserverMap_.size());
+    HandleOnProcessStateChanged(data);
+}
+
+void AppStateObserverManager::HandleOnProcessStateChanged(const ProcessData &data)
+{
+    std::lock_guard<std::recursive_mutex> lockNotify(observerLock_);
+    for (auto it = appStateObserverMap_.begin(); it != appStateObserverMap_.end(); ++it) {
+        std::vector<std::string>::iterator iter = std::find(it->second.begin(),
+            it->second.end(), data.bundleName);
+        if ((it->second.empty() || iter != it->second.end()) && it->first != nullptr) {
+            it->first->OnProcessStateChanged(data);
+        }
+    }
+}
+
 void AppStateObserverManager::HandleOnAppProcessDied(const std::shared_ptr<AppRunningRecord> &appRecord)
 {
     if (!appRecord) {
@@ -306,6 +344,9 @@ ProcessData AppStateObserverManager::WrapProcessData(const std::shared_ptr<AppRu
     processData.bundleName = appRecord->GetBundleName();
     processData.pid = appRecord->GetPriorityObject()->GetPid();
     processData.uid = appRecord->GetUid();
+    processData.state = static_cast<AppProcessState>(appRecord->GetState());
+    processData.isContinuounsTask = appRecord->IsContinuousTask();
+    processData.isKeepAlive = appRecord->IsKeepAliveApp();
     return processData;
 }
 

openharmony/ability_ability_runtime

interfaces/inner_api/app_manager/src/appmgr/process_data.cpp
@@ -16,19 +16,27 @@
 #include "process_data.h"
 
 #include "hilog_wrapper.h"
+#include "parcel_macro_base.h"
 
 namespace OHOS {
 namespace AppExecFwk {
 bool ProcessData::Marshalling(Parcel &parcel) const
 {
-    return (parcel.WriteString(bundleName) && parcel.WriteInt32(pid) && parcel.WriteInt32(uid));
+    WRITE_PARCEL_AND_RETURN_FALSE_IF_FAIL(Int32, parcel, static_cast<int32_t>(state));
+    return (parcel.WriteString(bundleName) && parcel.WriteInt32(pid) && parcel.WriteInt32(uid) &&
+        parcel.WriteBool(isContinuousTask) && parcel.WriteBool(isKeepAlive));
 }
 
 bool ProcessData::ReadFromParcel(Parcel &parcel)
 {
+    int32_t stateData;
+    READ_PARCEL_AND_RETURN_FALSE_IF_FAIL(Int32, parcel, stateData);
+    state = static_cast<AppProcessState>(stateData);
     bundleName = parcel.ReadString();
     pid = parcel.ReadInt32();
     uid = parcel.ReadInt32();
+    isContinuousTask = parcel.ReadBool();
+    isKeepAlive = parcel.ReadBool();
 
     return true;
 }

interfaces/inner_api/app_manager/src/appmgr/running_process_info.cpp
@@ -41,6 +41,8 @@ bool RunningProcessInfo::ReadFromParcel(Parcel &parcel)
     int32_t stateData;
     READ_PARCEL_AND_RETURN_FALSE_IF_FAIL(Int32, parcel, stateData);
     state_ = static_cast<AppProcessState>(stateData);
+    isContinuousTask = parcel.ReadBool();
+    isKeepAlive = parcel.ReadBool();
     if (!parcel.ReadStringVector(&bundleNames)) {
         HILOG_ERROR("read bundleNames failed.");
         return false;

services/appmgr/src/app_mgr_service_inner.cpp
@@ -155,6 +155,7 @@ void AppMgrServiceInner::LoadAbility(const sptr<IRemoteObject> &token, const spt
 
     auto appRecord =
         appRunningManager_->CheckAppRunningRecordIsExist(appInfo->name, processName, appInfo->uid, bundleInfo);
+    bool isContinuousTask = (want == nullptr) ? false : want->GetIntParam("isContinuousTask", false);
     if (!appRecord) {
         appRecord =
             CreateAppRunningRecord(token, preToken, appInfo, abilityInfo,
@@ -165,9 +166,11 @@ void AppMgrServiceInner::LoadAbility(const sptr<IRemoteObject> &token, const spt
         }
         uint32_t startFlags = (want == nullptr) ? 0 : BuildStartFlags(*want, *abilityInfo);
         int32_t bundleIndex = (want == nullptr) ? 0 : want->GetIntParam(DLP_PARAMS_INDEX, 0);
+        appRecord->SetContinuousTaskAppState(isContinuousTask);
         StartProcess(abilityInfo->applicationName, processName, startFlags, appRecord,
             appInfo->uid, appInfo->bundleName, bundleIndex);
     } else {
+        appRecord->SetContinuousTaskAppState(isContinuousTask);
         StartAbility(token, preToken, abilityInfo, appRecord, hapModuleInfo, want);
     }
     PerfProfile::GetInstance().SetAbilityLoadEndTime(GetTickCount());
@@ -366,6 +369,7 @@ void AppMgrServiceInner::ApplicationForegrounded(const int32_t recordId)
     if (appState == ApplicationState::APP_STATE_READY || appState == ApplicationState::APP_STATE_BACKGROUND) {
         appRecord->SetState(ApplicationState::APP_STATE_FOREGROUND);
         OnAppStateChanged(appRecord, ApplicationState::APP_STATE_FOREGROUND);
+        DelayedSingleton<AppStateObserverManager>::GetInstance()->OnProcessStateChanged(appRecord);
     } else {
         HILOG_WARN("app name(%{public}s), app state(%{public}d)!",
             appRecord->GetName().c_str(),
@@ -397,6 +401,7 @@ void AppMgrServiceInner::ApplicationBackgrounded(const int32_t recordId)
     if (appRecord->GetState() == ApplicationState::APP_STATE_FOREGROUND) {
         appRecord->SetState(ApplicationState::APP_STATE_BACKGROUND);
         OnAppStateChanged(appRecord, ApplicationState::APP_STATE_BACKGROUND);
+        DelayedSingleton<AppStateObserverManager>::GetInstance()->OnProcessStateChanged(appRecord);
     } else {
         HILOG_WARN("app name(%{public}s), app state(%{public}d)!",
             appRecord->GetName().c_str(),
@@ -749,6 +754,8 @@ void AppMgrServiceInner::GetRunningProcesses(const std::shared_ptr<AppRunningRec
     runningProcessInfo.pid_ = appRecord->GetPriorityObject()->GetPid();
     runningProcessInfo.uid_ = appRecord->GetUid();
     runningProcessInfo.state_ = static_cast<AppProcessState>(appRecord->GetState());
+    runningProcessInfo.isContinuousTask = appRecord->IsContinuousTask();
+    runningProcessInfo.isKeepAlive = appRecord->IsKeepAliveApp();
     appRecord->GetBundleNames(runningProcessInfo.bundleNames);
     info.emplace_back(runningProcessInfo);
 }

services/appmgr/src/app_state_observer_manager.cpp
@@ -114,7 +114,7 @@ void AppStateObserverManager::OnProcessDied(const std::shared_ptr<AppRunningReco
             return;
         }
         HILOG_INFO("OnProcessDied come.");
-        self->HandleOnAppProcessDied(appRecord);
+    self->HandleOnAppProcessDied(appRecord);
     };
     handler_->PostTask(task);
 }
@@ -129,6 +129,20 @@ void AppStateObserverManager::OnRenderProcessDied(const std::shared_ptr<RenderRe
         }
         HILOG_INFO("OnRenderProcessDied come.");
         self->HandleOnRenderProcessDied(renderRecord);
+    };
+    handler_->PostTask(task);
+}
+
+void AppStateObserverManager::OnProcessStateChanged(const std::shared_ptr<AppRunningRecord> &appRecord)
+{
+    auto task = [weak = weak_from_this(), appRecord]() {
+        auto self = weak.lock();
+        if (self == nullptr) {
+            HILOG_ERROR("self is nullptr, OnProcessStateChanged failed.");
+            return;
+        }
+        HILOG_INFO("OnProcessStateChanged come.");
+        self->HandleOnAppProcessStateChanged(appRecord);
     };
     handler_->PostTask(task);
 }
@@ -264,6 +278,30 @@ void AppStateObserverManager::HandleOnProcessCreated(const ProcessData &data)
     }
 }
 
+void AppStateObserverManager::HandleOnAppProcessStateChanged(const std::shared_ptr<AppRunningRecord> &appRecord)
+{
+    if (!appRecord) {
+        HILOG_ERROR("app record is null");
+        return;
+    }
+    ProcessData data = WrapProcessData(appRecord);
+    HILOG_DEBUG("Process State Change, bundle:%{public}s, pid:%{public}d, uid:%{public}d, size:%{public}zu",
+        data.bundleName.c_str(), data.pid, data.uid, appStateObserverMap_.size());
+    HandleOnProcessStateChanged(data);
+}
+
+void AppStateObserverManager::HandleOnProcessStateChanged(const ProcessData &data)
+{
+    std::lock_guard<std::recursive_mutex> lockNotify(observerLock_);
+    for (auto it = appStateObserverMap_.begin(); it != appStateObserverMap_.end(); ++it) {
+        std::vector<std::string>::iterator iter = std::find(it->second.begin(),
+            it->second.end(), data.bundleName);
+        if ((it->second.empty() || iter != it->second.end()) && it->first != nullptr) {
+            it->first->OnProcessStateChanged(data);
+        }
+    }
+}
+
 void AppStateObserverManager::HandleOnAppProcessDied(const std::shared_ptr<AppRunningRecord> &appRecord)
 {
     if (!appRecord) {
@@ -306,6 +344,9 @@ ProcessData AppStateObserverManager::WrapProcessData(const std::shared_ptr<AppRu
     processData.bundleName = appRecord->GetBundleName();
     processData.pid = appRecord->GetPriorityObject()->GetPid();
     processData.uid = appRecord->GetUid();
+    processData.state = static_cast<AppProcessState>(appRecord->GetState());
+    processData.isContinuousTask = appRecord->IsContinuousTask();
+    processData.isKeepAlive = appRecord->IsKeepAliveApp();
     return processData;
 }
 