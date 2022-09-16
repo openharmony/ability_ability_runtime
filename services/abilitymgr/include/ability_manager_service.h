04061dd543ea1292590d2769d5a1bbc5d2a3432d
caochunlei <caochunlei1@huawei.com>
2022-09-16 16:23:46 +0800

    caochunlei1@huawei.com
    Signed-off-by: caochunlei <caochunlei1@huawei.com>

--- services/abilitymgr/include/ability_manager_service.h
+++ services/abilitymgr/include/ability_manager_service.h
@@ -1122,6 +1122,8 @@ private:
 
     bool CheckNewRuleSwitchState(const std::string &param);
 
+    void UpdateFocusState(std::vector<AbilityRunningInfo> &info);
+
     constexpr static int REPOLL_TIME_MICRO_SECONDS = 1000000;
     constexpr static int WAITING_BOOT_ANIMATION_TIMER = 5;
 
