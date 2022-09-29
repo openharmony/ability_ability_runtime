0da51b247421e545aec68cb382cfa92dfe1e08f5
liqiang <liqiang121@huawei.com>
2022-09-29 08:43:39 +0800

    add tdd testcase for quick_fix_stub
    Signed-off-by: liqiang <liqiang121@huawei.com>
    Change-Id: I76003edd403d8ea67f3310a6aa9347257992bb45

--- test/unittest/quick_fix/quick_fix_manager_stub_test/quick_fix_manager_stub_test.cpp
+++ test/unittest/quick_fix/quick_fix_manager_stub_test/quick_fix_manager_stub_test.cpp
@@ -16,6 +16,7 @@
 #include <gtest/gtest.h>
 
 #define private public
+#include "quick_fix_errno_def.h"
 #include "quick_fix_manager_stub.h"
 #include "mock_quick_fix_manager_stub.h"
 #undef private
@@ -23,6 +24,9 @@
 
 using namespace testing;
 using namespace testing::ext;
+using ::testing::Return;
+using ::testing::SetArgReferee;
+using ::testing::DoAll;
 
 namespace OHOS {
 namespace AAFwk {
@@ -104,6 +108,121 @@ HWTEST_F(QuickFixManagerStubTest, GetApplyedQuickFixInfo_0100, TestSize.Level1)
         IQuickFixManager::QuickFixMgrCmd::ON_GET_APPLYED_QUICK_FIX_INFO, data, reply, option);
     EXPECT_EQ(result, NO_ERROR);
 
+    HILOG_INFO("%{public}s end.", __func__);
+}
+
+/**
+ * @tc.name: OnRemoteRequest_0100
+ * @tc.desc: OnRemoteRequest
+ * @tc.type: FUNC
+ * @tc.require: issueI5ODCD
+ */
+HWTEST_F(QuickFixManagerStubTest, OnRemoteRequest_0100, TestSize.Level1)
+{
+    HILOG_INFO("%{public}s start.", __func__);
+
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+
+    data.WriteInterfaceToken(u"fake_interface_token");
+    std::string bundleName = "com.ohos.quickfix";
+    data.WriteString(bundleName);
+
+    auto result = mockQuickFixMgrService_->OnRemoteRequest(
+        IQuickFixManager::QuickFixMgrCmd::ON_GET_APPLYED_QUICK_FIX_INFO, data, reply, option);
+    EXPECT_EQ(result, QUICK_FIX_INVALID_PARAM);
+
+    HILOG_INFO("%{public}s end.", __func__);
+}
+
+/**
+ * @tc.name: OnRemoteRequest_0200
+ * @tc.desc: OnRemoteRequest
+ * @tc.type: FUNC
+ * @tc.require: issueI5ODCD
+ */
+HWTEST_F(QuickFixManagerStubTest, OnRemoteRequest_0200, TestSize.Level1)
+{
+    HILOG_INFO("%{public}s start.", __func__);
+
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+
+    data.WriteInterfaceToken(QuickFixManagerStub::GetDescriptor());
+    std::string bundleName = "com.ohos.quickfix";
+    data.WriteString(bundleName);
+
+    uint32_t invalidCode = 10;
+    auto result = mockQuickFixMgrService_->OnRemoteRequest(invalidCode, data, reply, option);
+    EXPECT_EQ(result, IPC_STUB_UNKNOW_TRANS_ERR);
+
+    HILOG_INFO("%{public}s end.", __func__);
+}
+
+/**
+ * @tc.name: OnRemoteRequest_0300
+ * @tc.desc: OnRemoteRequest
+ * @tc.type: FUNC
+ * @tc.require: issueI5ODCD
+ */
+HWTEST_F(QuickFixManagerStubTest, OnRemoteRequest_0300, TestSize.Level1)
+{
+    HILOG_INFO("%{public}s start.", __func__);
+
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+
+    data.WriteInterfaceToken(QuickFixManagerStub::GetDescriptor());
+    std::string bundleName = "com.ohos.quickfix";
+    data.WriteString(bundleName);
+
+    uint32_t invalidCode = 10;
+    mockQuickFixMgrService_->requestFuncMap_[IQuickFixManager::ON_GET_APPLYED_QUICK_FIX_INFO] = nullptr;
+    auto result = mockQuickFixMgrService_->OnRemoteRequest(invalidCode, data, reply, option);
+    EXPECT_EQ(result, IPC_STUB_UNKNOW_TRANS_ERR);
+    HILOG_INFO("%{public}s end.", __func__);
+}
+
+/**
+ * @tc.name: ApplyQuickFixInner_0100
+ * @tc.desc: ApplyQuickFixInner
+ * @tc.type: FUNC
+ * @tc.require: issueI5ODCD
+ */
+HWTEST_F(QuickFixManagerStubTest, ApplyQuickFixInner_0100, TestSize.Level1)
+{
+    HILOG_INFO("%{public}s start.", __func__);
+
+    MessageParcel data;
+    data.WriteInt32(-1);
+    MessageParcel reply;
+
+    auto result = mockQuickFixMgrService_->ApplyQuickFixInner(data, reply);
+    EXPECT_EQ(result, QUICK_FIX_READ_PARCEL_FAILED);
+    HILOG_INFO("%{public}s end.", __func__);
+}
+
+/**
+ * @tc.name: GetApplyedQuickFixInfoInner_0100
+ * @tc.desc: GetApplyedQuickFixInfoInner
+ * @tc.type: FUNC
+ * @tc.require: issueI5ODCD
+ */
+HWTEST_F(QuickFixManagerStubTest, GetApplyedQuickFixInfoInner_0100, TestSize.Level1)
+{
+    HILOG_INFO("%{public}s start.", __func__);
+    MessageParcel data;
+    MessageParcel reply;
+    reply.allocator_ = nullptr;
+    ApplicationQuickFixInfo expectQuickFixInfo;
+    expectQuickFixInfo.asRemote_ = true;
+    EXPECT_CALL(*mockQuickFixMgrService_, GetApplyedQuickFixInfo(_,_))
+        .WillOnce(DoAll(SetArgReferee<1>(expectQuickFixInfo), Return(QUICK_FIX_OK)));
+    auto result = mockQuickFixMgrService_->GetApplyedQuickFixInfoInner(data, reply);
+    EXPECT_EQ(result, QUICK_FIX_WRITE_PARCEL_FAILED);
     HILOG_INFO("%{public}s end.", __func__);
 }
 } // namespace AppExecFwk
