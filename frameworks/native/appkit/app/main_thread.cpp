f8ba0b226f4708fafca53c1653b720370807cda1
yuwenze <yuaqiang1@huawei.com>
2022-10-10 20:09:46 +0800

    Description:not call watchdog stop Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source:No
    Signed-off-by: yuwenze <yuaqiang1@huawei.com>
    Change-Id: Id818ccc6ba84364ed28ed80b23c1f5010236ad79

--- frameworks/native/appkit/app/main_thread.cpp
+++ frameworks/native/appkit/app/main_thread.cpp
@@ -1448,7 +1448,6 @@ void MainThread::HandleTerminateApplication()
         signalRunner->Stop();
     }
 
-    appMgr_->ApplicationTerminated(applicationImpl_->GetRecordId());
     std::shared_ptr<EventRunner> runner = mainHandler_->GetEventRunner();
     if (runner == nullptr) {
         HILOG_ERROR("MainThread::handleTerminateApplication get manHandler error");
@@ -1474,6 +1473,7 @@ void MainThread::HandleTerminateApplication()
         handleAppLib_ = nullptr;
     }
 #endif  // APPLICATION_LIBRARY_LOADER
+    appMgr_->ApplicationTerminated(applicationImpl_->GetRecordId());
     HILOG_DEBUG("MainThread::handleTerminateApplication called end.");
 }
 
