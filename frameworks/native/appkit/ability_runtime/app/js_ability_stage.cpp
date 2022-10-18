cd350608a31b1a5e52b9b6719ec3f9956d8b994a
caochunlei <caochunlei1@huawei.com>
2022-10-18 10:23:42 +0800

    caochunlei1@huawei.com
    Signed-off-by: caochunlei <caochunlei1@huawei.com>

--- frameworks/native/appkit/ability_runtime/app/js_ability_stage.cpp
+++ frameworks/native/appkit/ability_runtime/app/js_ability_stage.cpp
@@ -244,6 +244,7 @@ void JsAbilityStage::OnConfigurationUpdated(const AppExecFwk::Configuration& con
         reinterpret_cast<napi_env>(&nativeEngine), *fullConfig);
     NativeValue* jsConfiguration = reinterpret_cast<NativeValue*>(napiConfiguration);
     CallObjectMethod("onConfigurationUpdated", &jsConfiguration, 1);
+    CallObjectMethod("onConfigurationUpdate", &jsConfiguration, 1);
 }
 
 void JsAbilityStage::OnMemoryLevel(int32_t level)
