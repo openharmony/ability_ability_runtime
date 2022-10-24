c0bcc5aa15d04ad22c860b9c880ebd4a8d6cfd48
yuwenze <yuaqiang1@huawei.com>
2022-10-24 06:59:36 +0000

    Description:update snapshot after first frame drawing Sig:SIG_ApplicationFramework Feature or Bugfix:Feature Binary Source:No
    Signed-off-by: yuwenze <yuaqiang1@huawei.com>
    Change-Id: I21e570b3514e747d06bc305a57390fe22944dba1

--- services/abilitymgr/include/ability_record.h
+++ services/abilitymgr/include/ability_record.h
@@ -414,15 +414,6 @@ public:
      */
     bool IsReady() const;
 
-    inline void SetNeedSnapShot(bool needTakeSnapShot)
-    {
-        needTakeSnapShot_ = needTakeSnapShot;
-    }
-
-    inline bool IsNeedTakeSnapShot()
-    {
-        return needTakeSnapShot_;
-    }
 #ifdef SUPPORT_GRAPHICS
     /**
      * check whether the ability 's window is attached.
@@ -965,7 +956,6 @@ private:
     mutable std::condition_variable dumpCondition_;
     mutable bool isDumpTimeout_ = false;
     std::vector<std::string> dumpInfos_;
-    bool needTakeSnapShot_ = true;
     std::atomic<AbilityState> pendingState_ = AbilityState::INITIAL;    // pending life state
 
 #ifdef SUPPORT_GRAPHICS
