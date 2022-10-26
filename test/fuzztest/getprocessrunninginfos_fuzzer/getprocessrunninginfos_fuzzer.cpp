3e111b0bd5ece06b2281913295aa54118581dacd
zhoushicheng <zhoushicheng2@huawei.com>
2022-10-26 18:15:15 +0800

    Issue:#I5XW04
    Signed-off-by: zhoushicheng <zhoushicheng2@huawei.com>
    Change-Id: I409ee6f98588801389e7350fa9890db0090215af

---
+++ test/fuzztest/getprocessrunninginfos_fuzzer/getprocessrunninginfos_fuzzer.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getprocessrunninginfos_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#include "ability_manager_client.h"
+#include "running_process_info.h"
+#include "parcel.h"
+
+using namespace OHOS::AAFwk;
+using namespace OHOS::AppExecFwk;
+
+namespace OHOS {
+constexpr size_t FOO_MAX_LEN = 1024;
+bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
+{
+    std::vector<RunningProcessInfo> infos;
+    auto abilityMgr = AbilityManagerClient::GetInstance();
+    if (!abilityMgr) {
+        return false;
+    }
+
+    // fuzz for abilityRunningInfo
+    abilityMgr->GetProcessRunningInfos(infos);
+
+    return true;
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        std::cout << "invalid data" << std::endl;
+        return 0;
+    }
+
+    /* Validate the length of size */
+    if (size == 0 || size > OHOS::FOO_MAX_LEN) {
+        std::cout << "invalid size" << std::endl;
+        return 0;
+    }
+
+    char* ch = (char *)malloc(size + 1);
+    if (ch == nullptr) {
+        std::cout << "malloc failed." << std::endl;
+        return 0;
+    }
+
+    (void)memset_s(ch, size + 1, 0x00, size + 1);
+    if (memcpy_s(ch, size, data, size) != EOK) {
+        std::cout << "copy failed." << std::endl;
+        free(ch);
+        ch = nullptr;
+        return 0;
+    }
+
+    OHOS::DoSomethingInterestingWithMyAPI(ch, size);
+    free(ch);
+    ch = nullptr;
+    return 0;
+}
+
