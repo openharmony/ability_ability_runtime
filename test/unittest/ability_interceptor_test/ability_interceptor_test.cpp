2b70a126eb325749bee8138de1572203cbfead15
xuzheheng <xuzheheng1@huawei.com>
2022-10-26 15:27:27 +0800

    xuzheheng1@huawei.com
    Signed-off-by: xuzheheng <xuzheheng1@huawei.com>
    Change-Id: Ic88855972211a1c6e32a64ed7fef98dc0ff1490e

--- test/unittest/ability_interceptor_test/ability_interceptor_test.cpp
+++ test/unittest/ability_interceptor_test/ability_interceptor_test.cpp
@@ -176,7 +176,7 @@ HWTEST_F(AbilityInterceptorTest, ControlInterceptor_004, TestSize.Level1)
     int userId = 100;
     executer->AddInterceptor(std::make_shared<ControlInterceptor>());
     int result = executer->DoProcess(want, 0, userId, false);
-    EXPECT_EQ(result, ERR_OK);
+    EXPECT_EQ(result, ERR_DISPOSED_STATUS);
 }
 
 /**
