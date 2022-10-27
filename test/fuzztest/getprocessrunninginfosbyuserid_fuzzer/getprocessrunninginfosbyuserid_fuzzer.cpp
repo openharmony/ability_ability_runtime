bfb8652cc903866a13cd69409284832315874a01
xuzheheng <xuzheheng1@huawei.com>
2022-10-27 19:36:52 +0800

    xuzheheng1@huawei.com
    Signed-off-by: xuzheheng <xuzheheng1@huawei.com>
    Change-Id: I537974d96737fda368efbf9804c05ba235b93f3e

--- test/fuzztest/getprocessrunninginfosbyuserid_fuzzer/getprocessrunninginfosbyuserid_fuzzer.cpp
+++ test/fuzztest/getprocessrunninginfosbyuserid_fuzzer/getprocessrunninginfosbyuserid_fuzzer.cpp
@@ -13,44 +13,38 @@
  * limitations under the License.
  */
 
-#include "clearupapplicationdata_fuzzer.h"
+#include "getprocessrunninginfosbyuserid_fuzzer.h"
 
 #include <cstddef>
 #include <cstdint>
 
-#include "ability_manager_client.h"
+#include "ability_record.h"
 #include "app_mgr_client.h"
+#include "parcel.h"
 #include "securec.h"
 
 using namespace OHOS::AAFwk;
 using namespace OHOS::AppExecFwk;
 
 namespace OHOS {
-constexpr size_t FOO_MAX_LEN = 1024;
-bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
-{
-    auto abilitymgr = AbilityManagerClient::GetInstance();
-    if (!abilitymgr) {
-        return false;
-    }
+    constexpr size_t FOO_MAX_LEN = 1024;
 
-    std::string bundleName(data, size);
-    if (abilitymgr->ClearUpApplicationData(bundleName) != 0) {
-        return false;
-    }
+    bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
+    {
+        AppMgrClient* appMgrClient = new AppMgrClient();
+        if (!appMgrClient) {
+            return false;
+        }
 
-    // fuzz for AppMgrClient
-    auto appMgrClient = new AppMgrClient();
-    if (!appMgrClient) {
-        return false;
-    }
+        std::vector<RunningProcessInfo> infos;
+        int32_t userId = 100;
 
-    if (appMgrClient->ClearUpApplicationData(bundleName) != 0) {
-        return false;
-    }
+        if (appMgrClient->GetProcessRunningInfosByUserId(infos, userId) != 0) {
+            return false;
+        }
 
-    return true;
-}
+        return true;
+    }
 }
 
 /* Fuzzer entry point */
