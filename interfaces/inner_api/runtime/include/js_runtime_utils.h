4aedc4034101ef78ac1a3db40e71e0428a6a1391
xinxin13 <xinxin13@huawei.com>
2022-10-31 15:32:35 +0800

    napi_form_sub
    Signed-off-by: xinxin13 <xinxin13@huawei.com>

--- interfaces/inner_api/runtime/include/js_runtime_utils.h
+++ interfaces/inner_api/runtime/include/js_runtime_utils.h
@@ -163,6 +163,7 @@ public:
     void Reject(NativeEngine& engine, NativeValue* error);
     void ResolveWithNoError(NativeEngine& engine, NativeValue* value);
     void RejectWithNull(NativeEngine& engine, NativeValue* error);
+    void ResolveWithErrObject(NativeEngine& engine, NativeValue* errObject, NativeValue* value);
 
 private:
     static void Execute(NativeEngine* engine, void* data);
