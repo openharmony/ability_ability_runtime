6a52f485375dbc6cea7baa7e9fef0125a285b00d
zhoushicheng <zhoushicheng2@huawei.com>
2022-11-01 12:40:31 +0000

    fixed bb3f104 from https://gitee.com/zhou-shicheng2/ability_ability_runtime/pulls/3700 Issue:#I5YWZF
    Signed-off-by: zhoushicheng <zhoushicheng2@huawei.com>
    Change-Id: I174e7f96041102bd498cec8741bcfac9fefdb83a

--- test/fuzztest/scheduledisconnectabilitydone_fuzzer/scheduledisconnectabilitydone_fuzzer.cpp
+++ test/fuzztest/scheduledisconnectabilitydone_fuzzer/scheduledisconnectabilitydone_fuzzer.cpp
@@ -19,6 +19,7 @@
 #include <cstdint>
 
 #include "ability_manager_client.h"
+#include "ability_connect_manager.h"
 #include "ability_record.h"
 
 using namespace OHOS::AAFwk;
@@ -26,14 +27,17 @@ using namespace OHOS::AppExecFwk;
 
 namespace OHOS {
 constexpr size_t FOO_MAX_LEN = 1024;
-sptr<Token> GetFuzzAbilityToken()
+constexpr int32_t UID_TEST = 100;
+constexpr int OFFSET_ZERO = 24;
+sptr<Token> GetFuzzAbilityToken(AbilityType type)
 {
     sptr<Token> token = nullptr;
 
     AbilityRequest abilityRequest;
+    abilityRequest.uid = UID_TEST;
     abilityRequest.appInfo.bundleName = "com.example.fuzzTest";
     abilityRequest.abilityInfo.name = "MainAbility";
-    abilityRequest.abilityInfo.type = AbilityType::DATA;
+    abilityRequest.abilityInfo.type = type;
     std::shared_ptr<AbilityRecord> abilityRecord = AbilityRecord::CreateAbilityRecord(abilityRequest);
     if (abilityRecord) {
         token = abilityRecord->GetToken();
@@ -41,18 +45,36 @@ sptr<Token> GetFuzzAbilityToken()
 
     return token;
 }
+uint32_t GetU32Data(const char* ptr)
+{
+    // convert fuzz input data to an integer
+    return (ptr[0] << OFFSET_ZERO) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
+}
 bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
 {
     auto abilitymgr = AbilityManagerClient::GetInstance();
+    int userId = static_cast<int>(GetU32Data(data));
+    auto connectManager = new AbilityConnectManager(userId);
     if (!abilitymgr) {
         return false;
     }
 
     // get token
-    sptr<IRemoteObject> token = GetFuzzAbilityToken();
+    sptr<IRemoteObject> token = GetFuzzAbilityToken(AbilityType::PAGE);
     if (!token) {
         std::cout << "Get ability token failed." << std::endl;
         return false;
+    }
+
+    // get serviceToken
+    sptr<IRemoteObject> serviceToken = GetFuzzAbilityToken(AbilityType::SERVICE);
+    if (!serviceToken) {
+        std::cout << "Get service ability token failed." << std::endl;
+        return false;
+    }
+
+    if (connectManager) {
+        connectManager->ScheduleDisconnectAbilityDoneLocked(serviceToken);
     }
 
     if (abilitymgr->ScheduleDisconnectAbilityDone(token) != 0) {
