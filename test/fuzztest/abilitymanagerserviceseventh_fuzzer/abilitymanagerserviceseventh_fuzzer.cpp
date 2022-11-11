6557c897694e09525101baca4235091d0cb2a21c
xuzheheng <xuzheheng1@huawei.com>
2022-11-11 07:36:28 +0000

    xuzheheng1@huawei.com
    Signed-off-by: xuzheheng <xuzheheng1@huawei.com>
    Change-Id: I904bff12f47f0f4f76df2203101163b578601ad3

---
+++ test/fuzztest/abilitymanagerserviceseventh_fuzzer/abilitymanagerserviceseventh_fuzzer.cpp
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "abilitymanagerserviceseventh_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#define private public
+#define protected public
+#include "ability_manager_service.h"
+#undef protected
+#undef private
+
+#include "ability_record.h"
+
+using namespace OHOS::AAFwk;
+using namespace OHOS::AppExecFwk;
+
+namespace OHOS {
+namespace {
+constexpr size_t FOO_MAX_LEN = 1024;
+constexpr size_t U32_AT_SIZE = 4;
+class MyAbilityConnection : public IAbilityConnection {
+public:
+    MyAbilityConnection() = default;
+    virtual ~MyAbilityConnection() = default;
+    void OnAbilityConnectDone(
+        const AppExecFwk::ElementName &element, const sptr<IRemoteObject> &remoteObject, int resultCode) override
+    {}
+    void OnAbilityDisconnectDone(const AppExecFwk::ElementName &element, int resultCode) override
+    {}
+    sptr<IRemoteObject> AsObject() override
+    {
+        return {};
+    }
+};
+}
+
+uint32_t GetU32Data(const char* ptr)
+{
+    // convert fuzz input data to an integer
+    return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
+}
+
+sptr<Token> GetFuzzAbilityToken()
+{
+    sptr<Token> token = nullptr;
+    AbilityRequest abilityRequest;
+    abilityRequest.appInfo.bundleName = "com.example.fuzzTest";
+    abilityRequest.abilityInfo.name = "MainAbility";
+    abilityRequest.abilityInfo.type = AbilityType::DATA;
+    std::shared_ptr<AbilityRecord> abilityRecord = AbilityRecord::CreateAbilityRecord(abilityRequest);
+    if (abilityRecord) {
+        token = abilityRecord->GetToken();
+    }
+    return token;
+}
+
+bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
+{
+    int32_t int32Param = static_cast<int32_t>(GetU32Data(data));
+    Parcel wantParcel;
+    Want *want = nullptr;
+    if (wantParcel.WriteBuffer(data, size)) {
+        want = Want::Unmarshalling(wantParcel);
+        if (!want) {
+            return false;
+        }
+    }
+    sptr<IRemoteObject> token = GetFuzzAbilityToken();
+    sptr<IAbilityConnection> connect = new MyAbilityConnection();
+
+    // fuzz for AbilityManagerService
+    auto abilityms = std::make_shared<AbilityManagerService>();
+    abilityms->GetDataAbilityManagerByToken(token);
+    abilityms->ConnectBmsService();
+    sptr<IWantSender> target;
+    std::shared_ptr<WantSenderInfo> wantSenderInfo;
+    abilityms->GetWantSenderInfo(target, wantSenderInfo);
+    abilityms->GetAppMemorySize();
+    abilityms->IsRamConstrainedDevice();
+    abilityms->GetMissionSaveTime();
+    abilityms->GetMissionIdByAbilityToken(token);
+    abilityms->GetAbilityTokenByMissionId(int32Param);
+    abilityms->StartRemoteAbilityByCall(*want, token, token);
+    AppExecFwk::ElementName element;
+    abilityms->ReleaseRemoteAbility(token, element);
+    abilityms->StartAbilityByCall(*want, connect, token);
+    abilityms->ReleaseCall(connect, element);
+
+    return true;
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    /* Validate the length of size */
+    if (size < OHOS::U32_AT_SIZE || size > OHOS::FOO_MAX_LEN) {
+        return 0;
+    }
+
+    char* ch = (char *)malloc(size + 1);
+    if (ch == nullptr) {
+        std::cout << "malloc failed." << std::endl;
+        return 0;
+    }
+
+    (void)memset_s(ch, size + 1, 0x00, size + 1);
+    if (memcpy_s(ch, size, data, size) != EOK) {
+        std::cout << "copy failed." << std::endl;
+        free(ch);
+        ch = nullptr;
+        return 0;
+    }
+
+    OHOS::DoSomethingInterestingWithMyAPI(ch, size);
+    free(ch);
+    ch = nullptr;
+    return 0;
+}
+
