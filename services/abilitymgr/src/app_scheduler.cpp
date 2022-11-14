ded0f73bbbfe0b6aad1966e54e0a28164d89e18e
HuangXW <huangxinwei4@huawei.com>
2022-11-14 08:23:47 +0000

    通过accessToken查询应用状态
    Change-Id: I1c0fbee209f269e6cf82f97fcd48e4606a788bbb
    Signed-off-by: HuangXW <huangxinwei4@huawei.com>

--- services/abilitymgr/src/app_scheduler.cpp
+++ services/abilitymgr/src/app_scheduler.cpp
@@ -260,6 +260,13 @@ void AppScheduler::GetRunningProcessInfoByToken(const sptr<IRemoteObject> &token
     IN_PROCESS_CALL_WITHOUT_RET(appMgrClient_->GetRunningProcessInfoByToken(token, info));
 }
 
+void AppScheduler::GetRunningProcessInfoByAccessTokenID(
+    const uint32_t accessTokenId, AppExecFwk::RunningProcessInfo &info) const
+{
+    CHECK_POINTER(appMgrClient_);
+    IN_PROCESS_CALL_WITHOUT_RET(appMgrClient_->GetRunningProcessInfoByAccessTokenID(accessTokenId, info));
+}
+
 void AppScheduler::StartupResidentProcess(const std::vector<AppExecFwk::BundleInfo> &bundleInfos)
 {
     CHECK_POINTER(appMgrClient_);
