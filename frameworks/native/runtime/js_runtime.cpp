04ef2520faa297bb18fa8f3ce40fb1fd420a0cf4
lukai <lukai25@huawei.com>
2022-11-17 16:29:39 +0800

    rename interface
    issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I61N2M?from=project-issue
    
    Signed-off-by: lukai <lukai25@huawei.com>
    Change-Id: I8a00d86f20e754c43c493639eeb5f5b70e3300b5

--- frameworks/native/runtime/js_runtime.cpp
+++ frameworks/native/runtime/js_runtime.cpp
@@ -280,7 +280,7 @@ private:
 
     void FinishPreload() override
     {
-        panda::JSNApi::preFork(vm_);
+        panda::JSNApi::PreFork(vm_);
     }
 
     bool Initialize(const Runtime::Options& options) override
@@ -292,7 +292,7 @@ private:
                 std::string sandBoxAnFilePath = SANDBOX_ARK_CACHE_PATH + options.arkNativeFilePath;
                 postOption.SetAnDir(sandBoxAnFilePath);
             }
-            panda::JSNApi::postFork(vm_, postOption);
+            panda::JSNApi::PostFork(vm_, postOption);
             nativeEngine_->ReinitUVLoop();
             panda::JSNApi::SetLoop(vm_, nativeEngine_->GetUVLoop());
         } else {
