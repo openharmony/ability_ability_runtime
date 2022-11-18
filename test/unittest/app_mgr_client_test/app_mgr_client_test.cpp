1bca0fdb131e64611501c8df0790cb64992d0147
xinxin13 <xinxin13@huawei.com>
2022-11-18 09:39:50 +0800

    Adaptation interface parameter modification
    Signed-off-by: xinxin13 <xinxin13@huawei.com>

--- test/unittest/app_mgr_client_test/app_mgr_client_test.cpp
+++ test/unittest/app_mgr_client_test/app_mgr_client_test.cpp
@@ -154,6 +154,7 @@ HWTEST_F(AppMgrClientTest, AppMgrClient_GetApplicationInfoByProcessID_001, TestS
     std::string appName = "FirstApp";
     std::string bundleName = "com.ix.First.Test";
     AppExecFwk::ApplicationInfo application;
+    bool debug = false;
     auto abilityReq = GenerateAbilityRequest(deviceName, abilityName, appName, bundleName);
     auto record = AbilityRecord::CreateAbilityRecord(abilityReq);
     auto token = record->GetToken();
@@ -164,7 +165,7 @@ HWTEST_F(AppMgrClientTest, AppMgrClient_GetApplicationInfoByProcessID_001, TestS
     auto result = appMgrClient->ConnectAppMgrService();
     EXPECT_EQ(result, AppMgrResultCode::RESULT_OK);
 
-    appMgrClient->GetApplicationInfoByProcessID(ERROR_PID, application);
+    appMgrClient->GetApplicationInfoByProcessID(ERROR_PID, application, debug);
     EXPECT_EQ(application.bundleName, EMPTY_STRING);
 }
 
