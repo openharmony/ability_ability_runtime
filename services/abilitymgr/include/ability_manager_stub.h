f43f47886d63e1bd9f8b09c6ff0fa5d7e472b7ae
zhancaijin <zhancaijin1@huawei.com>
2022-11-25 19:21:20 +0800

    fixed e879c4f from https://gitee.com/zhancaijin/ability_ability_runtime/pulls/4008 modify for recovery invalid issue
    Signed-off-by: zhancaijin <zhancaijin1@huawei.com>

--- services/abilitymgr/include/ability_manager_stub.h
+++ services/abilitymgr/include/ability_manager_stub.h
@@ -185,10 +185,9 @@ private:
     int BlockAmsServiceInner(MessageParcel &data, MessageParcel &reply);
     int BlockAbilityInner(MessageParcel &data, MessageParcel &reply);
     int BlockAppServiceInner(MessageParcel &data, MessageParcel &reply);
+    #endif
     int EnableRecoverAbilityInner(MessageParcel &data, MessageParcel &reply);
     int ScheduleRecoverAbilityInner(MessageParcel &data, MessageParcel &reply);
-    #endif
-
     int GetTopAbilityInner(MessageParcel &data, MessageParcel &reply);
     int DumpAbilityInfoDoneInner(MessageParcel &data, MessageParcel &reply);
 };
