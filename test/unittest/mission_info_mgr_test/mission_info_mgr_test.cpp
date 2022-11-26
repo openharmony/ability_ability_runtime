d8a14a6c14d9cf513a5eefb88c8946de535b709f
xuzheheng <xuzheheng1@huawei.com>
2022-11-26 03:11:33 +0000

    xuzheheng1@huawei.com
    Signed-off-by: xuzheheng <xuzheheng1@huawei.com>
    Change-Id: I89f95dce30d15f287fdd2d2e1b50e02047347f26

--- test/unittest/mission_info_mgr_test/mission_info_mgr_test.cpp
+++ test/unittest/mission_info_mgr_test/mission_info_mgr_test.cpp
@@ -196,5 +196,257 @@ HWTEST_F(MissionInfoMgrTest, UpdateMissionInfo_003, TestSize.Level1)
     bool res = missionInfoMgr->UpdateMissionInfo(missionInfo);
     EXPECT_TRUE(res);
 }
+
+/*
+ * Feature: MissionInfoMgr
+ * Function: DeleteMissionInfo
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage DeleteMissionInfo
+ * EnvConditions: NA
+ * CaseDescription: Verify DeleteMissionInfo
+ */
+HWTEST_F(MissionInfoMgrTest, DeleteMissionInfo_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    int userId = 0;
+    missionInfoMgr->Init(userId);
+    missionInfoMgr->missionIdMap_[1] = true;
+    InnerMissionInfo missionInfo;
+    missionInfo.missionInfo.id = 1;
+    auto listIter = missionInfoMgr->missionInfoList_.begin();
+    missionInfoMgr->missionInfoList_.insert(listIter, missionInfo);
+    EXPECT_TRUE(missionInfoMgr->DeleteMissionInfo(-1));
+    EXPECT_TRUE(missionInfoMgr->DeleteMissionInfo(1));
+}
+
+/*
+ * Feature: MissionInfoMgr
+ * Function: DeleteAllMissionInfos
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage DeleteAllMissionInfos
+ * EnvConditions: NA
+ * CaseDescription: Verify DeleteAllMissionInfos
+ */
+HWTEST_F(MissionInfoMgrTest, DeleteAllMissionInfos_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    std::shared_ptr<MissionListenerController> listenerController;
+    EXPECT_FALSE(missionInfoMgr->DeleteAllMissionInfos(listenerController));
+}
+
+/*
+ * Feature: MissionInfoMgr
+ * Function: GetMissionInfos
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage GetMissionInfos
+ * EnvConditions: NA
+ * CaseDescription: Verify GetMissionInfos
+ */
+HWTEST_F(MissionInfoMgrTest, GetMissionInfos_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    int userId = 0;
+    missionInfoMgr->Init(userId);
+    missionInfoMgr->missionIdMap_[1] = true;
+    InnerMissionInfo missionInfo;
+    missionInfo.missionInfo.id = 1;
+    auto listIter = missionInfoMgr->missionInfoList_.begin();
+    missionInfoMgr->missionInfoList_.insert(listIter, missionInfo);
+
+    std::vector<MissionInfo> missionInfos;
+    auto ret1 = missionInfoMgr->GetMissionInfos(-1, missionInfos);
+    EXPECT_EQ(ret1, -1);
+
+    auto ret2 = missionInfoMgr->GetMissionInfos(1, missionInfos);
+    EXPECT_EQ(ret2, 0);
+}
+
+/*
+ * Feature: MissionInfoMgr
+ * Function: GetMissionInfoById
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage GetMissionInfoById
+ * EnvConditions: NA
+ * CaseDescription: Verify GetMissionInfoById
+ */
+HWTEST_F(MissionInfoMgrTest, GetMissionInfoById_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    int userId = 0;
+    missionInfoMgr->Init(userId);
+    missionInfoMgr->missionIdMap_[1] = true;
+    InnerMissionInfo missionInfo;
+    missionInfo.missionInfo.id = 1;
+    auto listIter = missionInfoMgr->missionInfoList_.begin();
+    missionInfoMgr->missionInfoList_.insert(listIter, missionInfo);
+
+    MissionInfo myMissionInfo;
+    auto ret1 = missionInfoMgr->GetMissionInfoById(-1, myMissionInfo);
+    EXPECT_EQ(ret1, -1);
+
+    auto ret2 = missionInfoMgr->GetMissionInfoById(1, myMissionInfo);
+    EXPECT_EQ(ret2, 0);
+}
+
+/*
+ * Feature: MissionInfoMgr
+ * Function: GetInnerMissionInfoById
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage GetInnerMissionInfoById
+ * EnvConditions: NA
+ * CaseDescription: Verify GetInnerMissionInfoById
+ */
+HWTEST_F(MissionInfoMgrTest, GetInnerMissionInfoById_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    int userId = 0;
+    missionInfoMgr->Init(userId);
+    missionInfoMgr->missionIdMap_[1] = true;
+    InnerMissionInfo missionInfo;
+    missionInfo.missionInfo.id = 1;
+    auto listIter = missionInfoMgr->missionInfoList_.begin();
+    missionInfoMgr->missionInfoList_.insert(listIter, missionInfo);
+
+    InnerMissionInfo innerMissionInfo;
+    auto ret2 = missionInfoMgr->GetInnerMissionInfoById(1, innerMissionInfo);
+    EXPECT_EQ(ret2, 0);
+}
+
+/*
+ * Feature: MissionInfoMgr
+ * Function: UpdateMissionLabel
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage UpdateMissionLabel
+ * EnvConditions: NA
+ * CaseDescription: Verify UpdateMissionLabel
+ */
+HWTEST_F(MissionInfoMgrTest, UpdateMissionLabel_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    int userId = 0;
+    missionInfoMgr->Init(userId);
+    missionInfoMgr->missionIdMap_[1] = true;
+    InnerMissionInfo missionInfo;
+    missionInfo.missionInfo.id = 1;
+    auto listIter = missionInfoMgr->missionInfoList_.begin();
+    missionInfoMgr->missionInfoList_.insert(listIter, missionInfo);
+
+    std::string label = "test";
+    auto ret1 = missionInfoMgr->UpdateMissionLabel(-1, label);
+    EXPECT_EQ(ret1, -1);
+
+    auto ret2 = missionInfoMgr->UpdateMissionLabel(1, label);
+    EXPECT_EQ(ret2, 0);
+}
+
+/*
+ * Feature: MissionInfoMgr
+ * Function: HandleUnInstallApp
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage HandleUnInstallApp
+ * EnvConditions: NA
+ * CaseDescription: Verify HandleUnInstallApp
+ */
+HWTEST_F(MissionInfoMgrTest, HandleUnInstallApp_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    int userId = 0;
+    missionInfoMgr->Init(userId);
+    missionInfoMgr->missionIdMap_[1] = true;
+    InnerMissionInfo missionInfo;
+    missionInfo.missionInfo.id = 1;
+    auto listIter = missionInfoMgr->missionInfoList_.begin();
+    missionInfoMgr->missionInfoList_.insert(listIter, missionInfo);
+    std::list<int32_t> missions;
+    missionInfoMgr->HandleUnInstallApp("", 1, missions);
+}
+
+/*
+ * Feature: MissionInfoMgr
+ * Function: Dump
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage Dump
+ * EnvConditions: NA
+ * CaseDescription: Verify Dump
+ */
+HWTEST_F(MissionInfoMgrTest, Dump_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    int userId = 0;
+    missionInfoMgr->Init(userId);
+    missionInfoMgr->missionIdMap_[1] = true;
+    InnerMissionInfo missionInfo;
+    missionInfo.missionInfo.id = 1;
+    auto listIter = missionInfoMgr->missionInfoList_.begin();
+    missionInfoMgr->missionInfoList_.insert(listIter, missionInfo);
+    std::vector<std::string> info;
+    missionInfoMgr->Dump(info);
+}
+
+/*
+ * Feature: MissionInfoMgr
+ * Function: RegisterSnapshotHandler
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage RegisterSnapshotHandler
+ * EnvConditions: NA
+ * CaseDescription: Verify RegisterSnapshotHandler
+ */
+HWTEST_F(MissionInfoMgrTest, RegisterSnapshotHandler_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    int userId = 0;
+    missionInfoMgr->Init(userId);
+    missionInfoMgr->missionIdMap_[1] = true;
+    InnerMissionInfo missionInfo;
+    missionInfo.missionInfo.id = 1;
+    auto listIter = missionInfoMgr->missionInfoList_.begin();
+    missionInfoMgr->missionInfoList_.insert(listIter, missionInfo);
+
+    sptr<ISnapshotHandler> handler = nullptr;
+    missionInfoMgr->RegisterSnapshotHandler(handler);
+    EXPECT_EQ(missionInfoMgr->snapshotHandler_, nullptr);
+}
+
+/*
+ * Feature: MissionInfoMgr
+ * Function: GetMissionSnapshot
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage GetMissionSnapshot
+ * EnvConditions: NA
+ * CaseDescription: Verify GetMissionSnapshot
+ */
+HWTEST_F(MissionInfoMgrTest, GetMissionSnapshot_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    sptr<IRemoteObject> abilityToken = nullptr;
+    MissionSnapshot missionSnapshot;
+    auto ret = missionInfoMgr->GetMissionSnapshot(1, abilityToken, missionSnapshot, true, true);
+    EXPECT_FALSE(ret);
+}
+
+#ifdef SUPPORT_GRAPHICS
+/*
+ * Feature: MissionInfoMgr
+ * Function: CreateWhitePixelMap
+ * SubFunction: NA
+ * FunctionPoints: MissionDataStorage CreateWhitePixelMap
+ * EnvConditions: NA
+ * CaseDescription: Verify CreateWhitePixelMap
+ */
+HWTEST_F(MissionInfoMgrTest, CreateWhitePixelMap_001, TestSize.Level1)
+{
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    int userId = 0;
+    missionInfoMgr->Init(userId);
+    missionInfoMgr->missionIdMap_[1] = true;
+    InnerMissionInfo missionInfo;
+    missionInfo.missionInfo.id = 1;
+    auto listIter = missionInfoMgr->missionInfoList_.begin();
+    missionInfoMgr->missionInfoList_.insert(listIter, missionInfo);
+
+    Snapshot snapshot;
+    missionInfoMgr->CreateWhitePixelMap(snapshot);
+}
+#endif
 }  // namespace AAFwk
 }  // namespace OHOS
