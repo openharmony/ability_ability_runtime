8a804fd2fc86a4526b093744a3a8fd6ddb541172
huangshiwei <huangshiwei4@huawei.com>
2022-11-26 10:55:10 +0800

    huangshiwei4@huawei.com
    Signed-off-by: huangshiwei <huangshiwei4@huawei.com>

--- frameworks/js/napi/featureAbility/napi_context.cpp
+++ frameworks/js/napi/featureAbility/napi_context.cpp
@@ -788,7 +788,6 @@ void CallOnRequestPermissionsFromUserResult(int requestCode, const std::vector<s
 
     uv_loop_t *loop = nullptr;
     loop = callbackInfo.engine->GetUVLoop();
-
     if (loop == nullptr) {
         HILOG_ERROR("CallOnRequestPermissionsFromUserResult loop is nullptr.");
         return;
