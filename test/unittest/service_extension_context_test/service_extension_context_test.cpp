0308f923912fce4d6802bac9b7340be4ccc09193
yuyaozhi <yuyaozhi@huawei.com>
2022-12-07 20:18:30 +0800

    add tdd of runtime
    Signed-off-by: yuyaozhi <yuyaozhi@huawei.com>
    Change-Id: I07527e38707d9deffae64307d0e502e8b549641b

---
+++ test/unittest/service_extension_context_test/service_extension_context_test.cpp
@@ -0,0 +1,259 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#define private public
+#include "service_extension_context.h"
+#undef private
+
+#include "ability_connection.h"
+#include "ability_manager_client.h"
+
+using namespace testing::ext;
+using namespace OHOS::AppExecFwk;
+
+namespace OHOS {
+namespace AbilityRuntime {
+//const size_t ServiceExtensionContext::CONTEXT_TYPE_ID(std::hash<const char*> {} ("ServiceExtensionContext"));
+//int ServiceExtensionContext::ILLEGAL_REQUEST_CODE(-1);
+
+// class MockServiceExtensionContext : public ExtensionContext {
+// public:
+//     MockServiceExtensionContext() = default;
+//     virtual ~MockServiceExtensionContext() = default;
+
+//     ErrCode StartAbility(const AAFwk::Want &want) const;
+// };
+
+class ServiceExtensionContextTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp();
+    void TearDown();
+};
+
+void ServiceExtensionContextTest::SetUpTestCase(void)
+{}
+void ServiceExtensionContextTest::TearDownTestCase(void)
+{}
+void ServiceExtensionContextTest::SetUp(void)
+{}
+void ServiceExtensionContextTest::TearDown(void)
+{}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest startAbility
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_startAbility_001, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    Want want; 
+    ErrCode result = serviceExtensionContextTest.StartAbility(want);
+    EXPECT_EQ(ERR_IMPLICIT_START_ABILITY_FAIL, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest startAbility
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_startAbility_002, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    Want want;  
+    StartOptions startOptions;
+    ErrCode result = serviceExtensionContextTest.StartAbility(want, startOptions);
+    EXPECT_EQ(ERR_IMPLICIT_START_ABILITY_FAIL, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest startAbility
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_StartAbilityByCall_001, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    Want want;  
+    std::shared_ptr<CallerCallBack> callback = std::make_shared<CallerCallBack>();
+    ErrCode result = serviceExtensionContextTest.StartAbilityByCall(want, callback);
+    EXPECT_EQ(ERR_INVALID_VALUE, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest startAbility
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_ReleaseCall_001, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    std::shared_ptr<CallerCallBack> callback = std::make_shared<CallerCallBack>();
+    ErrCode result = serviceExtensionContextTest.ReleaseCall(callback);
+    EXPECT_EQ(ERR_INVALID_VALUE, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest startAbility
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_ConnectAbility_001, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    Want want;  
+    sptr<AbilityConnectCallback> connectCallback;
+    ErrCode result = serviceExtensionContextTest.ConnectAbility(want, connectCallback);
+    EXPECT_EQ(ERR_INVALID_VALUE, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest startAbility
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_StartAbilityWithAccount_001, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    Want want;  
+    int accountId = 1;
+    ErrCode result = serviceExtensionContextTest.StartAbilityWithAccount(want, accountId);
+    EXPECT_EQ(CHECK_PERMISSION_FAILED, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest startAbility
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_StartAbilityWithAccount_002, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    Want want;  
+    int accountId = 1;
+    StartOptions startOptions;
+    ErrCode result = serviceExtensionContextTest.StartAbilityWithAccount(want, accountId, startOptions);
+    EXPECT_EQ(CHECK_PERMISSION_FAILED, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest StartServiceExtensionAbility
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_StartServiceExtensionAbility_001, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    Want want;  
+    int32_t accountId = 1;
+    StartOptions startOptions;
+    ErrCode result = serviceExtensionContextTest.StartServiceExtensionAbility(want, accountId);
+    EXPECT_EQ(CHECK_PERMISSION_FAILED, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest StopServiceExtensionAbility
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_StopServiceExtensionAbility_001, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    Want want;  
+    int32_t accountId = 1;
+    StartOptions startOptions;
+    ErrCode result = serviceExtensionContextTest.StopServiceExtensionAbility(want, accountId);
+    EXPECT_EQ(CHECK_PERMISSION_FAILED, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest ConnectAbilityWithAccount
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_ConnectAbilityWithAccount_001, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    Want want;  
+    int32_t accountId = 1;
+    sptr<AbilityConnectCallback> connectCallback;
+    ErrCode result = serviceExtensionContextTest.ConnectAbilityWithAccount(want, accountId, connectCallback);
+    EXPECT_EQ(ERR_INVALID_VALUE, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest TerminateAbility
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_TerminateAbility_001, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    ErrCode result = serviceExtensionContextTest.TerminateAbility();
+    EXPECT_EQ(ERR_INVALID_VALUE, result);
+}
+
+/*
+ * Feature: ServiceExtensionContext
+ * Function: startAbility
+ * SubFunction: NA
+ * FunctionPoints: ServiceExtensionContextTest GetAbilityInfoType
+ * EnvConditions: NA
+ * CaseDescription: Verify startAbility
+ */
+HWTEST_F(ServiceExtensionContextTest, service_extension_context_GetAbilityInfoType_001, TestSize.Level1)
+{   
+    ServiceExtensionContext serviceExtensionContextTest;
+    EXPECT_EQ(AppExecFwk::AbilityType::UNKNOWN, serviceExtensionContextTest.GetAbilityInfoType());
+}
+}
+}
