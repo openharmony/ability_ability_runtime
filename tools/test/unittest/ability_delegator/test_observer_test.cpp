d234f0a103beccb28cb1b168450b4e8dbd986813
xinxin13 <xinxin13@huawei.com>
2022-12-26 15:50:15 +0800

    add tdd
    Signed-off-by: xinxin13 <xinxin13@huawei.com>

--- tools/test/unittest/ability_delegator/test_observer_test.cpp
+++ tools/test/unittest/ability_delegator/test_observer_test.cpp
@@ -110,3 +110,30 @@ HWTEST_F(TestObserverTest, Test_Observer_Test_0300, Function | MediumTest | Leve
     th.join();
     EXPECT_TRUE(ret);
 }
+
+/**
+ * @tc.number: Test_Observer_Test_0400
+ * @tc.name: WaitForFinish
+ * @tc.desc: Verify the WaitForFinish return false.
+ */
+HWTEST_F(TestObserverTest, Test_Observer_Test_0400, Function | MediumTest | Level1)
+{
+    HILOG_INFO("Test_Observer_Test_0400 is called");
+    TestObserver observer;
+    observer.isFinished_ = false;
+    bool ret = observer.WaitForFinish(1);
+    EXPECT_FALSE(ret);
+}
+
+/**
+ * @tc.number: Test_Observer_Test_0500
+ * @tc.name: WaitForFinish
+ * @tc.desc: Verify the TestStatus.
+ */
+HWTEST_F(TestObserverTest, Test_Observer_Test_0500, Function | MediumTest | Level1)
+{
+    HILOG_INFO("Test_Observer_Test_0500 is called");
+    TestObserver observer;
+    observer.isFinished_ = false;
+    observer.TestStatus(MSG.c_str(), RESULT_CODE);
+}
