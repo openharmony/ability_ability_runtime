0b91d7f150e87049f05cf90bc8bb4af0ac01097d
xinxin13 <xinxin13@huawei.com>
2022-12-28 14:14:28 +0800

    unittest_test
    Signed-off-by: xinxin13 <xinxin13@huawei.com>

---
+++ test/unittest/frameworks_kits_ability_native_test/ability_local_record_test.cpp
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#define private public
+#define protected public
+#include "ability_local_record.h"
+#include "ability_impl.h"
+#include "ability_thread.h"
+#include "iservice_registry.h"
+#include "iremote_object.h"
+#include "mock_ability_token.h"
+#include "parcel.h"
+#undef protected
+#undef private
+
+using namespace OHOS;
+using namespace OHOS::AppExecFwk;
+using namespace testing;
+using namespace testing::ext;
+
+class AbilityLocalRecordTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+};
+
+void AbilityLocalRecordTest::SetUpTestCase()
+{}
+
+void AbilityLocalRecordTest::TearDownTestCase()
+{}
+
+void AbilityLocalRecordTest::SetUp()
+{}
+
+void AbilityLocalRecordTest::TearDown()
+{}
+
+/**
+ * @tc.number: GetEventHandler_0100
+ * @tc.name: GetEventHandler
+ * @tc.desc: GetEventHandler Test, return is not nullptr.
+ */
+HWTEST_F(AbilityLocalRecordTest, GetEventHandler_0100, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetEventHandler_0100 start";
+    auto record = std::make_shared<OHOS::AppExecFwk::AbilityLocalRecord>(nullptr, nullptr);
+    auto handler = std::make_shared<EventHandler>(EventRunner::Create());
+    record->SetEventHandler(handler);
+    EXPECT_TRUE(record->GetEventHandler() != nullptr);
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetEventHandler_0100 end";
+}
+
+/**
+ * @tc.number: GetEventRunner_0100
+ * @tc.name: GetEventRunner
+ * @tc.desc: GetEventRunner Test, return is not nullptr.
+ */
+HWTEST_F(AbilityLocalRecordTest, GetEventRunner_0100, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest SetEventHandler_0100 start";
+    auto record = std::make_shared<OHOS::AppExecFwk::AbilityLocalRecord>(nullptr, nullptr);
+    std::shared_ptr<EventRunner> runner = EventRunner::GetMainEventRunner();
+    record->SetEventRunner(runner);
+    EXPECT_TRUE(record->GetEventRunner() != nullptr);
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest SetEventHandler_0100 end";
+}
+
+/**
+ * @tc.number: GetAbilityImpl_0100
+ * @tc.name: GetAbilityImpl
+ * @tc.desc: GetAbilityImpl Test, return is not nullptr.
+ */
+HWTEST_F(AbilityLocalRecordTest, GetAbilityImpl_0100, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetAbilityImpl_0100 start";
+    auto record = std::make_shared<OHOS::AppExecFwk::AbilityLocalRecord>(nullptr, nullptr);
+    auto abilityImpl = std::make_shared<AbilityImpl>();
+    record->SetAbilityImpl(abilityImpl);
+    EXPECT_TRUE(record->GetAbilityImpl() != nullptr);
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetAbilityImpl_0100 end";
+}
+
+/**
+ * @tc.number: GetAbilityThread_0100
+ * @tc.name: GetAbilityThread
+ * @tc.desc: GetAbilityThread Test, return is nullptr.
+ */
+HWTEST_F(AbilityLocalRecordTest, GetAbilityThread_0100, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetAbilityThread_0100 start";
+    auto record = std::make_shared<OHOS::AppExecFwk::AbilityLocalRecord>(nullptr, nullptr);
+    auto abilityThread = sptr<AbilityThread>(nullptr);
+    record->SetAbilityThread(abilityThread);
+    EXPECT_TRUE(record->GetAbilityThread() == nullptr);
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetAbilityThread_0100 end";
+}
+
+/**
+ * @tc.number: GetWant_0100
+ * @tc.name: GetWant
+ * @tc.desc: GetWant Test, return is nullptr.
+ */
+HWTEST_F(AbilityLocalRecordTest, GetWant_0100, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetWant_0100 start";
+    auto record = std::make_shared<OHOS::AppExecFwk::AbilityLocalRecord>(nullptr, nullptr);
+    std::shared_ptr<AAFwk::Want> want;
+    record->SetWant(want);
+    EXPECT_TRUE(record->GetWant() == nullptr);
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetWant_0100 end";
+}
+
+/**
+ * @tc.number: GetAbilityInfo_0100
+ * @tc.name: GetAbilityInfo
+ * @tc.desc: GetAbilityInfo Test, return is not nullptr.
+ */
+HWTEST_F(AbilityLocalRecordTest, GetAbilityInfo_0100, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetAbilityInfo_0100 start";
+    auto info = std::make_shared<OHOS::AppExecFwk::AbilityInfo>();
+    sptr<IRemoteObject> token = new (std::nothrow) MockAbilityToken();
+    auto record = std::make_shared<OHOS::AppExecFwk::AbilityLocalRecord>(info, token);
+    EXPECT_TRUE(record->GetAbilityInfo() != nullptr);
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetAbilityInfo_0100 end";
+}
+
+/**
+ * @tc.number: GetToken_0100
+ * @tc.name: GetToken
+ * @tc.desc: GetToken Test, return is not nullptr.
+ */
+HWTEST_F(AbilityLocalRecordTest, GetToken_0100, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetToken_0100 start";
+    auto info = std::make_shared<OHOS::AppExecFwk::AbilityInfo>();
+    sptr<IRemoteObject> token = new (std::nothrow) MockAbilityToken();
+    auto record = std::make_shared<OHOS::AppExecFwk::AbilityLocalRecord>(info, token);
+    EXPECT_TRUE(record->GetToken() != nullptr);
+    GTEST_LOG_(INFO) << "AbilityLocalRecordTest GetToken_0100 end";
+}
---
+++ test/unittest/frameworks_kits_ability_native_test/ability_runtime_test.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#define private public
+#define protected public
+#include "hilog_wrapper.h"
+#include "iremote_object.h"
+#include "iservice_registry.h"
+#include "js_runtime.h"
+#include "mock_runtime.h"
+#include "runtime.h"
+#undef protected
+#undef private
+
+using namespace testing::ext;
+namespace OHOS {
+namespace AbilityRuntime {
+
+class RuntimeTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void RuntimeTest::SetUpTestCase(void)
+{}
+
+void RuntimeTest::TearDownTestCase(void)
+{}
+
+void RuntimeTest::SetUp()
+{}
+
+void RuntimeTest::TearDown()
+{}
+
+/**
+ * @tc.number: Create_0100
+ * @tc.name: Create
+ * @tc.desc: Create Test, runtime is not nullptr.
+ */
+HWTEST_F(RuntimeTest, Create_0100, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "RuntimeTest Create_0100 start";
+    Runtime::Options options;
+    std::unique_ptr<Runtime> runtime = std::make_unique<MockRuntime>();
+    auto result = runtime->Create(options);
+    EXPECT_TRUE(runtime != nullptr);
+    EXPECT_TRUE(result == nullptr);
+    GTEST_LOG_(INFO) << "RuntimeTest Create_0100 end";
+}
+
+/**
+ * @tc.number: Create_0200
+ * @tc.name: Create
+ * @tc.desc: Create Test, runtime is not nullptr.
+ */
+HWTEST_F(RuntimeTest, Create_0200, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "RuntimeTest Create_0200 start";
+    Runtime::Options options;
+    options.lang = Runtime::Language::JS;
+    options.preload = true;
+    std::unique_ptr<Runtime> runtime = std::make_unique<MockRuntime>();
+    auto result = runtime->Create(options);
+    EXPECT_TRUE(runtime != nullptr);
+    EXPECT_TRUE(result != nullptr);
+    GTEST_LOG_(INFO) << "RuntimeTest Create_0200 end";
+}
+
+/**
+ * @tc.number: SavePreloaded_0100
+ * @tc.name: SavePreloaded
+ * @tc.desc: Runtime test for SavePreloaded, runtime is not nullptr.
+ */
+HWTEST_F(RuntimeTest, SavePreloaded_0100, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "RuntimeTest SavePreloaded_0100 start";
+    Runtime::Options options;
+    std::unique_ptr<Runtime> runtime = std::make_unique<MockRuntime>();
+    auto instance = std::make_unique<MockRuntime>();
+    runtime->SavePreloaded(std::move(instance));
+    EXPECT_TRUE(runtime != nullptr);
+    GTEST_LOG_(INFO) << "RuntimeTest SavePreloaded_0100 end";
+}
+
+/**
+ * @tc.number: GetPreloaded_0100
+ * @tc.name: GetPreloaded
+ * @tc.desc: Runtime test for GetPreloaded, runtime is not nullptr.
+ */
+HWTEST_F(RuntimeTest, GetPreloaded_0100, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "RuntimeTest GetPreloaded_0100 start";
+    Runtime::Options options;
+    std::unique_ptr<Runtime> runtime = std::make_unique<MockRuntime>();
+    auto instance = std::make_unique<MockRuntime>();
+    runtime->SavePreloaded(std::move(instance));
+    EXPECT_TRUE(runtime->GetPreloaded() != nullptr);
+    GTEST_LOG_(INFO) << "RuntimeTest GetPreloaded_0100 end";
+}
+} // namespace AbilityRuntime
+} // namespace OHOS
