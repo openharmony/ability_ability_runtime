a648d59f56090502f8ab1486635adc95059cf0f9
dy_study <dingyao5@huawei.com>
2023-01-13 02:40:52 +0000

    IssueNo:#I69VGD Description:ApplicationContext创建JS对象功能优化 Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source: No
    Signed-off-by: dy_study <dingyao5@huawei.com>
    Change-Id: I44564d560e33dfb6887b26fdc7a88ade87386a8d

--- frameworks/native/appkit/ability_runtime/context/js_context_utils.cpp
+++ frameworks/native/appkit/ability_runtime/context/js_context_utils.cpp
@@ -59,11 +59,6 @@ public:
     static NativeValue* GetPreferencesDir(NativeEngine* engine, NativeCallbackInfo* info);
     static NativeValue* GetBundleCodeDir(NativeEngine* engine, NativeCallbackInfo* info);
 
-    void KeepContext(std::shared_ptr<Context> context)
-    {
-        keepContext_ = context;
-    }
-
 protected:
     std::weak_ptr<Context> context_;
 
@@ -74,8 +69,6 @@ private:
     NativeValue* OnGetArea(NativeEngine& engine, NativeCallbackInfo& info);
     NativeValue* OnCreateModuleContext(NativeEngine& engine, NativeCallbackInfo& info);
     bool CheckCallerIsSystemApp();
-
-    std::shared_ptr<Context> keepContext_;
 };
 
 void JsBaseContext::Finalizer(NativeEngine* engine, void* data, void* hint)
@@ -436,7 +429,8 @@ NativeValue* JsBaseContext::OnGetApplicationContext(NativeEngine& engine, Native
         AbilityRuntimeErrorUtil::Throw(engine, ERR_ABILITY_RUNTIME_EXTERNAL_INVALID_PARAMETER);
         return engine.CreateUndefined();
     }
-    NativeValue* value = CreateJsApplicationContext(engine, applicationContext, true);
+
+    NativeValue* value = JsApplicationContextUtils::CreateJsApplicationContext(engine);
     auto systemModule = JsRuntime::LoadSystemModuleByEngine(&engine, "application.ApplicationContext", &value, 1);
     if (systemModule == nullptr) {
         HILOG_WARN("OnGetApplicationContext, invalid systemModule.");
@@ -519,7 +513,7 @@ NativeValue* AttachApplicationContext(NativeEngine* engine, void* value, void* h
         HILOG_WARN("invalid context.");
         return nullptr;
     }
-    NativeValue* object = CreateJsApplicationContext(*engine, ptr, true);
+    NativeValue* object = JsApplicationContextUtils::CreateJsApplicationContext(*engine);
     auto systemModule = JsRuntime::LoadSystemModuleByEngine(engine, "application.ApplicationContext", &object, 1);
     if (systemModule == nullptr) {
         HILOG_WARN("invalid systemModule.");
@@ -550,9 +544,6 @@ NativeValue* CreateJsBaseContext(NativeEngine& engine, std::shared_ptr<Context> 
         return objValue;
     }
     auto jsContext = std::make_unique<JsBaseContext>(context);
-    if (keepContext) {
-        jsContext->KeepContext(context);
-    }
     SetNamedNativePointer(engine, *object, BASE_CONTEXT_NAME, jsContext.release(), JsBaseContext::Finalizer);
 
     auto appInfo = context->GetApplicationInfo();
