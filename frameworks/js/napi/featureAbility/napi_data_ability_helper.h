8698d3aa376b48b8ecb8da4f56cefba2bda323f1
zhangyafei.echo <zhangyafei12@huawei.com>
2023-02-16 15:51:53 +0800

    Description:optimize libabilitykit_native.z.so
    Signed-off-by: zhangyafei.echo <zhangyafei12@huawei.com>
    Change-Id: I9d2e9ca19e96b83641790531de92ec08dce71892

--- frameworks/js/napi/featureAbility/napi_data_ability_helper.h
+++ frameworks/js/napi/featureAbility/napi_data_ability_helper.h
@@ -15,6 +15,8 @@
 
 #ifndef OHOS_ABILITY_RUNTIME_NAPI_DATA_ABILITY_HELPER_H
 #define OHOS_ABILITY_RUNTIME_NAPI_DATA_ABILITY_HELPER_H
+
+#include "data_ability_helper_common.h"
 #include "data_ability_observer_stub.h"
 #include "feature_ability_common.h"
 
