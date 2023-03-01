6b0901c062a0ba3cf86202219936365bdc775856
dujingcheng <dujingcheng@huawei.com>
2023-03-01 02:35:25 +0000

    fix fd meak
    Signed-off-by: dujingcheng <dujingcheng@huawei.com>
    Change-Id: I7d4fda196d6eab39093d2b048ad1507571139711

--- frameworks/native/runtime/js_runtime.cpp
+++ frameworks/native/runtime/js_runtime.cpp
@@ -640,6 +640,9 @@ void JsRuntime::Deinitialize()
 
     methodRequireNapiRef_.reset();
 
+    if (nativeEngine_ == nullptr) {
+        return;
+    }
     auto uvLoop = nativeEngine_->GetUVLoop();
     auto fd = uvLoop != nullptr ? uv_backend_fd(uvLoop) : -1;
     if (fd >= 0 && eventHandler_ != nullptr) {
@@ -647,6 +650,7 @@ void JsRuntime::Deinitialize()
     }
     RemoveTask(TIMER_TASK);
 
+    nativeEngine_->DeleteEngine();
     nativeEngine_.reset();
 }
 
