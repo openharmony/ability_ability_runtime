65617e1b182ca60923680a0f981c583e30a3f814
chenwei <chenwei469@huawei.com>
2023-03-24 12:34:32 +0800

    获取应用堆内存占用信息
    Signed-off-by: chenwei <chenwei469@huawei.com>

--- frameworks/native/appkit/app/main_thread.cpp
+++ frameworks/native/appkit/app/main_thread.cpp
@@ -1789,12 +1789,11 @@ void MainThread::ScheduleHeapMemory(const int32_t pid, OHOS::AppExecFwk::MallocI
     int usmblks = mi.usmblks; // 当前从分配器中分配的总的堆内存大小
     int uordblks = mi.uordblks; // 当前已释放给分配器，分配缓存了未释放给系统的内存大小
     int fordblks = mi.fordblks; // 当前未释放的大小
-    HILOG_DEBUG("the application's pid: %{public}i", pid);
+    HILOG_DEBUG("The pid of the app we want to dump memory allocation information is: %{public}i", pid);
     HILOG_DEBUG("usmblks: %{public}i, uordblks: %{public}i, fordblks: %{public}i", usmblks, uordblks, fordblks);
-    pidInfo.clear();
-    pidInfo.push_back(usmblks);
-    pidInfo.push_back(uordblks);
-    pidInfo.push_back(fordblks);
+    mallocInfo.usmblks = usmblks;
+    mallocInfo.uordblks = uordblks;
+    mallocInfo.fordblks = fordblks;
 }
 
 /**
