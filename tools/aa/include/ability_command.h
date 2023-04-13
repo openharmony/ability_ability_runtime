70f166d0966c1ca7202cef943cca5c271244ca1c
hongtao <hongtao11@huawei.com>
2023-04-13 15:57:48 +0800

    Feature: add Startup Debugging, add parameters -N for aa
    Signed-off-by: hongtao <hongtao11@huawei.com>
    Change-Id: I8de454acb3653eaeb87d9fc49af0fba7a8bf4134

--- tools/aa/include/ability_command.h
+++ tools/aa/include/ability_command.h
@@ -58,7 +58,7 @@ const std::string HELP_MSG_START =
     "usage: aa start <options>\n"
     "options list:\n"
     "  -h, --help                                                   list available commands\n"
-    "  [-d <device-id>] -a <ability-name> -b <bundle-name> [-m <module-name>] [-p <perf-cmd>] [-D] [-S] "
+    "  [-d <device-id>] -a <ability-name> -b <bundle-name> [-m <module-name>] [-p <perf-cmd>] [-D] [-S] [-N] "
     "  start ability with an element name\n";
 
 const std::string HELP_MSG_STOP_SERVICE =
