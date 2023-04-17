6a67ca730bd281b45042498ebef799f9de964c98
zhoushicheng <zhoushicheng2@huawei.com>
2023-04-17 12:17:30 +0000

    Issue:#I6WGUJ
    Signed-off-by: zhoushicheng <zhoushicheng2@huawei.com>
    Change-Id: Id4bb3d91330d9e408d9ca54a6764f31e698ad1f2

---
+++ test/fuzztest/amsmanager_fuzzer/amsmanager_fuzzer.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "amsmanager_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#include "ams_mgr_scheduler.h"
+#include "app_mgr_constants.h"
+#include "app_mgr_service_event_handler.h"
+#include "app_mgr_service_inner.h"
+#include "message_parcel.h"
+#include "securec.h"
+
+using namespace OHOS::AAFwk;
+using namespace OHOS::AppExecFwk;
+
+namespace OHOS {
+namespace {
+constexpr size_t FOO_MAX_LEN = 1024;
+constexpr size_t U32_AT_SIZE = 4;
+}
+const std::u16string AMSMGR_INTERFACE_TOKEN = u"ohos.aafwk.AmsManager";
+
+uint32_t GetU32Data(const char* ptr)
+{
+    // convert fuzz input data to an integer
+    return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
+}
+
+bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
+{
+    uint32_t code = GetU32Data(data);
+
+    MessageParcel parcel;
+    parcel.WriteInterfaceToken(AMSMGR_INTERFACE_TOKEN);
+    parcel.WriteBuffer(data, size);
+    parcel.RewindRead(0);
+    MessageParcel reply;
+    MessageOption option;
+    std::shared_ptr<AppMgrServiceInner> appMgrServiceInner = std::make_shared<AppMgrServiceInner>();
+    std::shared_ptr<EventRunner> runner = EventRunner::Create(Constants::APP_MGR_SERVICE_NAME);
+    std::shared_ptr<AMSEventHandler> eventHandler = std::make_shared<AMSEventHandler>(runner, appMgrServiceInner);
+    std::shared_ptr<AmsMgrScheduler> amsMgr = std::make_shared<AmsMgrScheduler>(appMgrServiceInner, eventHandler);
+    amsMgr->OnRemoteRequest(code, parcel, reply, option);
+
+    return true;
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        std::cout << "invalid data" << std::endl;
+        return 0;
+    }
+
+    /* Validate the length of size */
+    if (size > OHOS::FOO_MAX_LEN || size < OHOS::U32_AT_SIZE) {
+        return 0;
+    }
+
+    char* ch = (char*)malloc(size + 1);
+    if (ch == nullptr) {
+        std::cout << "malloc failed." << std::endl;
+        return 0;
+    }
+
+    (void)memset_s(ch, size + 1, 0x00, size + 1);
+    if (memcpy_s(ch, size, data, size) != EOK) {
+        std::cout << "copy failed." << std::endl;
+        free(ch);
+        ch = nullptr;
+        return 0;
+    }
+
+    OHOS::DoSomethingInterestingWithMyAPI(ch, size);
+    free(ch);
+    ch = nullptr;
+    return 0;
+}
+
