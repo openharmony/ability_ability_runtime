b066ae8d8308c4324606f14ce795f5719afa7c92
chenwei <chenwei469@huawei.com>
2023-04-18 20:05:30 +0800

    HeapMemory在appRunningManager中加锁粒度太大
    Signed-off-by: chenwei <chenwei469@huawei.com>

--- services/appmgr/src/app_running_manager.cpp
+++ services/appmgr/src/app_running_manager.cpp
@@ -592,7 +592,7 @@ int32_t AppRunningManager::NotifyMemoryLevel(int32_t level)
 
 int32_t AppRunningManager::DumpHeapMemory(const int32_t pid, OHOS::AppExecFwk::MallocInfo &mallocInfo)
 {
-    auto iter;
+    std::map<const int32_t, const std::shared_ptr<AppRunningRecord>>::iterator iter == nullptr;
     {
         std::lock_guard<std::recursive_mutex> guard(lock_);
         HILOG_INFO("call %{public}s, current app size %{public}zu", __func__, appRunningRecordMap_.size());
