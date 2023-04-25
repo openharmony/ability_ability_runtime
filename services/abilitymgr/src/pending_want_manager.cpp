33047fb1607586b5758034bd2b1efb2ad359a97b
XKK <huyingsong@huawei.com>
2023-04-25 21:16:07 +0800

    wantAgent发送公共事件 传递bundleName值
    Signed-off-by: XKK <huyingsong@huawei.com>

--- services/abilitymgr/src/pending_want_manager.cpp
+++ services/abilitymgr/src/pending_want_manager.cpp
@@ -284,6 +284,12 @@ int32_t PendingWantManager::PendingWantPublishCommonEvent(
     eventData.SetCode(senderInfo.code);
 
     CommonEventPublishInfo eventPublishData;
+
+    if (!want.GetBundle().empty()) {
+        HILOG_INFO("eventPublishData set bundleName = %{public}s", want.GetBundle().c_str());
+        eventPublishData.SetBundleName(want.GetBundle());
+    }
+
     if (!senderInfo.requiredPermission.empty()) {
         std::vector<std::string> permissions;
         permissions.emplace_back(senderInfo.requiredPermission);
