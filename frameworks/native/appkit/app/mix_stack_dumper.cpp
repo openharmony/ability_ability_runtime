197913dc2d00ab27d32b54da4c719099ff33dfaa
yanmengzhao1 <yanmengzhao1@huawei.com>
2023-05-23 11:41:02 +0800

    use backtrace_local instead of dumpcatcher
    Signed-off-by: yanmengzhao1 <yanmengzhao1@huawei.com>

--- frameworks/native/appkit/app/mix_stack_dumper.cpp
+++ frameworks/native/appkit/app/mix_stack_dumper.cpp
@@ -23,36 +23,26 @@
 #include <unistd.h>
 #include <sigchain.h>
 
-#include "dfx_dump_catcher.h"
 #include "dfx_dump_res.h"
+#include "dfx_frame_format.h"
+#include "dfx_util.h"
 #include "faultloggerd_client.h"
 #include "file_ex.h"
 #include "hilog_wrapper.h"
 #include "js_runtime.h"
+
+using namespace OHOS::HiviewDFX;
 
 namespace OHOS {
 namespace AppExecFwk {
 std::weak_ptr<EventHandler> MixStackDumper::signalHandler_;
 std::weak_ptr<OHOSApplication> MixStackDumper::application_;
 static std::mutex g_mutex;
-using OHOS::HiviewDFX::NativeFrame;
+
 namespace {
-static const char PID_STR_NAME[] = "Pid:";
-static const char PPID_STR_NAME[] = "PPid:";
-static const char NSPID_STR_NAME[] = "NSpid:";
-static const char PROC_SELF_STATUS_PATH[] = "/proc/self/status";
 static const std::string PROC_SELF_CMDLINE_PATH = "/proc/self/cmdline";
-static constexpr int STATUS_LINE_SIZE = 1024;
-static constexpr int FRAME_BUF_LEN = 1024;
 static constexpr int HEADER_BUF_LEN = 512;
-static constexpr int NAMESPACE_MATCH_NUM = 2;
 static bool hasInstalled = false;
-typedef struct ProcInfo {
-    int tid;
-    int pid;
-    int ppid;
-    bool ns;
-} ProcInfo;
 }
 
 static pid_t g_targetDumpTid = -1;
@@ -63,39 +53,6 @@ static std::string PrintJsFrame(const JsFrames& jsFrame)
     return "  at " + jsFrame.functionName + " (" + jsFrame.fileName + ":" + jsFrame.pos + ")\n";
 }
 
-static std::string PrintNativeFrame(const NativeFrame& frame)
-{
-    char buf[FRAME_BUF_LEN] = {0};
-    std::string mapName = frame.binaryName;
-    if (mapName.empty()) {
-        mapName = "Unknown";
-    }
-
-#ifdef __LP64__
-    char frameFormatWithMapName[] = "#%02zu pc %016" PRIx64 " %s\n";
-    char frameFormatWithFuncName[] = "#%02zu pc %016" PRIx64 " %s(%s+%" PRIu64 ")\n";
-#else
-    char frameFormatWithMapName[] = "#%02zu pc %08" PRIx64 " %s\n";
-    char frameFormatWithFuncName[] = "#%02zu pc %08" PRIx64 " %s(%s+%" PRIu64 ")\n";
-#endif
-
-    if (frame.funcName.empty()) {
-        int ret = snprintf_s(buf, sizeof(buf), sizeof(buf) - 1, frameFormatWithMapName, \
-            frame.index, frame.relativePc, mapName.c_str());
-        if (ret <= 0) {
-            HILOG_ERROR("DfxMixStackDumper::PrintNativeFrame snprintf_s failed.");
-        }
-        return std::string(buf);
-    }
-
-    int ret = snprintf_s(buf, sizeof(buf), sizeof(buf) - 1, frameFormatWithFuncName, \
-        frame.index, frame.relativePc, mapName.c_str(),\
-        frame.funcName.c_str(), frame.funcOffset);
-    if (ret <= 0) {
-        HILOG_ERROR("DfxMixStackDumper::PrintNativeFrame snprintf_s failed.");
-    }
-    return std::string(buf);
-}
 
 static pid_t GetPid()
 {
@@ -105,120 +62,6 @@ static pid_t GetPid()
 static bool HasNameSpace()
 {
     return g_procInfo.ns;
-}
-
-static int GetProcStatus(struct ProcInfo* procInfo)
-{
-    if (procInfo->pid >= 1) {
-        return 0;
-    }
-
-    procInfo->pid = getpid();
-    procInfo->tid = gettid();
-    procInfo->ppid = getppid();
-    procInfo->ns = false;
-    char buf[STATUS_LINE_SIZE];
-    FILE *fp = fopen(PROC_SELF_STATUS_PATH, "r");
-    if (fp == nullptr) {
-        return -1;
-    }
-    int p = 0, pp = 0, t = 0;
-    while (!feof(fp)) {
-        if (fgets(buf, STATUS_LINE_SIZE, fp) == nullptr) {
-            fclose(fp);
-            return -1;
-        }
-        if (strncmp(buf, PID_STR_NAME, strlen(PID_STR_NAME)) == 0) {
-            // Pid:    1892
-            if (sscanf_s(buf, "%*[^0-9]%d", &p) != 1) {
-                perror("sscanf_s failed.");
-            }
-            procInfo->pid = p;
-            if (procInfo->pid == getpid()) {
-                procInfo->ns = false;
-                break;
-            }
-            procInfo->ns = true;
-            continue;
-        }
-        if (strncmp(buf, PPID_STR_NAME, strlen(PPID_STR_NAME)) == 0) {
-            // PPid:   240
-            if (sscanf_s(buf, "%*[^0-9]%d", &pp) != 1) {
-                perror("sscanf_s failed.");
-            }
-            procInfo->ppid = pp;
-            continue;
-        }
-        // NSpid:  1892    1
-        if (strncmp(buf, NSPID_STR_NAME, strlen(NSPID_STR_NAME)) == 0) {
-            if (sscanf_s(buf, "%*[^0-9]%d%*[^0-9]%d", &p, &t) != NAMESPACE_MATCH_NUM) {
-                perror("sscanf_s failed.");
-            }
-            procInfo->tid = t;
-            break;
-        }
-    }
-    (void)fclose(fp);
-    return 0;
-}
-
-static void TidToNstid(const int tid, int& nstid)
-{
-    char path[NAME_LEN];
-    (void)memset_s(path, sizeof(path), '\0', sizeof(path));
-    if (snprintf_s(path, sizeof(path), sizeof(path) - 1, "/proc/%d/task/%d/status", GetPid(), tid) <= 0) {
-        HILOG_WARN("snprintf_s error.");
-        return;
-    }
-
-    char buf[STATUS_LINE_SIZE];
-    FILE *fp = fopen(path, "r");
-    if (fp == nullptr) {
-        return;
-    }
-
-    int p = 0, t = 0;
-    while (!feof(fp)) {
-        if (fgets(buf, STATUS_LINE_SIZE, fp) == nullptr) {
-            fclose(fp);
-            return;
-        }
-
-        // NSpid:  1892    1
-        if (strncmp(buf, NSPID_STR_NAME, strlen(NSPID_STR_NAME)) == 0) {
-            if (sscanf_s(buf, "%*[^0-9]%d%*[^0-9]%d", &p, &t) != NAMESPACE_MATCH_NUM) {
-                HILOG_ERROR("sscanf_s failed.");
-            }
-            nstid = t;
-            break;
-        }
-    }
-    (void)fclose(fp);
-}
-
-static std::string GetCurrentTimeStr(uint64_t current = 0)
-{
-    time_t now = time(nullptr);
-    uint64_t millisecond = 0;
-    const uint64_t ratio = 1000;
-    if (current > static_cast<uint64_t>(now)) {
-        millisecond = current % ratio;
-        now = static_cast<time_t>(current / ratio);
-    }
-
-    auto tm = std::localtime(&now);
-    char seconds[128] = { 0 }; // 128 : time buffer size
-    if (tm == nullptr || strftime(seconds, sizeof(seconds) - 1, "%Y-%m-%d %H:%M:%S", tm) == 0) {
-        return "invalid timestamp\n";
-    }
-
-    char formatTimeBuf[256] = { 0 }; // 256 : buffer size
-    int ret = snprintf_s(formatTimeBuf, sizeof(formatTimeBuf), sizeof(formatTimeBuf) - 1,
-        "%s.%03u\n", seconds, millisecond);
-    if (ret <= 0) {
-        return "invalid timestamp\n";
-    }
-    return std::string(formatTimeBuf, strlen(formatTimeBuf));
 }
 
 bool MixStackDumper::Dump_SignalHandler(int sig, siginfo_t *si, void *context)
@@ -275,7 +118,7 @@ bool MixStackDumper::IsJsNativePcEqual(uintptr_t *jsNativePointer, uint64_t nati
 }
 
 void MixStackDumper::BuildJsNativeMixStack(int fd, std::vector<JsFrames>& jsFrames,
-    std::vector<NativeFrame>& nativeFrames)
+    std::vector<DfxFrame>& nativeFrames)
 {
     uint32_t jsIdx = 0;
     uint32_t nativeIdx = 0;
@@ -295,13 +138,13 @@ void MixStackDumper::BuildJsNativeMixStack(int fd, std::vector<JsFrames>& jsFram
         if (IsJsNativePcEqual(jsFrames[jsIdx].nativePointer, nativeFrames[nativeIdx].pc,
             nativeFrames[nativeIdx].funcOffset)) {
             HILOG_DEBUG("DfxMixStackDumper::BuildJsNativeMixStack pc register values matched.");
-            mixStackStr += PrintNativeFrame(nativeFrames[nativeIdx]);
+            mixStackStr += HiviewDFX::DfxFrameFormat::GetFrameStr(nativeFrames[nativeIdx]);
             mixStackStr += PrintJsFrame(jsFrames[jsIdx]);
             nativeIdx++;
             jsIdx++;
             matchJsFrame = true;
         } else {
-            mixStackStr += PrintNativeFrame(nativeFrames[nativeIdx]);
+            mixStackStr += HiviewDFX::DfxFrameFormat::GetFrameStr(nativeFrames[nativeIdx]);
             nativeIdx++;
         }
     }
@@ -316,7 +159,7 @@ void MixStackDumper::BuildJsNativeMixStack(int fd, std::vector<JsFrames>& jsFram
     }
 
     while (nativeIdx < nativeFrames.size()) {
-        mixStackStr += PrintNativeFrame(nativeFrames[nativeIdx]);
+        mixStackStr += HiviewDFX::DfxFrameFormat::GetFrameStr(nativeFrames[nativeIdx]);
         nativeIdx++;
     }
 
@@ -337,15 +180,6 @@ std::string MixStackDumper::GetThreadStackTraceLabel(pid_t tid)
         result << std::endl;
     }
     return result.str();
-}
-
-void MixStackDumper::PrintNativeFrames(int fd, std::vector<NativeFrame>& nativeFrames)
-{
-    for (const auto& frame : nativeFrames) {
-        std::string nativeFrameStr = PrintNativeFrame(frame);
-        Write(fd, nativeFrameStr);
-    }
-    Write(fd, "\n");
 }
 
 void MixStackDumper::PrintProcessHeader(int fd, pid_t pid, uid_t uid)
@@ -376,7 +210,7 @@ bool MixStackDumper::DumpMixFrame(int fd, pid_t nstid, pid_t tid)
         return false;
     }
 
-    std::vector<NativeFrame> nativeFrames;
+    std::vector<DfxFrame> nativeFrames;
     bool hasNativeFrame = true;
     if (!catcher_->CatchFrame(nstid, nativeFrames)) {
         hasNativeFrame = false;
@@ -404,7 +238,8 @@ bool MixStackDumper::DumpMixFrame(int fd, pid_t nstid, pid_t tid)
     std::string threadComm = GetThreadStackTraceLabel(tid);
     Write(fd, threadComm);
     if (hasNativeFrame && !hasJsFrame) {
-        PrintNativeFrames(fd, nativeFrames);
+        Write(fd, HiviewDFX::DfxFrameFormat::GetFramesStr(nativeFrames));
+        Write(fd, "\n");
         return true;
     }
 
@@ -442,7 +277,7 @@ void MixStackDumper::GetThreadList(std::vector<pid_t>& threadList)
 
 void MixStackDumper::Init(pid_t pid)
 {
-    catcher_ = std::make_unique<OHOS::HiviewDFX::DfxDumpCatcher>(pid);
+    catcher_ = std::make_unique<OHOS::HiviewDFX::DfxCatchFrameLocal>(pid);
     if (!catcher_->InitFrameCatcher()) {
         HILOG_ERROR("Init DumpCatcher Failed.");
     }
@@ -460,16 +295,16 @@ void MixStackDumper::HandleMixDumpRequest()
 {
     int fd = -1;
     int resFd = -1;
-    int dumpRes = OHOS::HiviewDFX::ProcessDumpRes::DUMP_ESUCCESS;
+    int dumpRes = OHOS::HiviewDFX::DumpErrorCode::DUMP_ESUCCESS;
     std::unique_lock<std::mutex> lock(g_mutex);
     HILOG_INFO("Current process is ready to dump stack trace.");
-    (void)GetProcStatus(&g_procInfo);
+    (void)GetProcStatus(g_procInfo);
     do {
         fd = RequestPipeFd(GetPid(), FaultLoggerPipeType::PIPE_FD_WRITE_BUF);
         resFd = RequestPipeFd(GetPid(), FaultLoggerPipeType::PIPE_FD_WRITE_RES);
         if (fd < 0 || resFd < 0) {
             HILOG_ERROR("request pid(%{public}d) pipe fd failed", GetPid());
-            dumpRes = OHOS::HiviewDFX::ProcessDumpRes::DUMP_EGETFD;
+            dumpRes = OHOS::HiviewDFX::DumpErrorCode::DUMP_EGETFD;
             break;
         }
         MixStackDumper mixDumper;
@@ -477,14 +312,8 @@ void MixStackDumper::HandleMixDumpRequest()
         g_targetDumpTid = -1;
     } while (false);
 
-    OHOS::HiviewDFX::DumpResMsg dumpResMsg;
-    dumpResMsg.res = dumpRes;
-    const char* strRes = OHOS::HiviewDFX::DfxDumpRes::GetInstance().GetResStr(dumpRes);
-    if (strncpy_s(dumpResMsg.strRes, sizeof(dumpResMsg.strRes), strRes, sizeof(dumpResMsg.strRes) - 1) != 0) {
-        HILOG_ERROR("DfxMixStackDumper::HandleProcessMixDumpRequest strncpy_s failed.");
-    }
     if (resFd >= 0) {
-        write(resFd, &dumpResMsg, sizeof(struct OHOS::HiviewDFX::DumpResMsg));
+        write(resFd, &dumpRes, sizeof(dumpRes));
         close(resFd);
     }
     if (fd >= 0) {
@@ -514,7 +343,7 @@ std::string MixStackDumper::DumpMixStackLocked(int fd, pid_t requestTid)
         pid_t targetNsTid = requestTid;
         pid_t targetTid = requestTid;
         if (HasNameSpace()) {
-            TidToNstid(targetTid, targetNsTid);
+            TidToNstid(GetPid(), targetTid, targetNsTid);
         }
         DumpMixFrame(fd, targetNsTid, targetTid);
     } else {
@@ -523,7 +352,7 @@ std::string MixStackDumper::DumpMixStackLocked(int fd, pid_t requestTid)
         for (auto& tid : threads) {
             pid_t nstid = tid;
             if (HasNameSpace()) {
-                TidToNstid(tid, nstid);
+                TidToNstid(GetPid(), tid, nstid);
             }
             if (nstid == gettid()) {
                 continue;
@@ -538,9 +367,9 @@ std::string MixStackDumper::DumpMixStackLocked(int fd, pid_t requestTid)
 std::string MixStackDumper::GetMixStack(bool onlyMainThread)
 {
     std::unique_lock<std::mutex> lock(g_mutex);
-    (void)GetProcStatus(&g_procInfo);
+    (void)GetProcStatus(g_procInfo);
     MixStackDumper mixDumper;
     return mixDumper.DumpMixStackLocked(-1, onlyMainThread ? GetPid() : -1);
 }
-} // AppExecFwk
-} // OHOS
+} // namespace AppExecFwk
+} // namespace OHOS
