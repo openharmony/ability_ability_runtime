8902ddc82d6d44e2dab46a010a7e0eaafa242300
wangzhen <wangzhen346@huawei.com>
2023-05-31 17:19:57 +0800

    Support loading library from hap file
    Signed-off-by: wangzhen <wangzhen346@huawei.com>

--- interfaces/kits/native/appkit/app/main_thread.h
+++ interfaces/kits/native/appkit/app/main_thread.h
@@ -558,7 +558,8 @@ private:
      */
     void LoadAbilityLibrary(const std::vector<std::string> &libraryPaths);
 
-    void LoadNativeLiabrary(std::string &nativeLibraryPath);
+    void CalcNativeLiabraryEntries(const BundleInfo &bundleInfo, std::string &nativeLibraryPath);
+    void LoadNativeLiabrary(const BundleInfo &bundleInfo, std::string &nativeLibraryPath);
 
     void LoadAppDetailAbilityLibrary(std::string &nativeLibraryPath);
 
