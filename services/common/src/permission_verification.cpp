a88d2ff610d0ce4ed5a3728c33bfd2ff778627ff
donglin <donglin9@huawei.com>
2023-06-09 06:31:18 +0000

    add mission permission check
    Signed-off-by: donglin <donglin9@huawei.com>
    Change-Id: Ia3f70aac72389a1a7338fbc6f87c92c1bb4d681d

--- services/common/src/permission_verification.cpp
+++ services/common/src/permission_verification.cpp
@@ -405,5 +405,17 @@ bool PermissionVerification::VerifyPrepareTerminatePermission() const
     HILOG_DEBUG("%{public}s: Permission verification failed", __func__);
     return false;
 }
+
+bool PermissionVerification::VerifyPrepareTerminatePermission(const int &tokenId) const
+{
+    int32_t ret = Security::AccessToken::AccessTokenKit::VerifyAccessToken(tokenId,
+        PermissionConstants::PERMISSION_PREPARE_TERMINATE);
+    if (ret == Security::AccessToken::PermissionState::PERMISSION_DENIED) {
+        HILOG_DEBUG("permission denied.");
+        return false;
+    }
+    HILOG_DEBUG("verify AccessToken success");
+    return true;
+}
 }  // namespace AAFwk
 }  // namespace OHOS
