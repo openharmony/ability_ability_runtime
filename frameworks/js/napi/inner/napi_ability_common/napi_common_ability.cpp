a7f3a04135f0c1594608930d615e6ac7cd0aa149
xuzheheng <xuzheheng2@huawei.com>
2023-06-13 09:00:23 +0000

    xuzheheng2@huawei.com
    Signed-off-by: xuzheheng <xuzheheng2@huawei.com>
    Change-Id: Ie4c5eff3993684ba1d9e74fb09f2daab0c5d5391

--- frameworks/js/napi/inner/napi_ability_common/napi_common_ability.cpp
+++ frameworks/js/napi/inner/napi_ability_common/napi_common_ability.cpp
@@ -4058,6 +4058,7 @@ NativeValue* JsNapiCommon::JsConnectAbility(
             engine.CreateUndefined());
         NAPI_CALL_BASE(env, napi_call_function(env, undefined, callback, ARGS_ONE, &resultVal, &callResult),
             engine.CreateUndefined());
+        connectionCallback->Reset();
         RemoveConnectionLocked(want);
     }
     return CreateJsValue(engine, id);
