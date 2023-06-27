f4d5dac3ee0a88c4f34e5f5b2a1b4c68a677fb85
dy_study <dingyao5@huawei.com>
2023-06-27 09:44:54 +0000

    Add UIExtension default background color.
    Signed-off-by: dy_study <dingyao5@huawei.com>
    Change-Id: Ia5da0c01c058fe3ce7e19c1c54641643759ddd9c

--- frameworks/native/ability/native/ui_extension_ability/js_ui_extension.cpp
+++ frameworks/native/ability/native/ui_extension_ability/js_ui_extension.cpp
@@ -38,6 +38,7 @@ using namespace OHOS::AppExecFwk;
 namespace {
 constexpr size_t ARGC_ONE = 1;
 constexpr size_t ARGC_TWO = 2;
+constexpr static char DEFAULT_BACKGROUND_COLOR[] = "#40ffffff";
 }
 
 NativeValue *AttachUIExtensionContext(NativeEngine *engine, void *value, void *)
@@ -329,6 +330,7 @@ void JsUIExtension::ForegroundWindow(const AAFwk::Want &want, const sptr<AAFwk::
     }
     auto& uiWindow = uiWindowMap_[obj];
     if (uiWindow) {
+        uiWindow->SetBackgroundColor(DEFAULT_BACKGROUND_COLOR);
         uiWindow->Show();
         foregroundWindows_.emplace(obj);
     }
