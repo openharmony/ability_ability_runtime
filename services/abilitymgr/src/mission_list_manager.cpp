92202f9a502b2f1fa335a82274f13059fc6b0bd6
gongyuechen <gongyuechen@huawei.com>
2023-07-03 12:45:06 +0000

    fix nextabilityrecord
    Signed-off-by: gongyuechen <gongyuechen@huawei.com>

--- services/abilitymgr/src/mission_list_manager.cpp
+++ services/abilitymgr/src/mission_list_manager.cpp
@@ -394,7 +394,7 @@ int MissionListManager::StartAbilityLocked(const std::shared_ptr<AbilityRecord> 
             // mark if need back to other mission stack
             targetAbilityRecord->SetNeedBackToOtherMissionStack(true);
             auto focusAbility = OHOS::DelayedSingleton<AbilityManagerService>::GetInstance()->GetFocusAbility();
-            if (focusAbility) {
+            if (focusAbility && (GetMissionIdByAbilityToken(focusAbility->GetToken()) != -1)) {
                 targetAbilityRecord->SetOtherMissionStackAbilityRecord(focusAbility);
             } else {
                 targetAbilityRecord->SetOtherMissionStackAbilityRecord(currentTopAbility);
