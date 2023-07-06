ba62c136e2bc7a6a3cb2122d6fe300ac36c98405
xinking129 <xinxin13@huawei.com>
2023-07-06 15:02:34 +0800

    Suggestions for modifying the simulator review
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- frameworks/simulator/ability_simulator/src/js_resource_manager_utils.cpp
+++ frameworks/simulator/ability_simulator/src/js_resource_manager_utils.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -27,5 +27,5 @@ NativeValue *CreateJsResourceManager(NativeEngine &engine,
     napi_value result = Global::Resource::ResourceManagerAddon::Create(env, "", resourceManager, context);
     return reinterpret_cast<NativeValue*>(result);
 }
-}  // namespace AbilityRuntime
-}  // namespace OHOS
+} // namespace AbilityRuntime
+} // namespace OHOS
