f8425e68e5460b3d5c3019c95376b64431468e59
yuwenze <yuwenze1@huawei.com>
2023-07-20 02:42:48 +0000

    support uri permission
    Signed-off-by: yuwenze <yuwenze1@huawei.com>
    Change-Id: I35ec0b6d9e62f88d69e157c8a330a1d8dc384952

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -2219,7 +2219,7 @@ int AbilityManagerService::SendResultToAbility(int32_t requestCode, int32_t resu
     CHECK_POINTER_AND_RETURN(abilityRecord, ERR_INVALID_VALUE);
 
     abilityRecord->SetResult(std::make_shared<AbilityResult>(requestCode, resultCode, resultWant));
-    abilityRecord->SendResult();
+    abilityRecord->SendResult(0, 0);
     return ERR_OK;
 }
 
@@ -7447,7 +7447,7 @@ int32_t AbilityManagerService::SendResultToAbilityByToken(const Want &want, cons
         return ERR_INVALID_VALUE;
     }
     abilityRecord->SetResult(std::make_shared<AbilityResult>(requestCode, resultCode, want));
-    abilityRecord->SendResult();
+    abilityRecord->SendResult(0, 0);
     return ERR_OK;
 }
 
