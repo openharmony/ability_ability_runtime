93d9f0dd8f5c9ac85ad212a06823cf2f8e4e9bb0
donglin <donglin9@huawei.com>
2023-08-01 16:15:02 +0800

    fix ClearMission time
    Signed-off-by: donglin <donglin9@huawei.com>

--- services/abilitymgr/src/mission_info_mgr.cpp
+++ services/abilitymgr/src/mission_info_mgr.cpp
@@ -404,35 +404,6 @@ void MissionInfoMgr::SetMissionAbilityState(int32_t missionId, AbilityState stat
         return;
     }
     it->missionInfo.abilityState = state;
-
-    auto collaborator = DelayedSingleton<AbilityManagerService>::GetInstance()->GetCollaborator(
-        it->collaboratorType);
-    if (collaborator == nullptr) {
-        HILOG_DEBUG("collaborator is nullptr");
-    } else {
-        HILOG_INFO("start notify collaborator, missionId:%{public}d, state:%{public}d", missionId,
-            static_cast<int32_t>(state));
-        int ret = ERR_OK;
-        switch (state) {
-            case AbilityState::FOREGROUNDING: {
-                ret = collaborator->NotifyMoveMissionToForeground(missionId);
-                break;
-            }
-            case AbilityState::BACKGROUNDING: {
-                ret = collaborator->NotifyMoveMissionToBackground(missionId);
-                break;
-            }
-            case AbilityState::TERMINATING: {
-                ret = collaborator->NotifyTerminateMission(missionId);
-                break;
-            }
-            default:
-                break;
-        }
-        if (ret != ERR_OK) {
-            HILOG_ERROR("notify broker move mission to background failed, err: %{public}d", ret);
-        }
-    }
 }
 
 bool MissionInfoMgr::LoadAllMissionInfo()
