826ef0b800eb401b454285f2bbc0356ef10abc57
xia <xialiangwei1@huawei.com>
2023-09-25 17:27:21 +0800

    稳定性代码检视问题修复
    Signed-off-by: xia <xialiangwei1@huawei.com>

--- services/abilitymgr/src/ability_manager_stub.cpp
+++ services/abilitymgr/src/ability_manager_stub.cpp
@@ -654,19 +654,19 @@ int AbilityManagerStub::StartAbilityByUIContentSessionAddCallerInner(MessageParc
     sptr<IRemoteObject> callerToken = nullptr;
     if (data.ReadBool()) {
         callerToken = data.ReadRemoteObject();
-    }
-    if (callerToken == nullptr) {
-        HILOG_ERROR("callerToken is nullptr");
-        return ERR_INVALID_VALUE;
+        if (callerToken == nullptr) {
+            HILOG_ERROR("callerToken is nullptr");
+            return ERR_INVALID_VALUE;
+        }
     }
 
     sptr<SessionInfo> sessionInfo = nullptr;
     if (data.ReadBool()) {
         sessionInfo = data.ReadParcelable<SessionInfo>();
-    }
-    if (sessionInfo == nullptr) {
-        HILOG_ERROR("sessionInfo is nullptr");
-        return ERR_INVALID_VALUE;
+        if (sessionInfo == nullptr) {
+            HILOG_ERROR("sessionInfo is nullptr");
+            return ERR_INVALID_VALUE;
+        }
     }
 
     int32_t userId = data.ReadInt32();
@@ -691,18 +691,18 @@ int AbilityManagerStub::StartAbilityByUIContentSessionForOptionsInner(MessagePar
     sptr<IRemoteObject> callerToken = nullptr;
     if (data.ReadBool()) {
         callerToken = data.ReadRemoteObject();
-    }
-    if (callerToken == nullptr) {
-        HILOG_ERROR("callerToken is nullptr");
-        return ERR_INVALID_VALUE;
+        if (callerToken == nullptr) {
+            HILOG_ERROR("callerToken is nullptr");
+            return ERR_INVALID_VALUE;
+        }
     }
     sptr<SessionInfo> sessionInfo = nullptr;
     if (data.ReadBool()) {
         sessionInfo = data.ReadParcelable<SessionInfo>();
-    }
-    if (sessionInfo == nullptr) {
-        HILOG_ERROR("sessionInfo is nullptr");
-        return ERR_INVALID_VALUE;
+        if (sessionInfo == nullptr) {
+            HILOG_ERROR("sessionInfo is nullptr");
+            return ERR_INVALID_VALUE;
+        }
     }
     int32_t userId = data.ReadInt32();
     int requestCode = data.ReadInt32();
