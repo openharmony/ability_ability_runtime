809d06674776c1ed40b9379a057578c2314d0dda
xinking129 <xinxin13@huawei.com>
2023-09-27 17:46:15 +0800

    add ui ability code fourth
    Signed-off-by: xinking129 <xinxin13@huawei.com>

---
+++ interfaces/kits/native/ability/native/ui_ability_impl.h
@@ -0,0 +1,229 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_ABILITY_RUNTIME_UI_ABILITY_IMPL_H
+#define OHOS_ABILITY_RUNTIME_UI_ABILITY_IMPL_H
+
+#include "ui_ability.h"
+
+namespace OHOS {
+namespace AppExecFwk {
+class AbilityHandler;
+class AbilityLocalRecord;
+class AbilityLifecycleCallbacks;
+class OHOSApplication;
+} // namespace AppExecFwk
+namespace AbilityRuntime {
+class UIAbility;
+class UIAbilityImpl : public std::enable_shared_from_this<UIAbilityImpl> {
+public:
+    UIAbilityImpl() = default;
+    ~UIAbilityImpl() = default;
+
+    /**
+     * @brief Init the UIAbilityImpl object.
+     * @param application Indicates the main process.
+     * @param record current running ability record
+     * @param ability current uiability object.
+     * @param handler the UIability EventHandler object
+     * @param token the remote token
+     */
+    void Init(const std::shared_ptr<AppExecFwk::OHOSApplication> &application,
+        const std::shared_ptr<AppExecFwk::AbilityLocalRecord> &record, std::shared_ptr<UIAbility> &ability,
+        std::shared_ptr<AppExecFwk::AbilityHandler> &handler, const sptr<IRemoteObject> &token);
+
+    /**
+     * @brief Prepare terminate the ability.
+     * @return Return true if ability need be terminated; return false if ability need stop terminating.
+     */
+    bool PrepareTerminateAbility();
+
+    /**
+     * @brief Save data and states of an ability when it is restored by the system. and Calling information back to
+     * Ability. This method should be implemented by a Page ability.
+     */
+    void DispatchSaveAbilityState();
+
+    /**
+     * @brief Restores data and states of an ability when it is restored by the system. and Calling information back
+     * to Ability. This method should be implemented by a Page ability.
+     * @param instate The Want object to connect to.
+     */
+    void DispatchRestoreAbilityState(const AppExecFwk::PacMap &inState);
+
+    /**
+     * @brief Page Service Ability has different AbilityTransaction
+     * @param want Indicates the Want containing information about the ability.
+     * @param targetState The life cycle state to switch to.
+     * @param sessionInfo Indicates the sessionInfo
+     */
+    void HandleAbilityTransaction(const AAFwk::Want &want, const AAFwk::LifeCycleStateInfo &targetState,
+        sptr<AppExecFwk::SessionInfo> sessionInfo = nullptr);
+
+    /**
+     * @brief The life cycle callback.
+     * @param state The life cycle state to switch to.
+     */
+    void AbilityTransactionCallback(const AAFwk::AbilityLifeCycleState &state);
+
+    /**
+     * @brief Handling the life cycle switching of NewAbility in switch.
+     * @param want Indicates the structure containing information about the ability.
+     * @param targetState The life cycle state to switch to.
+     * @return return true if the lifecycle transaction successfully, otherwise return false.
+     */
+    bool AbilityTransaction(const AAFwk::Want &want, const AAFwk::LifeCycleStateInfo &targetState);
+
+    /**
+     * @brief Send the result code and data to be returned by this Page ability to the caller.
+     * When a Page ability is destroyed, the caller overrides the AbilitySlice#onAbilityResult(int, int, Want)
+     * method to receive the result set in the current method. This method can be called only after the ability has
+     * been initialized.
+     * @param requestCode Indicates the request code.
+     * @param resultCode Indicates the result code returned after the ability is destroyed. You can define the
+     * result code to identify an error.
+     * @param resultData Indicates the data returned after the ability is destroyed. You can define the data
+     * returned. This parameter can be null.
+     */
+    void SendResult(int requestCode, int resultCode, const AAFwk::Want &resultData);
+
+    /**
+     * @brief Called when the launch mode of an ability is set to singleInstance. This happens when you re-launch
+     * an ability that has been at the top of the ability stack.
+     * @param want Indicates the new Want containing information about the ability.
+     */
+    void NewWant(const AAFwk::Want &want);
+
+    /**
+     * @brief Set deviceId/bundleName/abilityName of the calling ability
+     * @param deviceId deviceId of the calling ability
+     * @param bundleName bundleName of the calling ability
+     * @param abilityName abilityName of the calling ability
+     * @param moduleName moduleName of the calling ability
+     */
+    void SetCallingContext(const std::string &deviceId, const std::string &bundleName, const std::string &abilityName,
+        const std::string &moduleName);
+
+    /**
+     * @brief ScheduleUpdateConfiguration, scheduling update configuration.
+     */
+    void ScheduleUpdateConfiguration(const AppExecFwk::Configuration &config);
+
+    /**
+     * @brief continue ability to target device.
+     * @param deviceId target deviceId
+     * @param versionCode Target bundle version.
+     */
+    void ContinueAbility(const std::string &deviceId, uint32_t versionCode);
+
+    /**
+     * @brief Notify continuation result to ability.
+     * @param result Continuaton result.
+     */
+    void NotifyContinuationResult(int32_t result);
+
+    /**
+     * @brief Notify current memory level to ability.
+     * @param level Current memory level.
+     */
+    void NotifyMemoryLevel(int32_t level);
+
+    /**
+     * @brief Provide operating system ShareData information to the observer
+     * @param uniqueId Indicates the Id of request
+     */
+    void HandleShareData(int32_t uniqueId);
+
+#ifdef SUPPORT_GRAPHICS
+public:
+
+    /**
+     * @brief Notify caller that window is UnFocused.
+     */
+    void AfterUnFocused();
+
+    /**
+     * @brief Notify caller that window is Focused.
+     */
+    void AfterFocused();
+
+protected:
+    /**
+     * @brief Toggles the lifecycle status of Ability to AAFwk::ABILITY_STATE_INACTIVE. And notifies the application
+     * that it belongs to of the lifecycle status.
+     * @param want The Want object to switch the life cycle.
+     */
+    void Foreground(const AAFwk::Want &want);
+
+    /**
+     * @brief Toggles the lifecycle status of Ability to AAFwk::ABILITY_STATE_BACKGROUND. And notifies the
+     * application that it belongs to of the lifecycle status.
+     */
+    void Background();
+#endif
+
+protected:
+    void Start(const AAFwk::Want &want, sptr<AppExecFwk::SessionInfo> sessionInfo = nullptr);
+    void Stop();
+    void Stop(bool &isAsyncCallback);
+    void StopCallback();
+    void SetLifeCycleStateInfo(const AAFwk::LifeCycleStateInfo &info);
+    bool CheckAndRestore();
+    int32_t Share(AAFwk::WantParams &wantParam);
+    AppExecFwk::PacMap &GetRestoreData();
+
+    bool isStageBasedModel_ = false;
+    int32_t lifecycleState_ = AAFwk::ABILITY_STATE_INITIAL;
+    sptr<IRemoteObject> token_;
+    std::shared_ptr<UIAbility> ability_;
+    std::shared_ptr<AppExecFwk::AbilityHandler> handler_;
+    bool notifyForegroundByWindow_ = false;
+    bool notifyForegroundByAbility_ = false;
+    std::mutex notifyForegroundLock_;
+
+private:
+    void AfterFocusedCommon(bool isFocused);
+
+    std::shared_ptr<AppExecFwk::AbilityLifecycleCallbacks> abilityLifecycleCallbacks_;
+    bool hasSaveData_ = false;
+    bool needSaveDate_ = false;
+    AppExecFwk::PacMap restoreData_;
+
+#ifdef SUPPORT_GRAPHICS
+private:
+    void HandleForegroundNewState(const AAFwk::Want &want, bool &bFlag);
+
+    class WindowLifeCycleImpl : public Rosen::IWindowLifeCycle {
+    public:
+        WindowLifeCycleImpl(const sptr<IRemoteObject> &token, const std::shared_ptr<UIAbilityImpl> &owner)
+            : token_(token), owner_(owner)
+        {}
+        virtual ~WindowLifeCycleImpl() {}
+        void AfterForeground() override;
+        void AfterBackground() override;
+        void AfterFocused() override;
+        void AfterUnfocused() override;
+        void ForegroundFailed(int32_t type) override;
+        void BackgroundFailed(int32_t type) override;
+
+    private:
+        sptr<IRemoteObject> token_ = nullptr;
+        std::weak_ptr<UIAbilityImpl> owner_;
+    };
+#endif
+};
+} // namespace AbilityRuntime
+} // namespace OHOS
+#endif // OHOS_ABILITY_RUNTIME_UI_ABILITY_IMPL_H
---
+++ interfaces/kits/native/ability/native/ui_ability_thread.h
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_ABILITY_RUNTIME_UI_ABILITY_THREAD_H
+#define OHOS_ABILITY_RUNTIME_UI_ABILITY_THREAD_H
+
+#include "ability_thread.h"
+#include "context.h"
+#include "ui_ability.h"
+#include "ui_ability_impl.h"
+
+namespace OHOS {
+namespace AppExecFwk {
+class AbilityLocalRecord;
+class OHOSApplication;
+} // namespace AppExecFwk
+namespace AbilityRuntime {
+using LifeCycleStateInfo = OHOS::AAFwk::LifeCycleStateInfo;
+class UIAbilityThread : public AppExecFwk::AbilityThread {
+public:
+    /**
+     * @brief Default constructor used to create a UIAbilityThread instance.
+     */
+    UIAbilityThread();
+    ~UIAbilityThread() override;
+
+    /**
+     * @brief Attach The ability thread to the main process.
+     * @param application Indicates the main process.
+     * @param abilityRecord current running ability record
+     * @param mainRunner The runner which main_thread holds.
+     * @param appContext the AbilityRuntime context
+     */
+    void Attach(const std::shared_ptr<AppExecFwk::OHOSApplication> &application,
+        const std::shared_ptr<AppExecFwk::AbilityLocalRecord> &abilityRecord,
+        const std::shared_ptr<AppExecFwk::EventRunner> &mainRunner,
+        const std::shared_ptr<Context> &appContext) override;
+
+    /**
+     * @brief Attach The ability thread to the main process.
+     * @param application Indicates the main process.
+     * @param abilityRecord current running ability record
+     * @param appContext the AbilityRuntime context
+     */
+    void Attach(const std::shared_ptr<AppExecFwk::OHOSApplication> &application,
+        const std::shared_ptr<AppExecFwk::AbilityLocalRecord> &abilityRecord,
+        const std::shared_ptr<Context> &appContext) override;
+
+    /**
+     * @brief ScheduleUpdateConfiguration, scheduling update configuration.
+     * @param config Indicates the updated configuration information.
+     */
+    void ScheduleUpdateConfiguration(const AppExecFwk::Configuration &config) override;
+
+    /**
+     * @brief notify this ability current memory level.
+     * @param level Current memory level
+     */
+    void NotifyMemoryLevel(int32_t level) override;
+
+    /**
+     * @brief Provide operating system AbilityTransaction information to the observer
+     * @param want Indicates the structure containing Transaction information about the ability.
+     * @param targetState Indicates the lifecycle state.
+     * @param sessionInfo Indicates the session info.
+     */
+    void ScheduleAbilityTransaction(const Want &want, const LifeCycleStateInfo &targetState,
+        sptr<AppExecFwk::SessionInfo> sessionInfo = nullptr) override;
+
+    /**
+     * @brief Provide operating system ShareData information to the observer
+     * @param requestCode Indicates the Ability request code.
+     */
+    void ScheduleShareData(const int32_t &requestCode) override;
+
+    /**
+     * @brief Provide operating system PrepareTerminateAbility information to the observer
+     */
+    bool SchedulePrepareTerminateAbility() override;
+
+    /**
+     * @brief Provide operating system SaveabilityState information to the observer
+     */
+    void ScheduleSaveAbilityState() override;
+
+    /**
+     * @brief Provide operating system RestoreAbilityState information to the observer
+     * @param state Indicates resotre ability state used to dispatchRestoreAbilityState.
+     */
+    void ScheduleRestoreAbilityState(const AppExecFwk::PacMap &state) override;
+
+    /**
+     * @brief Send the result code and data to be returned by this Page ability to the caller.
+     * When a Page ability is destroyed, the caller overrides the AbilitySlice#onAbilityResult(int, int, Want) method to
+     * receive the result set in the current method. This method can be called only after the ability has been
+     * initialized.
+     * @param requestCode Indicates the request code for send.
+     * @param resultCode Indicates the result code returned after the ability is destroyed. You can define the result
+     * code to identify an error.
+     * @param want Indicates the data returned after the ability is destroyed. You can define the data returned. This
+     * parameter can be null.
+     */
+    void SendResult(int requestCode, int resultCode, const Want &resultData) override;
+
+    /**
+     * @brief continue ability to target device.
+     * @param deviceId target deviceId
+     * @param versionCode Target bundle version.
+     */
+    void ContinueAbility(const std::string &deviceId, uint32_t versionCode) override;
+
+    /**
+     * @brief notify this ability continuation result.
+     * @param result Continuation result
+     */
+    void NotifyContinuationResult(int32_t result) override;
+
+    /**
+     * @brief Dump ability runner info.
+     * @param runnerInfo ability runner info.
+     */
+    void DumpAbilityInfo(const std::vector<std::string> &params, std::vector<std::string> &info) override;
+
+    /**
+     * @brief Call Request
+     */
+    void CallRequest() override;
+
+private:
+    void DumpAbilityInfoInner(const std::vector<std::string> &params, std::vector<std::string> &info);
+    void DumpOtherInfo(std::vector<std::string> &info);
+    void AttachInner(const std::shared_ptr<AppExecFwk::OHOSApplication> &application,
+        const std::shared_ptr<AppExecFwk::AbilityLocalRecord> &abilityRecord,
+        const std::shared_ptr<Context> &stageContext);
+    std::string CreateAbilityName(const std::shared_ptr<AppExecFwk::AbilityLocalRecord> &abilityRecord);
+    std::shared_ptr<AppExecFwk::ContextDeal> CreateAndInitContextDeal(
+        const std::shared_ptr<AppExecFwk::OHOSApplication> &application,
+        const std::shared_ptr<AppExecFwk::AbilityLocalRecord> &abilityRecord,
+        const std::shared_ptr<AppExecFwk::AbilityContext> &abilityObject);
+    std::shared_ptr<AbilityContext> BuildAbilityContext(
+        const std::shared_ptr<AppExecFwk::AbilityInfo> &abilityInfo,
+        const std::shared_ptr<AppExecFwk::OHOSApplication> &application, const sptr<IRemoteObject> &token,
+        const std::shared_ptr<AbilityRuntime::Context> &stageContext);
+    void HandleAbilityTransaction(const Want &want, const LifeCycleStateInfo &lifeCycleStateInfo,
+        sptr<AppExecFwk::SessionInfo> sessionInfo = nullptr);
+    void HandleShareData(const int32_t &requestCode);
+    void HandlePrepareTermianteAbility();
+    void HandleUpdateConfiguration(const AppExecFwk::Configuration &config);
+
+    std::shared_ptr<UIAbilityImpl> abilityImpl_ = nullptr;
+    std::shared_ptr<UIAbility> currentAbility_ = nullptr;
+    bool isPrepareTerminate_ = false;
+    std::atomic_bool isPrepareTerminateAbilityDone_ = false;
+    std::mutex mutex_;
+    std::condition_variable cv_;
+};
+} // namespace AbilityRuntime
+} // namespace OHOS
+#endif // OHOS_ABILITY_RUNTIME_UI_ABILITY_THREAD_H
