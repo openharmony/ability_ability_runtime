3a9080567bd23f243c9a5b1356c3f73e7fa10db5
xinking129 <xinxin13@huawei.com>
2023-09-27 09:56:34 +0800

    attach UT code first
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- test/unittest/app_lifecycle_deal_test/app_lifecycle_deal_test.cpp
+++ test/unittest/app_lifecycle_deal_test/app_lifecycle_deal_test.cpp
@@ -72,5 +72,61 @@ HWTEST_F(AppLifecycleDealTest, NotifyAppFault_002, TestSize.Level1)
     auto retsult = appLifeCycle->NotifyAppFault(faultData);
     EXPECT_EQ(ERR_OK, retsult);
 }
+
+/**
+ * @tc.name: AttachAppDebug_001
+ * @tc.desc: Test the normal state of AttachAppDebug
+ * @tc.type: FUNC
+ */
+HWTEST_F(AppLifecycleDealTest, AttachAppDebug_001, TestSize.Level1)
+{
+    auto appLifeCycle = std::make_shared<AppLifeCycleDeal>();
+    EXPECT_NE(appLifeCycle, nullptr);
+    auto result = appLifeCycle->AttachAppDebug();
+    EXPECT_EQ(result, ERR_INVALID_VALUE);
+}
+
+/**
+ * @tc.name: AttachAppDebug_002
+ * @tc.desc: Test the abnormal state of AttachAppDebug
+ * @tc.type: FUNC
+ */
+HWTEST_F(AppLifecycleDealTest, AttachAppDebug_002, TestSize.Level1)
+{
+    auto appLifeCycle = std::make_shared<AppLifeCycleDeal>();
+    EXPECT_NE(appLifeCycle, nullptr);
+    sptr<MockAppScheduler> mockAppScheduler = new (std::nothrow) MockAppScheduler();
+    appLifeCycle->SetApplicationClient(mockAppScheduler);
+    auto result = appLifeCycle->AttachAppDebug();
+    EXPECT_EQ(ERR_OK, result);
+}
+
+/**
+ * @tc.name: DetachAppDebug_001
+ * @tc.desc: Test the abnormal state of DetachAppDebug
+ * @tc.type: FUNC
+ */
+HWTEST_F(AppLifecycleDealTest, DetachAppDebug_001, TestSize.Level1)
+{
+    auto appLifeCycle = std::make_shared<AppLifeCycleDeal>();
+    EXPECT_NE(appLifeCycle, nullptr);
+    auto result = appLifeCycle->DetachAppDebug();
+    EXPECT_EQ(result, ERR_INVALID_VALUE);
+}
+
+/**
+ * @tc.name: DetachAppDebug_002
+ * @tc.desc: Test the normal state of DetachAppDebug
+ * @tc.type: FUNC
+ */
+HWTEST_F(AppLifecycleDealTest, DetachAppDebug_002, TestSize.Level1)
+{
+    auto appLifeCycle = std::make_shared<AppLifeCycleDeal>();
+    EXPECT_NE(appLifeCycle, nullptr);
+    sptr<MockAppScheduler> mockAppScheduler = new (std::nothrow) MockAppScheduler();
+    appLifeCycle->SetApplicationClient(mockAppScheduler);
+    auto result = appLifeCycle->DetachAppDebug();
+    EXPECT_EQ(ERR_OK, result);
+}
 } // namespace AppExecFwk
 } // namespace OHOS
