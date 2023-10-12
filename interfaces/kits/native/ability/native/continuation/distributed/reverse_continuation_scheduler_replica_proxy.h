d105c0901bef40b87559360f1899573bb57af674
Baisong Zhong <zhongbaisong@huawei.com>
2023-10-12 11:20:29 +0000

    元能力代码仓对Remote()->SendRequest()调用提供封装接口
    Signed-off-by: Baisong Zhong <zhongbaisong@huawei.com>
    Change-Id: Ib58053f3f66dcb8413bf1d7b8225410d29016db3

--- interfaces/kits/native/ability/native/continuation/distributed/reverse_continuation_scheduler_replica_proxy.h
+++ interfaces/kits/native/ability/native/continuation/distributed/reverse_continuation_scheduler_replica_proxy.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -36,6 +36,9 @@ public:
     virtual void NotifyReverseResult(int reverseResult) override;
 
     virtual sptr<IRemoteObject> AsObject() override;
+
+private:
+    int32_t SendTransactCmd(uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option);
 };
 }  // namespace AppExecFwk
 }  // namespace OHOS
