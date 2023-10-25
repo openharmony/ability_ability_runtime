8ee1b7379a0492092cb446b3ef05392b9f8ecb1b
xia <xialiangwei1@huawei.com>
2023-10-25 15:38:58 +0800

    修复UIExtension前后台切换问题
    Signed-off-by: xia <xialiangwei1@huawei.com>

--- interfaces/inner_api/app_manager/include/appmgr/app_mgr_constants.h
+++ interfaces/inner_api/app_manager/include/appmgr/app_mgr_constants.h
@@ -32,6 +32,12 @@ enum class ApplicationState {
     APP_STATE_END,
 };
 
+enum class ApplicationPendingState {
+    READY = 0,
+    FOREGROUNDING,
+    BACKGROUNDING,
+};
+
 enum class AbilityState {
     ABILITY_STATE_CREATE = 0,
     ABILITY_STATE_READY,
