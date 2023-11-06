13c84bb49480a2ed6c119efc9124162be377c62e
kunge-hub <likunhui@huawei.com>
2023-11-06 09:17:54 +0000

    if implicit start ability couldn't match any application, no tip dialog will be pulled up.
    Signed-off-by: kunge-hub <likunhui@huawei.com>
    
    if implicit start ability couldn't match any application, no tip dialog will be pulled up.
    
    Signed-off-by: kunge-hub <likunhui@huawei.com>

--- services/abilitymgr/src/implicit_start_processor.cpp
+++ services/abilitymgr/src/implicit_start_processor.cpp
@@ -109,6 +109,10 @@ int ImplicitStartProcessor::ImplicitStartAbility(AbilityRequest &request, int32_
     AAFwk::Want want;
     auto abilityMgr = DelayedSingleton<AbilityManagerService>::GetInstance();
     if (dialogAppInfos.size() == 0 && (deviceType == STR_PHONE || deviceType == STR_DEFAULT)) {
+        if ((request.want.GetFlags() & Want::FLAG_START_WITHOUT_TIPS) == Want::FLAG_START_WITHOUT_TIPS) {
+            HILOG_INFO("hint dialog doesn't generate.");
+            return ERR_IMPLICIT_START_ABILITY_FAIL;
+        }
         HILOG_ERROR("implicit query ability infos failed, show tips dialog.");
         want = sysDialogScheduler->GetTipsDialogWant(request.callerToken);
         abilityMgr->StartAbility(want);
@@ -122,6 +126,10 @@ int ImplicitStartProcessor::ImplicitStartAbility(AbilityRequest &request, int32_
             return ret;
         }
         if (dialogAllAppInfos.size() == 0) {
+            if ((request.want.GetFlags() & Want::FLAG_START_WITHOUT_TIPS) == Want::FLAG_START_WITHOUT_TIPS) {
+                HILOG_INFO("hint dialog doesn't generate.");
+                return ERR_IMPLICIT_START_ABILITY_FAIL;
+            }
             Want dialogWant = sysDialogScheduler->GetTipsDialogWant(request.callerToken);
             abilityMgr->StartAbility(dialogWant);
             return ERR_IMPLICIT_START_ABILITY_FAIL;
