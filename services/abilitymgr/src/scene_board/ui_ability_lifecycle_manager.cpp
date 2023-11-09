a445040d6bff20fd4798d25ef2816986dcddf1be
yu <zhangyusi3@huawei.com>
2023-11-09 13:27:49 +0000

    exception when foreground failed
    Signed-off-by: yu <zhangyusi3@huawei.com>
    Change-Id: I58ee223deb0443ea2ea036132889037573c9cb5c

--- services/abilitymgr/src/scene_board/ui_ability_lifecycle_manager.cpp
+++ services/abilitymgr/src/scene_board/ui_ability_lifecycle_manager.cpp
@@ -414,6 +414,9 @@ void UIAbilityLifecycleManager::HandleForegroundFailed(const std::shared_ptr<Abi
         return;
     }
 
+    NotifySCBToHandleException(ability,
+        static_cast<int32_t>(ErrorLifecycleState::ABILITY_STATE_LOAD_TIMEOUT), "handleForegroundTimeout");
+
     EraseAbilityRecord(ability);
     // foreground failed, notify appMs force terminate the ability
     DelayedSingleton<AppScheduler>::GetInstance()->AttachTimeOut(ability->GetToken());
