c40e1906792691a1f77dac6a9fc9de256dc919b5
duansizhao <duansizhao@huawei.com>
2023-11-13 03:08:08 +0000

    displayId常量改为GetDefaultDisplayId()获取
    Signed-off-by: duansizhao <duansizhao@huawei.com>
    Change-Id: Ifff9de0f3917560481d3b7f4a61abcf720a1b193

--- frameworks/native/ability/native/ability.cpp
+++ frameworks/native/ability/native/ability.cpp
@@ -175,7 +175,7 @@ void Ability::OnStart(const Want &want, sptr<AAFwk::SessionInfo> sessionInfo)
     HILOG_INFO("AbilityName is %{public}s.", abilityInfo_->name.c_str());
 #ifdef SUPPORT_GRAPHICS
     if (abilityInfo_->type == AppExecFwk::AbilityType::PAGE) {
-        int defualtDisplayId = Rosen::WindowScene::DEFAULT_DISPLAY_ID;
+        int defualtDisplayId =  Rosen::DisplayManager::GetInstance().GetDefaultDisplayId();
         int displayId = want.GetIntParam(Want::PARAM_RESV_DISPLAY_ID, defualtDisplayId);
         HILOG_DEBUG("abilityName:%{public}s, displayId:%{public}d", abilityInfo_->name.c_str(), displayId);
         if (!abilityInfo_->isStageBasedModel) {
