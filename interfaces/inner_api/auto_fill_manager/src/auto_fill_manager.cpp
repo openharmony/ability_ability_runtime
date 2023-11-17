17ec6684d6dad863ca7ac72251b92c770c090fef
xinking129 <xinxin13@huawei.com>
2023-11-17 15:32:48 +0800

    auto fill code 3
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- interfaces/inner_api/auto_fill_manager/src/auto_fill_manager.cpp
+++ interfaces/inner_api/auto_fill_manager/src/auto_fill_manager.cpp
@@ -16,9 +16,24 @@
 #include "auto_fill_manager.h"
 
 #include "auto_fill_error.h"
+#include "auto_fill_extension_callback.h"
+#include "extension_ability_info.h"
+#include "hilog_wrapper.h"
 
 namespace OHOS {
 namespace AbilityRuntime {
+namespace {
+const std::string WANT_PARAMS_AUTO_FILL_CMD = "fill";
+const std::string WANT_PARAMS_AUTO_SAVE_CMD = "save";
+const std::string WANT_PARAMS_EXTENSION_TYPE = "autoFill/password";
+constexpr static char AUTO_FILL_BUNDLE_NAME[] = "com.ohos.passwordbox";
+constexpr static char AUTO_FILL_MODULE_NAME[] = "entry";
+constexpr static char AUTO_FILL_ABILITY_NAME[] = "AutoFillAbility";
+constexpr static char WANT_PARAMS_VIEW_DATA_KEY[] = "ohos.ability.params.viewData";
+constexpr static char WANT_PARAMS_AUTO_FILL_CMD_KEY[] = "ohos.ability.params.autoFillCmd";
+constexpr static char WANT_PARAMS_EXTENSION_TYPE_KEY[] = "ability.want.params.uiExtensionType";
+constexpr static char WANT_PARAMS_AUTO_FILL_TYPE_KEY[] = "ability.want.params.AutoFillType";
+} // namespace
 AutoFillManager &AutoFillManager::GetInstance()
 {
     static AutoFillManager instance;
@@ -31,7 +46,44 @@ int32_t AutoFillManager::RequestAutoFill(
     const AbilityBase::ViewData &viewdata,
     const std::shared_ptr<IFillRequestCallback> &fillCallback)
 {
-    return AutoFiil::AUTO_FILL_OBJECT_IS_NULL;
+    HILOG_DEBUG("Called.");
+    if (uiContent == nullptr || fillCallback == nullptr) {
+        HILOG_ERROR("UIContent or fillCallback is nullptr.");
+        return AutoFiil::AUTO_FILL_OBJECT_IS_NULL;
+    }
+
+    AAFwk::Want want;
+    want.SetElementName(AUTO_FILL_BUNDLE_NAME, AUTO_FILL_ABILITY_NAME);
+    want.SetModuleName(AUTO_FILL_MODULE_NAME);
+    want.SetParam(WANT_PARAMS_EXTENSION_TYPE_KEY, WANT_PARAMS_EXTENSION_TYPE);
+    want.SetParam(WANT_PARAMS_AUTO_FILL_TYPE_KEY, static_cast<int32_t>(autoFillType));
+    want.SetParam(WANT_PARAMS_AUTO_FILL_CMD_KEY, WANT_PARAMS_AUTO_FILL_CMD);
+    want.SetParam(WANT_PARAMS_VIEW_DATA_KEY, viewdata.ToJsonString());
+
+    auto extensionCallback = std::make_shared<AutoFillExtensionCallback>();
+    if (extensionCallback == nullptr) {
+        HILOG_ERROR("Extension callback is nullptr.");
+        return AutoFiil::AUTO_FILL_OBJECT_IS_NULL;
+    }
+    extensionCallback->SetFillRequestCallback(fillCallback);
+
+    Ace::ModalUIExtensionCallbacks callback;
+    callback.onResult = std::bind(
+        &AutoFillExtensionCallback::OnResult, extensionCallback, std::placeholders::_1, std::placeholders::_2);
+    callback.onRelease = std::bind(
+        &AutoFillExtensionCallback::OnRelease, extensionCallback, std::placeholders::_1);
+    callback.onError = std::bind(&AutoFillExtensionCallback::OnError,
+        extensionCallback, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+
+    Ace::ModalUIExtensionConfig config;
+    int32_t sessionId = uiContent->CreateModalUIExtension(want, callback, config);
+    if (sessionId == 0) {
+        HILOG_ERROR("Create modal ui extension is failed.");
+        return AutoFiil::AUTO_FILL_CREATE_MODULE_UI_EXTENSION_FAILED;
+    }
+    extensionCallback->SetUIContent(uiContent);
+    extensionCallback->SetSessionId(sessionId);
+    return AutoFiil::AUTO_FILL_SUCCESS;
 }
 
 int32_t AutoFillManager::RequestAutoSave(
@@ -39,7 +91,43 @@ int32_t AutoFillManager::RequestAutoSave(
     const AbilityBase::ViewData &viewdata,
     const std::shared_ptr<ISaveRequestCallback> &saveCallback)
 {
-    return AutoFiil::AUTO_FILL_OBJECT_IS_NULL;
+    HILOG_DEBUG("Called.");
+    if (uiContent == nullptr || saveCallback == nullptr) {
+        HILOG_ERROR("UIContent or save callback is nullptr.");
+        return AutoFiil::AUTO_FILL_OBJECT_IS_NULL;
+    }
+
+    AAFwk::Want want;
+    want.SetElementName(AUTO_FILL_BUNDLE_NAME, AUTO_FILL_ABILITY_NAME);
+    want.SetModuleName(AUTO_FILL_MODULE_NAME);
+    want.SetParam(WANT_PARAMS_EXTENSION_TYPE_KEY, WANT_PARAMS_EXTENSION_TYPE);
+    want.SetParam(WANT_PARAMS_AUTO_FILL_CMD_KEY, WANT_PARAMS_AUTO_SAVE_CMD);
+    want.SetParam(WANT_PARAMS_VIEW_DATA_KEY, viewdata.ToJsonString());
+
+    auto extensionCallback = std::make_shared<AutoFillExtensionCallback>();
+    if (extensionCallback == nullptr) {
+        HILOG_ERROR("Extension callback is nullptr.");
+        return AutoFiil::AUTO_FILL_OBJECT_IS_NULL;
+    }
+    extensionCallback->SetSaveRequestCallback(saveCallback);
+
+    Ace::ModalUIExtensionCallbacks callback;
+    callback.onResult = std::bind(
+        &AutoFillExtensionCallback::OnResult, extensionCallback, std::placeholders::_1, std::placeholders::_2);
+    callback.onRelease = std::bind(
+        &AutoFillExtensionCallback::OnRelease, extensionCallback, std::placeholders::_1);
+    callback.onError = std::bind(&AutoFillExtensionCallback::OnError,
+        extensionCallback, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+
+    Ace::ModalUIExtensionConfig config;
+    int32_t sessionId = uiContent->CreateModalUIExtension(want, callback, config);
+    if (sessionId == 0) {
+        HILOG_ERROR("Create modal ui extension is failed.");
+        return AutoFiil::AUTO_FILL_CREATE_MODULE_UI_EXTENSION_FAILED;
+    }
+    extensionCallback->SetUIContent(uiContent);
+    extensionCallback->SetSessionId(sessionId);
+    return AutoFiil::AUTO_FILL_SUCCESS;
 }
 } // namespace AbilityRuntime
 } // namespace OHOS
