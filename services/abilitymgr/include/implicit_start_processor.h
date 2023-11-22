5106bb01b408c015757fb5add6ff3f00f5d83d9c
danshenwudi <zlds123123@126.com>
2023-11-22 01:03:11 +0000

    NFC查询弹窗列表
    Signed-off-by: danshenwudi <zlds123123@126.com>
    Change-Id: Ib52a0d10ca1ebdce1c89cf823adf3045bbd4f08f

--- services/abilitymgr/include/implicit_start_processor.h
+++ services/abilitymgr/include/implicit_start_processor.h
@@ -50,6 +50,8 @@ private:
         std::vector<DialogAppInfo> &dialogAppInfos, std::string &deviceType, bool isMoreHapList);
     std::string MatchTypeAndUri(const AAFwk::Want &want);
     sptr<AppExecFwk::IBundleMgr> GetBundleManager();
+    std::vector<std::string> splitStr(const std::string& str, char delimiter);
+    int queryBmsAppInfos(AbilityRequest &request, int32_t userId, std::vector<DialogAppInfo> &dialogAppInfos);
 
     using StartAbilityClosure = std::function<int32_t()>;
     int CallStartAbilityInner(int32_t userId, const Want &want, const StartAbilityClosure &callBack,
