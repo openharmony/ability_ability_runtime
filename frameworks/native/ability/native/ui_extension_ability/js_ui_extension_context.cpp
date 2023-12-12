clipsheep6/ability_runtime

新增Embeddable_ui_ability_context
Signed-off-by: q30043944 <qinlong18@huawei.com>

frameworks/native/ability/native/ui_extension_ability/js_ui_extension_context.cpp
@@ -43,7 +43,7 @@ constexpr size_t ARGC_ONE = 1;
 constexpr size_t ARGC_TWO = 2;
 } // namespace
 
-static std::map<ConnectionKey, sptr<JSUIExtensionConnection>, key_compare> g_connects;
+static std::map<ExtensionConnectionKey, sptr<JSUIExtensionConnection>, key_compare> g_connects;
 static int64_t g_serialNumber = 0;
 void RemoveConnection(int64_t connectId)
 {
@@ -86,7 +86,7 @@ bool CheckConnectionParam(napi_env env, napi_value value, sptr<JSUIExtensionConn
         return false;
     }
     connection->SetJsConnectionObject(value);
-    ConnectionKey key;
+    ExtensionConnectionKey key;
     key.id = g_serialNumber;
     key.want = want;
     connection->SetConnectionId(key.id);

openharmony/ability_ability_runtime

frameworks/native/ability/native/ui_extension_ability/js_ui_extension_context.cpp
@@ -43,7 +43,7 @@ constexpr size_t ARGC_ONE = 1;
 constexpr size_t ARGC_TWO = 2;
 } // namespace
 
-static std::map<ConnectionKey, sptr<JSUIExtensionConnection>, key_compare> g_connects;
+static std::map<UIExtensionConnectionKey, sptr<JSUIExtensionConnection>, key_compare> g_connects;
 static int64_t g_serialNumber = 0;
 void RemoveConnection(int64_t connectId)
 {
@@ -86,7 +86,7 @@ bool CheckConnectionParam(napi_env env, napi_value value, sptr<JSUIExtensionConn
         return false;
     }
     connection->SetJsConnectionObject(value);
-    ConnectionKey key;
+    UIExtensionConnectionKey key;
     key.id = g_serialNumber;
     key.want = want;
     connection->SetConnectionId(key.id);