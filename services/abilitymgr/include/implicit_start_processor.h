608bd0c7b791d6ff9c653542989b05b10539d2cd
danshenwudi <zlds123123@126.com>
2023-12-19 08:56:47 +0000

    修改NFC查询bms接口方式/NFC单框架查询
    Signed-off-by: danshenwudi <zlds123123@126.com>
    Change-Id: I8f09058f4642aa67f2a19edb164e91211ff5beeb

--- services/abilitymgr/include/implicit_start_processor.h
+++ services/abilitymgr/include/implicit_start_processor.h
@@ -73,8 +73,8 @@ private:
         std::vector<DialogAppInfo> &dialogAppInfos, std::string &deviceType, bool isMoreHapList);
     std::string MatchTypeAndUri(const AAFwk::Want &want);
     std::shared_ptr<AppExecFwk::BundleMgrHelper> GetBundleManagerHelper();
-    std::vector<std::string> splitStr(const std::string& str, char delimiter);
-    int queryBmsAppInfos(AbilityRequest &request, int32_t userId, std::vector<DialogAppInfo> &dialogAppInfos);
+    std::vector<std::string> SplitStr(const std::string& str, char delimiter);
+    int QueryBmsAppInfos(AbilityRequest &request, int32_t userId, std::vector<DialogAppInfo> &dialogAppInfos);
 
     using StartAbilityClosure = std::function<int32_t()>;
     int CallStartAbilityInner(int32_t userId, const Want &want, const StartAbilityClosure &callBack,
