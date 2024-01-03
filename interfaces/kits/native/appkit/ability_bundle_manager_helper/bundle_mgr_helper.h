433747f2ba0ce0047183f5af7aae974cfaf5d359
savior-xzh <xuzheheng2@huawei.com>
2024-01-03 06:36:49 +0000

    bms
    Signed-off-by: savior-xzh <xuzheheng2@huawei.com>
    Change-Id: Icce6595fc638ee1ee7346ff7c5105f9e0d61ce0f

--- interfaces/kits/native/appkit/ability_bundle_manager_helper/bundle_mgr_helper.h
+++ interfaces/kits/native/appkit/ability_bundle_manager_helper/bundle_mgr_helper.h
@@ -58,6 +58,8 @@ public:
     sptr<IAppControlMgr> GetAppControlProxy();
     bool QueryExtensionAbilityInfos(const Want &want, const int32_t &flag, const int32_t &userId,
         std::vector<ExtensionAbilityInfo> &extensionInfos);
+    ErrCode GetBundleInfoV9(
+        const std::string &bundleName, int32_t flags, BundleInfo &bundleInfo, int32_t userId);
     bool GetApplicationInfo(
         const std::string &appName, const ApplicationFlag flag, const int32_t userId, ApplicationInfo &appInfo);
     bool GetApplicationInfo(const std::string &appName, int32_t flags, int32_t userId, ApplicationInfo &appInfo);
