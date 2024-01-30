e90064f1b1ba52cf8e8a287018a4106df5887bc7
savior-xzh <xuzheheng2@huawei.com>
2024-01-30 08:25:54 +0000

    Revert VerifyProcessPermission-1
    Signed-off-by: savior-xzh <xuzheheng2@huawei.com>
    Change-Id: I4b3b67506abf3b3a7c61b91ce56fdd0aa65896ec

--- services/appmgr/src/app_mgr_service_inner.cpp
+++ services/appmgr/src/app_mgr_service_inner.cpp
@@ -3594,6 +3594,10 @@ int AppMgrServiceInner::VerifyProcessPermission() const
         return ERR_OK;
     }
 
+    if (VerifyAPL()) {
+        return ERR_OK;
+    }
+
     auto isCallingPerm = AAFwk::PermissionVerification::GetInstance()->VerifyCallingPermission(
         AAFwk::PermissionConstants::PERMISSION_CLEAN_BACKGROUND_PROCESSES);
     return isCallingPerm ? ERR_OK : ERR_PERMISSION_DENIED;
@@ -3605,6 +3609,10 @@ int AppMgrServiceInner::VerifyProcessPermission(const std::string &bundleName) c
     auto isSaCall = AAFwk::PermissionVerification::GetInstance()->IsSACall();
     auto isShellCall = AAFwk::PermissionVerification::GetInstance()->IsShellCall();
     if (isSaCall || isShellCall) {
+        return ERR_OK;
+    }
+
+    if (VerifyAPL()) {
         return ERR_OK;
     }
 
