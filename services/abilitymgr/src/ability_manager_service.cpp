b4050772aca32613f2855d91dab6c45f907811e1
zhaoleyi <zhaoleyi@huawei.com>
2024-02-05 21:26:04 +0800

    request modal uiextenison add ability type
    Signed-off-by: zhaoleyi <zhaoleyi@huawei.com>
    Change-Id: I6285637f6a7fff1c489a1d3ea5f1c050900e6ee1

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -2104,7 +2104,6 @@ int AbilityManagerService::RequestModalUIExtensionInner(const Want &want)
     // Gets the abilityName, bundleName, modulename corresponding to the current focus appliaction
     EventInfo focusInfo;
     focusInfo.bundleName = record->GetAbilityInfo().bundleName;
-    focusInfo.abilityName = record->GetAbilityInfo().name;
 
     // Gets the abilityName, bundleName, modulename corresponding to the caller appliaction
     EventInfo callerInfo;
@@ -2113,7 +2112,8 @@ int AbilityManagerService::RequestModalUIExtensionInner(const Want &want)
         focusInfo.bundleName.c_str(), callerInfo.bundleName.c_str());
 
     // Compare
-    if (focusInfo.bundleName == callerInfo.bundleName) {
+    if (record->GetAbilityInfo().type == AppExecFwk::AbilityType::PAGE &&
+        focusInfo.bundleName == callerInfo.bundleName) {
         HILOG_DEBUG("CreateModalUIExtension is called!");
         return record->CreateModalUIExtension(want);
     }
