9d2d89b79d4c6e083807f001818342a4b2048456
xd <xiadi@huawei.com>
2024-02-17 18:54:01 +0800

    卡片框架提供可见性查询接口供SA使用
    Signed-off-by: xd <xiadi@huawei.com>

--- test/unittest/frameworks_kits_appkit_native_test/form_extension_context_mock_test.h
+++ test/unittest/frameworks_kits_appkit_native_test/form_extension_context_mock_test.h
@@ -87,6 +87,11 @@ public:
         return 0;
     }
 
+    bool HasFormVisible(const uint32_t tokenId) override
+    {
+        return false;
+    }
+
     int CastTempForm(const int64_t formId, const sptr<IRemoteObject> &callerToken) override
     {
         return 0;
