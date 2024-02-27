3ffc90aab9652b458bab384aa915c733e25c78a6
于文泽 <yuwenze1@huawei.com>
2024-02-27 12:41:16 +0000

    回退 'Pull Request !7684 : 修复后台恶意启动'

--- services/abilitymgr/include/ability_manager_service.h
+++ services/abilitymgr/include/ability_manager_service.h
@@ -1844,8 +1844,6 @@ private:
 
     void WaitBootAnimationStart();
 
-    int CheckAbilityState(bool withContinuousTask, sptr<IRemoteObject> callerToken) const;
-
     constexpr static int REPOLL_TIME_MICRO_SECONDS = 1000000;
     constexpr static int WAITING_BOOT_ANIMATION_TIMER = 5;
 
