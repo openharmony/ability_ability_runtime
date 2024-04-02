e4900652a99a24f81effab97c7fe7c1ed98ce9bd
gongyuechen <gongyuechen@huawei.com>
2024-04-02 09:35:28 +0800

    add lock for abilityResult
    Signed-off-by: gongyuechen <gongyuechen@huawei.com>

--- services/abilitymgr/include/ability_record.h
+++ services/abilitymgr/include/ability_record.h
@@ -1125,6 +1125,7 @@ private:
     ffrt::mutex lock_;
     mutable ffrt::mutex dumpInfoLock_;
     mutable ffrt::mutex dumpLock_;
+    mutable ffrt::mutex resultLock_;
     mutable ffrt::mutex wantLock_;
     mutable ffrt::condition_variable dumpCondition_;
     mutable bool isDumpTimeout_ = false;
