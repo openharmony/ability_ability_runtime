1df28a22106b067d5b5a3f0815a50bc2df3171b9
xinking129 <xinxin13@huawei.com>
2024-04-22 21:02:03 +0800

    add fix close assert fault dialog
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- services/abilitymgr/include/ability_manager_service.h
+++ services/abilitymgr/include/ability_manager_service.h
@@ -2109,6 +2109,8 @@ private:
     bool ParseJsonFromBoot(nlohmann::json jsonObj, const std::string &relativePath,
         const std::string &WHITE_LIST);
 
+    void CloseAssertDialog(const std::string &assertSessionId);
+
 #ifdef BGTASKMGR_CONTINUOUS_TASK_ENABLE
     std::shared_ptr<BackgroundTaskObserver> bgtaskObserver_;
 #endif
