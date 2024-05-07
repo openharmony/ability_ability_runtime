02fbe0e08b790e81435a0646fc8455b49a25abd0
拉不拉卡 <zhaoxiaoting5@huawei.com>
2024-05-07 14:25:31 +0800

    Modifying global variables causes crash issues
    Signed-off-by: 拉不拉卡 <zhaoxiaoting5@huawei.com>

--- services/appdfr/include/appfreeze_manager.h
+++ services/appdfr/include/appfreeze_manager.h
@@ -63,7 +63,7 @@ public:
     int AppfreezeHandle(const FaultData& faultData, const AppfreezeManager::AppInfo& appInfo);
     int AppfreezeHandleWithStack(const FaultData& faultData, const AppfreezeManager::AppInfo& appInfo);
     int LifecycleTimeoutHandle(const ParamInfo& info, std::unique_ptr<FreezeUtil::LifecycleFlow> flow = nullptr);
-    bool WriteToFile(const std::string& fileName, std::string& content);
+    std::string WriteToFile(const std::string& fileName, std::string& content);
     bool IsHandleAppfreeze(const std::string& bundleName);
     bool IsProcessDebug(int32_t pid, std::string processName);
 
