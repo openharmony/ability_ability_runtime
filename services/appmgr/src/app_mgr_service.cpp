f6ff05fed21ee5cf99e23a280bd8512beee35af2
夏良伟 <xialiangwei1@huawei.com>
2024-05-21 15:35:12 +0000

    update services/appmgr/src/app_mgr_service.cpp.
    Signed-off-by: 夏良伟 <xialiangwei1@huawei.com>

--- services/appmgr/src/app_mgr_service.cpp
+++ services/appmgr/src/app_mgr_service.cpp
@@ -1132,7 +1132,7 @@ int32_t AppMgrService::GetBundleNameByPid(const int32_t pid, std::string &bundle
     return appMgrServiceInner_->GetBundleNameByPid(pid, bundleName, uid);
 }
 
-int32_t GetRunningProcessInfoByPid(const pid_t pid, OHOS::AppExecFwk::RunningProcessInfo &info)
+int32_t AppMgrService::GetRunningProcessInfoByPid(const pid_t pid, OHOS::AppExecFwk::RunningProcessInfo &info)
 {
     if (!IsReady()) {
         TAG_LOGE(AAFwkTag::APPMGR, "AppMgrService is not ready.");
