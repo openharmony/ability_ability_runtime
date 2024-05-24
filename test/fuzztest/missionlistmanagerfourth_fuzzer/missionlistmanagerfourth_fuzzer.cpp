b544c931e6e3b7aede83cabfb62a7814d9bcafc1
sunxuhui <sunxuhui7@huawei.com>
2024-05-24 16:35:44 +0800

    add fuzz
    Signed-off-by: sunxuhui <sunxuhui7@huawei.com>

---
+++ test/fuzztest/missionlistmanagerfourth_fuzzer/missionlistmanagerfourth_fuzzer.cpp
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "missionlistmanagerfourth_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#define private public
+#include "mission_info_mgr.h"
+#include "mission_list_manager.h"
+#undef private
+
+#include "ability_record.h"
+
+using namespace OHOS::AAFwk;
+using namespace OHOS::AppExecFwk;
+
+namespace OHOS {
+namespace {
+constexpr size_t FOO_MAX_LEN = 1024;
+constexpr size_t U32_AT_SIZE = 4;
+constexpr uint8_t ENABLE = 2;
+class MyAbilityConnection : public IAbilityConnection {
+public:
+  MyAbilityConnection() = default;
+  virtual ~MyAbilityConnection() = default;
+  void OnAbilityConnectDone(const AppExecFwk::ElementName &element,
+                            const sptr<IRemoteObject> &remoteObject,
+                            int resultCode) override {}
+  void OnAbilityDisconnectDone(const AppExecFwk::ElementName &element,
+                               int resultCode) override {}
+  sptr<IRemoteObject> AsObject() override { return {}; }
+};
+} // namespace
+
+uint32_t GetU32Data(const char *ptr) {
+  // convert fuzz input data to an integer
+  return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
+}
+
+std::shared_ptr<AbilityRecord> GetFuzzAbilityRecord() {
+  sptr<Token> token = nullptr;
+  AbilityRequest abilityRequest;
+  abilityRequest.appInfo.bundleName = "com.example.fuzzTest";
+  abilityRequest.abilityInfo.name = "MainAbility";
+  abilityRequest.abilityInfo.type = AbilityType::DATA;
+  std::shared_ptr<AbilityRecord> abilityRecord =
+      AbilityRecord::CreateAbilityRecord(abilityRequest);
+  if (!abilityRecord) {
+    return nullptr;
+  }
+  return abilityRecord;
+}
+
+sptr<Token> GetFuzzAbilityToken() {
+  sptr<Token> token = nullptr;
+  std::shared_ptr<AbilityRecord> abilityRecord = GetFuzzAbilityRecord();
+  if (abilityRecord) {
+    token = abilityRecord->GetToken();
+  }
+  return token;
+}
+
+bool DoSomethingInterestingWithMyAPI(const char *data, size_t size) {
+  bool boolParam = *data % ENABLE;
+  int intParam = static_cast<int>(GetU32Data(data));
+  Parcel wantParcel;
+  Want *want = nullptr;
+  if (wantParcel.WriteBuffer(data, size)) {
+    want = Want::Unmarshalling(wantParcel);
+    if (!want) {
+      return false;
+    }
+  }
+  std::shared_ptr<AbilityRecord> abilityRecord = GetFuzzAbilityRecord();
+  sptr<IRemoteObject> token = GetFuzzAbilityToken();
+  auto missionListManager = std::make_shared<MissionListManager>(intParam);
+  auto launcherList = std::make_shared<MissionList>(MissionListType::LAUNCHER);
+  missionListManager->launcherList_ = launcherList;
+  missionListManager->defaultStandardList_ =
+      std::make_shared<MissionList>(MissionListType::DEFAULT_STANDARD);
+  missionListManager->defaultSingleList_ =
+      std::make_shared<MissionList>(MissionListType::DEFAULT_SINGLE);
+  missionListManager->currentMissionLists_.push_front(launcherList);
+  if (!missionListManager->listenerController_) {
+    missionListManager->listenerController_ =
+        std::make_shared<MissionListenerController>();
+  }
+  DelayedSingleton<MissionInfoMgr>::GetInstance()->taskDataPersistenceMgr_ =
+      DelayedSingleton<TaskDataPersistenceMgr>::GetInstance();
+  std::shared_ptr<MissionList> missionList = std::make_shared<MissionList>();
+  missionListManager->TerminateAbilityLocked(abilityRecord, boolParam);
+  missionListManager->RemoveTerminatingAbility(abilityRecord, boolParam);
+  missionListManager->RemoveMissionList(missionList);
+  missionListManager->DispatchTerminate(abilityRecord);
+  missionListManager->DelayCompleteTerminate(abilityRecord);
+  missionListManager->CompleteTerminate(abilityRecord);
+  missionListManager->CompleteTerminateAndUpdateMission(abilityRecord);
+  missionListManager->GetAbilityFromTerminateList(token);
+  missionListManager->SetMissionLockedState(intParam, boolParam);
+  if (want) {
+    delete want;
+    want = nullptr;
+  }
+  return true;
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  /* Run your code on data */
+  if (data == nullptr) {
+    return 0;
+  }
+
+  /* Validate the length of size */
+  if (size < OHOS::U32_AT_SIZE || size > OHOS::FOO_MAX_LEN) {
+    return 0;
+  }
+
+  char *ch = (char *)malloc(size + 1);
+  if (ch == nullptr) {
+    std::cout << "malloc failed." << std::endl;
+    return 0;
+  }
+
+  (void)memset_s(ch, size + 1, 0x00, size + 1);
+  if (memcpy_s(ch, size, data, size) != EOK) {
+    std::cout << "copy failed." << std::endl;
+    free(ch);
+    ch = nullptr;
+    return 0;
+  }
+
+  OHOS::DoSomethingInterestingWithMyAPI(ch, size);
+  free(ch);
+  ch = nullptr;
+  return 0;
+}
