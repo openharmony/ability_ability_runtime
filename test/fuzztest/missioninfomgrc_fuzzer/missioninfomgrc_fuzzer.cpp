233070dac7403a39125cfcf7555f3d321c7a98df
孙旭辉 <sunxuhui7@huawei.com>
2024-05-25 06:29:36 +0000

    update test/fuzztest/missioninfomgrc_fuzzer/missioninfomgrc_fuzzer.cpp.
    Signed-off-by: 孙旭辉 <sunxuhui7@huawei.com>

--- test/fuzztest/missioninfomgrc_fuzzer/missioninfomgrc_fuzzer.cpp
+++ test/fuzztest/missioninfomgrc_fuzzer/missioninfomgrc_fuzzer.cpp
@@ -35,81 +35,82 @@ constexpr uint8_t ENABLE = 2;
 } // namespace
 
 uint32_t GetU32Data(const char *ptr) {
-  // convert fuzz input data to an integer
-  return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
+    // convert fuzz input data to an integer
+    return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
 }
 
-sptr<Token> GetFuzzAbilityToken() {
-  sptr<Token> token = nullptr;
-  AbilityRequest abilityRequest;
-  abilityRequest.appInfo.bundleName = "com.example.fuzzTest";
-  abilityRequest.abilityInfo.name = "MainAbility";
-  abilityRequest.abilityInfo.type = AbilityType::DATA;
-  std::shared_ptr<AbilityRecord> abilityRecord =
-      AbilityRecord::CreateAbilityRecord(abilityRequest);
-  if (abilityRecord) {
-    token = abilityRecord->GetToken();
-  }
-  return token;
+sptr<Token> GetFuzzAbilityToken()
+{
+    sptr<Token> token = nullptr;
+    AbilityRequest abilityRequest;
+    abilityRequest.appInfo.bundleName = "com.example.fuzzTest";
+    abilityRequest.abilityInfo.name = "MainAbility";
+    abilityRequest.abilityInfo.type = AbilityType::DATA;
+    std::shared_ptr<AbilityRecord> abilityRecord =
+        AbilityRecord::CreateAbilityRecord(abilityRequest);
+    if (abilityRecord) {
+      token = abilityRecord->GetToken();
+    }
+    return token;
 }
 
 bool DoSomethingInterestingWithMyAPI(const char *data, size_t size) {
-  bool boolParam = *data % ENABLE;
-  int32_t int32Param = static_cast<int32_t>(GetU32Data(data));
-  std::string stringParam(data, size);
-  sptr<IRemoteObject> token = GetFuzzAbilityToken();
-  std::vector<std::string> info;
-  MissionSnapshot missionSnapshot;
+    bool boolParam = *data % ENABLE;
+    int32_t int32Param = static_cast<int32_t>(GetU32Data(data));
+    std::string stringParam(data, size);
+    sptr<IRemoteObject> token = GetFuzzAbilityToken();
+    std::vector<std::string> info;
+    MissionSnapshot missionSnapshot;
 
-  // fuzz for MissionInfoMgr
-  auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
-  missionInfoMgr->UpdateMissionLabel(int32Param, stringParam);
-  missionInfoMgr->LoadAllMissionInfo();
-  std::list<int32_t> missions;
-  missionInfoMgr->HandleUnInstallApp(stringParam, int32Param, missions);
-  missionInfoMgr->GetMatchedMission(stringParam, int32Param, missions);
-  missionInfoMgr->Dump(info);
-  sptr<ISnapshotHandler> snapshotHandler;
-  missionInfoMgr->RegisterSnapshotHandler(snapshotHandler);
-  missionInfoMgr->UpdateMissionSnapshot(int32Param, token, missionSnapshot,
-                                        boolParam);
-  missionInfoMgr->GetSnapshot(int32Param);
-  missionInfoMgr->GetMissionSnapshot(int32Param, token, missionSnapshot,
-                                     boolParam, boolParam);
-  Snapshot missionInfoMgrSnapshot;
-  missionInfoMgr->CreateWhitePixelMap(missionInfoMgrSnapshot);
-  return true;
+    // fuzz for MissionInfoMgr
+    auto missionInfoMgr = std::make_shared<MissionInfoMgr>();
+    missionInfoMgr->UpdateMissionLabel(int32Param, stringParam);
+    missionInfoMgr->LoadAllMissionInfo();
+    std::list<int32_t> missions;
+    missionInfoMgr->HandleUnInstallApp(stringParam, int32Param, missions);
+    missionInfoMgr->GetMatchedMission(stringParam, int32Param, missions);
+    missionInfoMgr->Dump(info);
+    sptr<ISnapshotHandler> snapshotHandler;
+    missionInfoMgr->RegisterSnapshotHandler(snapshotHandler);
+    missionInfoMgr->UpdateMissionSnapshot(int32Param, token, missionSnapshot,
+                                          boolParam);
+    missionInfoMgr->GetSnapshot(int32Param);
+    missionInfoMgr->GetMissionSnapshot(int32Param, token, missionSnapshot,
+                                       boolParam, boolParam);
+    Snapshot missionInfoMgrSnapshot;
+    missionInfoMgr->CreateWhitePixelMap(missionInfoMgrSnapshot);
+    return true;
 }
 } // namespace OHOS
 
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  /* Run your code on data */
-  if (data == nullptr) {
-    return 0;
-  }
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
 
-  /* Validate the length of size */
-  if (size < OHOS::U32_AT_SIZE || size > OHOS::FOO_MAX_LEN) {
-    return 0;
-  }
+    /* Validate the length of size */
+    if (size < OHOS::U32_AT_SIZE || size > OHOS::FOO_MAX_LEN) {
+        return 0;
+    }
 
-  char *ch = (char *)malloc(size + 1);
-  if (ch == nullptr) {
-    std::cout << "malloc failed." << std::endl;
-    return 0;
-  }
+    char *ch = (char *)malloc(size + 1);
+    if (ch == nullptr) {
+        std::cout << "malloc failed." << std::endl;
+        return 0;
+    }
 
-  (void)memset_s(ch, size + 1, 0x00, size + 1);
-  if (memcpy_s(ch, size, data, size) != EOK) {
-    std::cout << "copy failed." << std::endl;
+    (void)memset_s(ch, size + 1, 0x00, size + 1);
+    if (memcpy_s(ch, size, data, size) != EOK) {
+        std::cout << "copy failed." << std::endl;
+        free(ch);
+        ch = nullptr;
+        return 0;
+    }
+
+    OHOS::DoSomethingInterestingWithMyAPI(ch, size);
     free(ch);
     ch = nullptr;
     return 0;
-  }
-
-  OHOS::DoSomethingInterestingWithMyAPI(ch, size);
-  free(ch);
-  ch = nullptr;
-  return 0;
 }
