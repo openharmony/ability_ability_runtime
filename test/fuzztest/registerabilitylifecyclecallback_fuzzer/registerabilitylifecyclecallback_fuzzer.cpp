b5147db263621f102a4b0a3006723ea08f68b805
孙旭辉 <sunxuhui7@huawei.com>
2024-05-25 07:16:43 +0000

    update test/fuzztest/registerabilitylifecyclecallback_fuzzer
    Signed-off-by: 孙旭辉 <sunxuhui7@huawei.com>

--- test/fuzztest/registerabilitylifecyclecallback_fuzzer/registerabilitylifecyclecallback_fuzzer.cpp
+++ test/fuzztest/registerabilitylifecyclecallback_fuzzer/registerabilitylifecyclecallback_fuzzer.cpp
@@ -81,7 +81,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     /* Validate the length of size */
     if (size > OHOS::FOO_MAX_LEN || size < OHOS::U32_AT_SIZE) {
         return 0;
-    }    
+    }
     char *ch = (char *)malloc(size + 1);
     if (ch == nullptr) {
         std::cout << "malloc failed." << std::endl;
