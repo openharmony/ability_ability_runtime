88c6145a8435f611677dd47041608fec98668512
孙旭辉 <sunxuhui7@huawei.com>
2024-05-27 08:07:50 +0000

    update frameworks/native/appkit/ability_bundle_manager_helper/bundle_mgr_helper.cpp.
    Signed-off-by: 孙旭辉 <sunxuhui7@huawei.com>

--- frameworks/native/appkit/ability_bundle_manager_helper/bundle_mgr_helper.cpp
+++ frameworks/native/appkit/ability_bundle_manager_helper/bundle_mgr_helper.cpp
@@ -643,7 +643,7 @@ bool BundleMgrHelper::ImplicitQueryInfos(const Want &want, int32_t flags, int32_
     AAFwk::Want newWant = want;
     newWant.RemoveAllFd();
     HITRACE_METER_NAME(HITRACE_TAG_ABILITY_MANAGER, __PRETTY_FUNCTION__);
-    bool findDefaultApp;
+    bool findDefaultApp = false;
     return bundleMgr->ImplicitQueryInfos(newWant, flags, userId, withDefault, abilityInfos, extensionInfos,
         findDefaultApp);
 }
