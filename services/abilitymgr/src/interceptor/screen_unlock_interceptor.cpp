c36a19632aa2d772161c52a1ada889be81c58a94
donglin <donglin9@huawei.com>
2024-05-27 01:12:25 +0000

    增加启动未解锁前拦截
    Signed-off-by: donglin <donglin9@huawei.com>
    Change-Id: Ie171353e57187310fcdb7b6cbc158e19e416121e

---
+++ services/abilitymgr/src/interceptor/screen_unlock_interceptor.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "interceptor/screen_unlock_interceptor.h"
+
+#include "ability_info.h"
+#include "ability_util.h"
+#include "hilog_tag_wrapper.h"
+#include "parameters.h"
+#include "start_ability_utils.h"
+
+namespace OHOS {
+namespace AAFwk {
+namespace {
+const std::string SUPPORT_SCREEN_UNLOCK_STARTUP = "persist.sys.ability.support.screen_unlock_startup";
+}
+ErrCode ScreenUnlockInterceptor::DoProcess(AbilityInterceptorParam param)
+{
+    // xts reboot can still run
+    std::string supportStart = OHOS::system::GetParameter(SUPPORT_SCREEN_UNLOCK_STARTUP, "false");
+    if (supportStart == "true") {
+        TAG_LOGD(AAFwkTag::ABILITYMGR, "Abilityms support screen unlock startup.");
+        return ERR_OK;
+    }
+    // get target application info
+    AppExecFwk::AbilityInfo targetAbilityInfo;
+    if (StartAbilityUtils::startAbilityInfo != nullptr) {
+        targetAbilityInfo = StartAbilityUtils::startAbilityInfo->abilityInfo;
+    } else {
+        auto bundleMgrHelper = AbilityUtil::GetBundleManagerHelper();
+        if (bundleMgrHelper == nullptr) {
+            TAG_LOGE(AAFwkTag::ABILITYMGR, "The bundleMgrHelper is nullptr.");
+            return ERR_OK;
+        }
+        IN_PROCESS_CALL_WITHOUT_RET(bundleMgrHelper->QueryAbilityInfo(param.want,
+            AppExecFwk::AbilityInfoFlag::GET_ABILITY_INFO_WITH_APPLICATION, param.userId, targetAbilityInfo));
+    }
+
+    // temp add isSystemApp pass, remove after systemapp adjust
+    if (targetAbilityInfo.applicationInfo.isSystemApp ||
+        targetAbilityInfo.applicationInfo.allowAppRunWhenDeviceFirstLocked) {
+        return ERR_OK;
+    }
+    TAG_LOGE(AAFwkTag::ABILITYMGR, "Can not startup when device first locked.");
+    return ERR_BLOCK_START_FIRST_BOOT_SCREEN_UNLOCK;
+}
+} // namespace AAFwk
+} // namespace OHOS
