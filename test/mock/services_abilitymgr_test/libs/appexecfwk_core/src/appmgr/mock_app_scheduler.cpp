0c8e98593d4a2e1cf152435a92583e6128cce88a
sodanotgreen <limengzheng@huawei.com>
2024-05-28 17:44:34 +0800

    fix
    Signed-off-by: sodanotgreen <limengzheng@huawei.com>

--- test/mock/services_abilitymgr_test/libs/appexecfwk_core/src/appmgr/mock_app_scheduler.cpp
+++ test/mock/services_abilitymgr_test/libs/appexecfwk_core/src/appmgr/mock_app_scheduler.cpp
@@ -110,7 +110,7 @@ void AppScheduler::OnAbilityRequestDone(const sptr<IRemoteObject>& token, const 
     TAG_LOGI(AAFwkTag::TEST, "Test AppScheduler::OnAbilityRequestDone()");
 }
 
-int AppScheduler::KillApplication(const std::string& bundleName, const bool clearPageStack = true)
+int AppScheduler::KillApplication(const std::string& bundleName, const bool clearPageStack)
 {
     TAG_LOGI(AAFwkTag::TEST, "Test AppScheduler::KillApplication()");
     return ERR_OK;
