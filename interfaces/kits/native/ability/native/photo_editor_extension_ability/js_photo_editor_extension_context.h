bb28fd7dd5702c16c011cc52879f659b82b1e01e
mars10577020 <8651080+mars10577020@user.noreply.gitee.com>
2024-05-29 06:01:51 +0000

    codecheck
    Signed-off-by: mars10577020 <8651080+mars10577020@user.noreply.gitee.com>

--- interfaces/kits/native/ability/native/photo_editor_extension_ability/js_photo_editor_extension_context.h
+++ interfaces/kits/native/ability/native/photo_editor_extension_ability/js_photo_editor_extension_context.h
@@ -26,8 +26,7 @@ struct NapiCallbackInfo;
 
 class JsPhotoEditorExtensionContext {
 public:
-    explicit JsPhotoEditorExtensionContext(const std::shared_ptr<PhotoEditorExtensionContext> &context) :
-        context_(context)
+    explicit JsPhotoEditorExtensionContext(const std::shared_ptr<PhotoEditorExtensionContext> &context) : context_(context)
     {}
 
     virtual ~JsPhotoEditorExtensionContext() = default;
