857c8544b9faaeb0ab0196f4aca548c4819ddfca
sodanotgreen <limengzheng@huawei.com>
2024-05-29 11:52:04 +0800

    fix
    Signed-off-by: sodanotgreen <limengzheng@huawei.com>

--- test/unittest/sys_mgr_client_test/mock_ability_manager_service.h
+++ test/unittest/sys_mgr_client_test/mock_ability_manager_service.h
@@ -89,7 +89,7 @@ public:
     int StopServiceAbility(const Want& want, int32_t userId = DEFAULT_INVAL_VALUE,
         const sptr<IRemoteObject> &token = nullptr) override;
 
-    MOCK_METHOD1(KillProcess, int(const std::string& bundleName), true);
+    MOCK_METHOD1(KillProcess, int(const std::string& bundleName, const bool clearPageStack));
     MOCK_METHOD2(UninstallApp, int(const std::string& bundleName, int32_t uid));
     MOCK_METHOD2(
         GetWantSender, sptr<IWantSender>(const WantSenderInfo& wantSenderInfo, const sptr<IRemoteObject>& callerToken));
