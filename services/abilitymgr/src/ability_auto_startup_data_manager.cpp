420e6d3f9b6a0d4f6de1497f513afb321fdcdd6a
mashaohua7 <mashaohua7@huawei.com>
2024-06-04 17:18:26 +0800

    修改bug
    Signed-off-by: mashaohua7 <mashaohua7@huawei.com>

--- services/abilitymgr/src/ability_auto_startup_data_manager.cpp
+++ services/abilitymgr/src/ability_auto_startup_data_manager.cpp
@@ -462,7 +462,7 @@ bool AbilityAutoStartupDataManager::IsEqual(const DistributedKv::Key &key, const
         }
     }
 
-    if (jsonObject.contains(JSON_KEY_APP_CLONE_INDEX) && jsonObject[JSON_KEY_APP_CLONE_INDEX].is_string()) {
+    if (jsonObject.contains(JSON_KEY_APP_CLONE_INDEX) && jsonObject[JSON_KEY_APP_CLONE_INDEX].is_number()) {
         if (info.appCloneIndex != jsonObject.at(JSON_KEY_APP_CLONE_INDEX).get<std::int32_t>()) {
             return false;
         }
