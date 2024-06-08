bf364f2cd7b7bcd852e59f729a019b41b638d7c5
rentangyu <rentangyu@huawei.com>
2024-06-08 12:40:57 +0800

    sourcemap解析之后url不正确
    issues:https://gitee.com/openharmony/ability_ability_runtime/issues/I9W649
    
    Signed-off-by: rentangyu <rentangyu@huawei.com>

--- js_environment/frameworks/js_environment/src/source_map.cpp
+++ js_environment/frameworks/js_environment/src/source_map.cpp
@@ -224,13 +224,13 @@ void SourceMap::SplitSourceMap(const std::string& sourceMapData)
             isUrl = true;
         }
     }
-    std::shared_ptr<SourceMapData> modularMap = std::make_shared<SourceMapData>();
     for (auto it = sources_.begin(); it != sources_.end(); ++it) {
         std::string mappings = mappings_[it->first];
         if (mappings.size() < FLAG_MAPPINGS_LEN + 1) {
             TAG_LOGE(AAFwkTag::JSENV, "Translate failed, url: %{public}s", it->first.c_str());
             continue;
         }
+        std::shared_ptr<SourceMapData> modularMap = std::make_shared<SourceMapData>();
         ExtractSourceMapData(mappings.substr(FLAG_MAPPINGS_LEN, mappings.size() - FLAG_MAPPINGS_LEN - 1), modularMap);
         if (modularMap == nullptr) {
             TAG_LOGE(AAFwkTag::JSENV, "Extract mappings failed, url: %{public}s", it->first.c_str());
