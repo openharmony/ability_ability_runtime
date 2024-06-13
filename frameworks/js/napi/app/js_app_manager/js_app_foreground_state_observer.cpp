0772bfd82b92245f24ed35e23daa070a681c1daf
chenqiao <chenqiao002@chinasoftinc.com>
2024-06-13 19:18:58 +0800

    4.1release 修复日志单词拼写错误
    Signed-off-by: chenqiao <chenqiao002@chinasoftinc.com>

--- frameworks/js/napi/app/js_app_manager/js_app_foreground_state_observer.cpp
+++ frameworks/js/napi/app/js_app_manager/js_app_foreground_state_observer.cpp
@@ -29,7 +29,7 @@ void JSAppForegroundStateObserver::OnAppStateChanged(const AppStateData &appStat
 {
     HILOG_DEBUG("Called.");
     if (!valid_) {
-        HILOG_ERROR("The app manager may has destoryed.");
+        HILOG_ERROR("The app manager may has destroyed.");
         return;
     }
     wptr<JSAppForegroundStateObserver> self = this;
