af110a851790efbe15a8279c4d1a167c0035b98b
jsjzju <jinsenjun@huawei.com>
2024-07-22 01:18:56 +0800

    设置foregrounding状态，避免超时任务无法执行
    Signed-off-by: jsjzju <jinsenjun@huawei.com>
    Change-Id: I4d0d585674fa5c01da7703e46adf39c1799f254d

--- services/abilitymgr/src/ability_record.cpp
+++ services/abilitymgr/src/ability_record.cpp
@@ -601,6 +601,7 @@ void AbilityRecord::ProcessForegroundAbility(const std::shared_ptr<AbilityRecord
         // background to active state
         TAG_LOGD(AAFwkTag::ABILITYMGR, "MoveToForeground, %{public}s", element.c_str());
         lifeCycleStateInfo_.sceneFlagBak = sceneFlag;
+        SetAbilityStateInner(AbilityState::FOREGROUNDING);
         DelayedSingleton<AppScheduler>::GetInstance()->MoveToForeground(token_);
     }
 }
@@ -752,6 +753,7 @@ void AbilityRecord::ProcessForegroundAbility(bool isRecent, const AbilityRequest
             // background to active state
             TAG_LOGD(AAFwkTag::ABILITYMGR, "MoveToForeground, %{public}s", element.c_str());
             lifeCycleStateInfo_.sceneFlagBak = sceneFlag;
+            SetAbilityStateInner(AbilityState::FOREGROUNDING);
             DelayedSingleton<AppScheduler>::GetInstance()->MoveToForeground(token_);
         }
     } else {
