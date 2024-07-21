c63edd618ec9d0d6b479812ee07863140631d118
jsjzju <jinsenjun@huawei.com>
2024-07-22 01:18:56 +0800

    设置foregrounding状态，避免超时任务无法执行 （cherry picked commit from <gitee.com//openharmony/ability_ability_runtime/commit/af110a851790efbe15a8279c4d1a167c0035b98b>
    Signed-off-by: jsjzju <jinsenjun@huawei.com>

--- services/abilitymgr/src/scene_board/ui_ability_lifecycle_manager.cpp
+++ services/abilitymgr/src/scene_board/ui_ability_lifecycle_manager.cpp
@@ -256,6 +256,7 @@ int UIAbilityLifecycleManager::AttachAbilityThread(const sptr<IAbilityScheduler>
         if (abilityRecord->GetWant().GetBoolParam(Want::PARAM_RESV_CALL_TO_FOREGROUND, false)) {
             abilityRecord->SetStartToForeground(true);
             abilityRecord->PostForegroundTimeoutTask();
+            abilityRecord->SetAbilityState(AbilityState::FOREGROUNDING);
             DelayedSingleton<AppScheduler>::GetInstance()->MoveToForeground(token);
         } else {
             abilityRecord->SetStartToBackground(true);
@@ -267,6 +268,7 @@ int UIAbilityLifecycleManager::AttachAbilityThread(const sptr<IAbilityScheduler>
         abilityRecord->CallRequest();
     }
     abilityRecord->PostForegroundTimeoutTask();
+    abilityRecord->SetAbilityState(AbilityState::FOREGROUNDING);
     DelayedSingleton<AppScheduler>::GetInstance()->MoveToForeground(token);
     return ERR_OK;
 }
@@ -1106,6 +1108,7 @@ void UIAbilityLifecycleManager::CompleteBackground(const std::shared_ptr<Ability
 
     if (abilityRecord->GetPendingState() == AbilityState::FOREGROUND) {
         abilityRecord->PostForegroundTimeoutTask();
+        abilityRecord->SetAbilityState(AbilityState::FOREGROUNDING);
         DelayedSingleton<AppScheduler>::GetInstance()->MoveToForeground(abilityRecord->GetToken());
     } else if (abilityRecord->GetPendingState() == AbilityState::BACKGROUND) {
         TAG_LOGD(AAFwkTag::ABILITYMGR, "not continuous startup.");
