8ec8e5f709621228359f9f8ada028f6d4048cc28
sodanotgreen <limengzheng@huawei.com>
2024-08-06 21:29:17 +0800

    log
    Signed-off-by: sodanotgreen <limengzheng@huawei.com>

--- frameworks/js/napi/app/app_manager/js_app_manager.cpp
+++ frameworks/js/napi/app/app_manager/js_app_manager.cpp
@@ -39,13 +39,11 @@ namespace {
 constexpr int32_t INDEX_ZERO = 0;
 constexpr int32_t INDEX_ONE = 1;
 constexpr int32_t INDEX_TWO = 2;
-constexpr int32_t INDEX_THREE = 3;
 constexpr int32_t ERROR_CODE_ONE = 1;
 constexpr size_t ARGC_ZERO = 0;
 constexpr size_t ARGC_ONE = 1;
 constexpr size_t ARGC_TWO = 2;
 constexpr size_t ARGC_THREE = 3;
-constexpr size_t ARGC_FOUR = 4;
 constexpr int32_t ERR_NOT_OK = -1;
 
 class JsAppManager final {
