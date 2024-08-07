55e635a1a8e5b531a914f90b43eb5532553bd3c0
liuhongjie <liuhongjie3@huawei.com>
2024-08-07 16:14:11 +0800

    补充PREPARE_CONTINUATION NAPI转换
    Signed-off-by: liuhongjie <liuhongjie3@huawei.com>

--- frameworks/js/napi/ability_constant/ability_constant_module.cpp
+++ frameworks/js/napi/ability_constant/ability_constant_module.cpp
@@ -59,6 +59,7 @@ static napi_value InitLaunchReasonObject(napi_env env)
     NAPI_CALL(env, SetEnumItem(env, object, "SHARE", LAUNCHREASON_SHARE));
     NAPI_CALL(env, SetEnumItem(env, object, "AUTO_STARTUP", LAUNCHREASON_AUTO_STARTUP));
     NAPI_CALL(env, SetEnumItem(env, object, "INSIGHT_INTENT", LAUNCHREASON_INSIGHT_INTENT));
+    NAPI_CALL(env, SetEnumItem(env, object, "PREPARE_CONTINUATION", LAUNCHREASON_PREPARE_CONTINUATION));
 
     return object;
 }
