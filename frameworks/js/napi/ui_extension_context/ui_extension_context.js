e2664749d81afff596900dd463c4f3ee929103e3
zhubingwei <zhubingwei@huawei.com>
2024-08-16 14:51:10 +0800

    Revert "!9339 ui service extension双向通信发起方UIExtensionContext改动"
    This reverts commit aae6a78c34181f0b012e68c154a90f760da3464f, reversing
    changes made to bbdc8fde2d25712c6cd5d354698787f8ae9a6b0b.
    
    Signed-off-by: zhubingwei <zhubingwei@huawei.com>
    Change-Id: I08e7378ff67dbee1cb1181c6c2381e5a3c1c0aa9

--- frameworks/js/napi/ui_extension_context/ui_extension_context.js
+++ frameworks/js/napi/ui_extension_context/ui_extension_context.js
@@ -39,14 +39,6 @@ class UIExtensionContext extends ExtensionContext {
     return this.__context_impl__.startUIServiceExtensionAbility(want, callback);
   }
 
-  connectUIServiceExtensionAbility(want, callback) {
-    return this.__context_impl__.connectUIServiceExtensionAbility(want, callback);
-  }
-
-  disconnectUIServiceExtensionAbility(proxy) {
-    return this.__context_impl__.disconnectUIServiceExtensionAbility(proxy);
-  }
-
   openLink(link, options, callback) {
     console.log('openLink');
     return this.__context_impl__.openLink(link, options, callback);
