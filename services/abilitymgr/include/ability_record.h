253e2b17fc92c24d97abc747be9e4dec33ec3da9
yangzk <yangzhongkai@huawei.com>
2024-08-17 17:07:40 +0800

    Description: UIAbility启动want安全加固 IssueNo: #IAKPID Sig: SIG_ApplicationFramework Feature or Bugfix: Feature Binary Source: No
    Signed-off-by: yangzk <yangzhongkai@huawei.com>
    Change-Id: I086faff0031756df8f9f1361958b361f2ac6c5d1

--- services/abilitymgr/include/ability_record.h
+++ services/abilitymgr/include/ability_record.h
@@ -348,6 +348,18 @@ enum class AbilityVisibilityState {
     UNSPECIFIED,
 };
 
+struct LaunchDebugInfo {
+public:
+    void Update(const Want &want);
+
+    bool isDebugAppSet = false;
+    bool isNativeDebugSet = false;
+    bool isPerfCmdSet = false;
+    bool debugApp = false;
+    bool nativeDebug = false;
+    std::string perfCmd;
+};
+
 /**
  * @class AbilityRecord
  * AbilityRecord records ability info and states and used to schedule ability life.
@@ -1254,6 +1266,8 @@ private:
 
     std::shared_ptr<Want> connectWant_ = nullptr;
     ffrt::mutex connectWantLock_;
+    bool isLaunching_ = true;
+    LaunchDebugInfo launchDebugInfo_;
 };
 }  // namespace AAFwk
 }  // namespace OHOS
