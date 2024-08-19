b1c08aee8d952c7869f4a8419f8a3cfdafb99bfa
sodanotgreen <limengzheng@huawei.com>
2024-08-19 17:07:27 +0800

    local
    Signed-off-by: sodanotgreen <limengzheng@huawei.com>

--- frameworks/native/ability/native/resource_config_helper.cpp
+++ frameworks/native/ability/native/resource_config_helper.cpp
@@ -16,6 +16,7 @@
 #include "hilog_tag_wrapper.h"
 #include "configuration_convertor.h"
 #include "hitrace_meter.h"
+#include "application_configuration_manager.h"
 
 namespace OHOS {
 namespace AbilityRuntime {
@@ -94,8 +95,11 @@ void ResourceConfigHelper::UpdateResConfig(
         UErrorCode status = U_ZERO_ERROR;
         icu::Locale locale = icu::Locale::forLanguageTag(language_, status);
         TAG_LOGD(AAFwkTag::ABILITY, "get forLanguageTag return[%{public}d]", static_cast<int>(status));
-        if (status == U_ZERO_ERROR) {
+        SetLevel curSetLevel = ApplicationConfigurationManager::GetInstance().GetLanguageSetLevel();
+        if (status == U_ZERO_ERROR && curSetLevel < SetLevel::Application) {
             resConfig->SetLocaleInfo(locale);
+        } else if (status == U_ZERO_ERROR) {
+            resConfig->SetPreferredLocaleInfo(locale);
         }
         const icu::Locale *localeInfo = resConfig->GetLocaleInfo();
         if (localeInfo != nullptr) {
