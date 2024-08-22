c90220c5a45169e80e00804b988a1f177f8dddde
chenzexin <chenzexin14@huawei.com>
2024-08-22 17:08:47 +0800

    update onstop callback
    Signed-off-by: chenzexin <chenzexin14@huawei.com>

--- frameworks/native/ability/native/ui_extension_ability/js_ui_extension.cpp
+++ frameworks/native/ability/native/ui_extension_ability/js_ui_extension.cpp
@@ -353,6 +353,7 @@ bool JsUIExtension::CheckPromise(napi_value result)
     return true;
 }
 
+namespace {
 napi_value PromiseCallback(napi_env env, napi_callback_info info)
 {
     void *data = nullptr;
@@ -366,6 +367,7 @@ napi_value PromiseCallback(napi_env env, napi_callback_info info)
     AppExecFwk::AbilityTransactionCallbackInfo<>::Destroy(callbackInfo);
     data = nullptr;
     return nullptr;
+}
 }
 
 bool JsUIExtension::CallPromise(napi_value result, AppExecFwk::AbilityTransactionCallbackInfo<> *callbackInfo)
