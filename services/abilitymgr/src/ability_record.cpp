c39910f0b16223ec6d34218647d4aaaa7464f182
夏良伟 <xialiangwei1@huawei.com>
2024-09-04 09:32:17 +0000

    update services/abilitymgr/src/ability_record.cpp.
    Signed-off-by: 夏良伟 <xialiangwei1@huawei.com>

--- services/abilitymgr/src/ability_record.cpp
+++ services/abilitymgr/src/ability_record.cpp
@@ -707,7 +707,6 @@ void AbilityRecord::ProcessForegroundAbility(bool isRecent, const AbilityRequest
             // background to active state
             TAG_LOGD(AAFwkTag::ABILITYMGR, "MoveToForeground, %{public}s", element.c_str());
             lifeCycleStateInfo_.sceneFlagBak = sceneFlag;
-            SetAbilityStateInner(AbilityState::FOREGROUNDING);
             DelayedSingleton<AppScheduler>::GetInstance()->MoveToForeground(token_);
         }
     } else {
