0bfd73fdd6cd548bc9f3d44c27e6656ae00045a5
xia <xialiangwei1@huawei.com>
2024-09-16 19:02:36 +0800

    蓝黄一致 Signed-off-by: xia <xialiangwei1@huawei.com>

--- services/abilitymgr/include/ability_manager_service.h
+++ services/abilitymgr/include/ability_manager_service.h
@@ -1194,6 +1194,8 @@ public:
 
     int CreateCloneSelectorDialog(AbilityRequest &request, int32_t userId, const std::string &replaceWantString = "");
 
+    void SetTargetCloneIndexInSameBundle(const Want &want, sptr<IRemoteObject> callerToken);
+
     virtual int RegisterAbilityFirstFrameStateObserver(const sptr<IAbilityFirstFrameStateObserver> &observer,
         const std::string &bundleName) override;
 
