0529d0c0003470127ad1743cddd6476e9e177d35
XKK <huyingsong@huawei.com>
2024-09-18 14:53:17 +0800

    wantAgent拉起extension新增校验
    Signed-off-by: XKK <huyingsong@huawei.com>

--- services/abilitymgr/include/ability_manager_service.h
+++ services/abilitymgr/include/ability_manager_service.h
@@ -2224,7 +2224,7 @@ private:
 
     bool CheckUIExtensionCallerPidByHostWindowId(const AbilityRequest &abilityRequest);
 
-    int CheckExtensionCallPermission(const Want& want, const AbilityRequest& abilityRequest);
+    int CheckExtensionCallPermission(const Want& want, const AbilityRequest& abilityRequest, uint32_t specifyTokenId);
 
     int CheckServiceCallPermission(const AbilityRequest& abilityRequest,
         const AppExecFwk::AbilityInfo& abilityInfo);
