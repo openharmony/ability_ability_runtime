540151316dd51dd53bc69eb9166241a2588f99ee
段嗣钊 <duansizhao@huawei.com>
2024-09-20 12:51:18 +0800

    PARAMS_STREAM字段赋值 （cherry picked commit from <gitee.com//openharmony/ability_ability_runtime/commit/bd44f16f301dd16d52a25cb2eeac3f5f3afe844e>
    Signed-off-by: 段嗣钊 <duansizhao@huawei.com>

--- frameworks/js/napi/wantConstant/want_constant.cpp
+++ frameworks/js/napi/wantConstant/want_constant.cpp
@@ -119,6 +119,7 @@ napi_value WantConstantInit(napi_env env, napi_value exports)
     SetNamedProperty(env, params, "ohos.param.atomicservice.buildFunction", "BUILD_FUNCTION");
     SetNamedProperty(env, params, "ohos.param.atomicservice.subpackageName", "SUB_PACKAGE_NAME");
     SetNamedProperty(env, params, "ohos.extra.param.key.callerRequestCode", "CALLER_REQUEST_CODE");
+    SetNamedProperty(env, params, "ability.params.stream", "PARAMS_STREAM");
     napi_property_descriptor exportFuncs[] = {
         DECLARE_NAPI_PROPERTY("Action", action),
         DECLARE_NAPI_PROPERTY("Entity", entity),
