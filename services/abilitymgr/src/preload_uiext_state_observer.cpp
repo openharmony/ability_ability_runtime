3261cf452f2e4240e2ed19adac2c3121496ec81f
chenzexin <chenzexin14@huawei.com>
2024-10-08 15:54:15 +0800

    cherry pick 22cae07 from https://gitee.com/zexin_c/ability_ability_runtime/pulls/10943 del anonymous space, import defined enum
    Signed-off-by: chenzexin <chenzexin14@huawei.com>

--- services/abilitymgr/src/preload_uiext_state_observer.cpp
+++ services/abilitymgr/src/preload_uiext_state_observer.cpp
@@ -20,9 +20,6 @@
 
 namespace OHOS {
 namespace AAFwk {
-namespace {
-constexpr int32_t APP_STATE_CACHED = 100;
-}
 PreLoadUIExtStateObserver::PreLoadUIExtStateObserver(
     std::weak_ptr<OHOS::AbilityRuntime::ExtensionRecord> extensionRecord) : extensionRecord_(extensionRecord) {}
 
@@ -51,7 +48,7 @@ void PreLoadUIExtStateObserver::OnAppCacheStateChanged(const AppExecFwk::AppStat
     if (extensionRecord != nullptr) {
         auto hostPid = extensionRecord->hostPid_;
         int32_t cachePid = appStateData.pid;
-        if (static_cast<int32_t>(hostPid) != cachePid || appStateData.state != APP_STATE_CACHED) {
+        if (static_cast<int32_t>(hostPid) != cachePid || appStateData.state != AppExecFwk::ApplicationState::APP_STATE_CACHED) {
             TAG_LOGI(AAFwkTag::ABILITYMGR, "appStateData.state = %{public}d", appStateData.state);
             return;
         }
