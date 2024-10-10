e7a5e32950ad16449a7b9dfe6ab187b8fb2c6093
huangshiwei <huangshiwei4@huawei.com>
2024-10-10 17:01:50 +0800

    huangshiwei4@huawei.com
    Signed-off-by: huangshiwei <huangshiwei4@huawei.com>

--- test/unittest/cache_process_manager_test/cache_process_manager_test.cpp
+++ test/unittest/cache_process_manager_test/cache_process_manager_test.cpp
@@ -131,6 +131,8 @@ HWTEST_F(CacheProcessManagerTest, CacheProcessManager_PenddingCacheProcess_0100,
     appRecord->SetKeepAliveEnableState(true);
     appRecord->SetSingleton(true);
     appRecord->SetEmptyKeepAliveAppState(true);
+    appRecord->SetKeepAliveBundle(true);
+    appRecord->SetMainProcess(true);
     EXPECT_EQ(cacheProcMgr->PenddingCacheProcess(appRecord), false);
     // nullptr not allowed
     std::shared_ptr<AppRunningRecord> appRecord2 = nullptr;
