ede1dc1e25cbde9595fbc6cd6d696193835b1aca
wentao <wentao18@huawei.com>
2024-10-12 10:49:43 +0800

    Developers can use custom app package names in Cangjie applications
    Signed-off-by: wentao <wentao18@huawei.com>

--- frameworks/native/appkit/app/main_thread.cpp
+++ frameworks/native/appkit/app/main_thread.cpp
@@ -1345,7 +1345,9 @@ void MainThread::HandleLaunchApplication(const AppLaunchData &appLaunchData, con
         }
 #ifdef CJ_FRONTEND
         if (!entryHapModuleInfo.abilityInfos.empty()) {
-            isCJApp = AbilityRuntime::CJRuntime::IsCJAbility(entryHapModuleInfo.abilityInfos.front().srcEntrance);
+            auto srcEntrancenName = entryHapModuleInfo.abilityInfos.front().srcEntrance;
+            isCJApp = AbilityRuntime::CJRuntime::IsCJAbility(srcEntrancenName);
+            AbilityRuntime::CJRuntime::SetPackageName(srcEntrancenName);
         }
 #endif
         moduelJson = entryHapModuleInfo.isModuleJson;
