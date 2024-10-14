b701f3ff85ff870830324176557d5f0e0c67c149
XKK <huyingsong@huawei.com>
2024-10-14 20:43:57 +0800

    缩小ClearPendingWantRecordTask锁范围
    Signed-off-by: XKK <huyingsong@huawei.com>

--- services/abilitymgr/include/pending_want_manager.h
+++ services/abilitymgr/include/pending_want_manager.h
@@ -184,9 +184,17 @@ private:
 
     bool CheckWindowState(int32_t pid);
 
+    void EraseBundleRecord(const std::vector<WantsInfo> &wantsInfos, std::shared_ptr<PendingWantKey> key);
+
+    void InsertBundleRecord(const std::vector<WantsInfo> &wantsInfos, std::shared_ptr<PendingWantKey> key);
+
+    bool QueryRecordByBundle(const std::string &bundleName);
+
 private:
     std::map<std::shared_ptr<PendingWantKey>, sptr<PendingWantRecord>> wantRecords_;
+    std::map<std::string, std::vector<std::shared_ptr<PendingWantKey>>> bundleRecords_;
     ffrt::mutex mutex_;
+    ffrt::mutex bundleRecordsMutex_;
 };
 }  // namespace AAFwk
 }  // namespace OHOS
