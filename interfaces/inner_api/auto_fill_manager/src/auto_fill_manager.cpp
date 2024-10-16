f5f95667c274bce4a8fe2843dc8355af070092cb
songjindian <songjindian1@huawei.com>
2024-10-16 17:11:50 +0800

    代码一致性修改第8笔
    Signed-off-by: songjindian <songjindian1@huawei.com>

--- interfaces/inner_api/auto_fill_manager/src/auto_fill_manager.cpp
+++ interfaces/inner_api/auto_fill_manager/src/auto_fill_manager.cpp
@@ -275,9 +275,9 @@ void AutoFillManager::SetTimeOutEvent(uint32_t eventId)
 
 void AutoFillManager::RemoveEvent(uint32_t eventId)
 {
-    TAG_LOGI(AAFwkTag::AUTOFILLMGR, "called");
+    TAG_LOGD(AAFwkTag::AUTOFILLMGR, "called");
     if (eventHandler_ == nullptr) {
-        TAG_LOGE(AAFwkTag::AUTOFILLMGR, "null eventHandler");
+        TAG_LOGE(AAFwkTag::AUTOFILLMGR, "null eventhandler");
         return;
     }
     eventHandler_->RemoveEvent(eventId);
@@ -296,7 +296,7 @@ void AutoFillManager::HandleTimeOut(uint32_t eventId)
 
 bool AutoFillManager::IsNeedToCreatePopupWindow(const AbilityBase::AutoFillType &autoFillType)
 {
-    TAG_LOGD(AAFwkTag::AUTOFILLMGR, "called");
+    TAG_LOGD(AAFwkTag::AUTOFILLMGR, "Called.");
     if (autoFillType == AbilityBase::AutoFillType::PASSWORD ||
         autoFillType == AbilityBase::AutoFillType::USER_NAME ||
         autoFillType == AbilityBase::AutoFillType::NEW_PASSWORD) {
