6aa1611f9a379c582893b7c6740029e1d75976c8
lixiyuan <lixiyuan7@huawei.com>
2024-10-26 10:28:03 +0800

    5.0.1同步修改
    Signed-off-by: lixiyuan <lixiyuan7@huawei.com>

--- test/unittest/uri_permission_impl_test/mock/include/mock_storage_manager_service.h
+++ test/unittest/uri_permission_impl_test/mock/include/mock_storage_manager_service.h
@@ -71,7 +71,7 @@ public:
         return E_OK;
     }
 
-    virtual int32_t GetBundleStats(std::string pkgName, BundleStats &bundleStats, int32_t appIndex) override
+    virtual int32_t GetBundleStats(std::string pkgName, BundleStats &bundleStats, int32_t) override
     {
         return E_OK;
     }
