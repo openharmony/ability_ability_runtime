10d9bfc5169422a86790f154ba4cd362bd7500ff
luopengtao <luopengtao1@huawei.com>
2024-10-26 04:37:51 +0000

    update services/appmgr/include/app_running_record.h.
    Signed-off-by: luopengtao <luopengtao1@huawei.com>

--- services/appmgr/include/app_running_record.h
+++ services/appmgr/include/app_running_record.h
@@ -988,6 +988,7 @@ private:
     bool isErrorInfoEnhance_ = false;
     bool isNativeStart_ = false;
     bool isMultiThread_ = false;
+    bool enableProcessCache_ = false;
     SupportProcessCacheState procCacheSupportState_ = SupportProcessCacheState::UNSPECIFIED;
     bool processCacheBlocked = false; // temporarily block process cache feature
     sptr<IRemoteObject> browserHost_;
