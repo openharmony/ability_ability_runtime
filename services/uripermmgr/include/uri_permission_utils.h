bf4f372b796b43ed51e62d72dc4570dee822db44
段嗣钊 <duansizhao@huawei.com>
2024-10-28 17:32:02 +0800

    支持分身应用URI授权 (挑单5.0.0release)
    Signed-off-by: 段嗣钊 <duansizhao@huawei.com>

--- services/uripermmgr/include/uri_permission_utils.h
+++ services/uripermmgr/include/uri_permission_utils.h
@@ -32,9 +32,10 @@ public:
     static bool IsSAOrSystemAppCall();
     static bool IsSystemAppCall(uint32_t tokenId);
     static bool CheckIsSystemAppByTokenId(uint32_t tokenId);
-    static int32_t GetAppIdByBundleName(const std::string &bundleName, std::string &appId);
+    static bool GetDirByBundleNameAndAppIndex(const std::string &bundleName, int32_t appIndex, std::string &dirName);
     static bool GetBundleNameByTokenId(uint32_t tokenId, std::string &bundleName);
-    static std::string GetCallerNameByTokenId(uint32_t callerTokenId);
+    static bool GetAlterableBundleNameByTokenId(uint32_t tokenId, std::string &bundleName);
+    static int32_t GetAppIdByBundleName(const std::string &bundleName, std::string &appId);
     static int32_t GetTokenIdByBundleName(const std::string &bundleName, int32_t appIndex, uint32_t &tokenId);
     static bool IsDocsCloudUri(Uri &uri);
 
