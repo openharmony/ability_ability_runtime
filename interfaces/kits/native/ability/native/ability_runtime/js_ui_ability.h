b4a68d8919395390438f871d23605887ea719388
jsy <jusiyao1@huawei.com>
2024-10-28 20:06:06 +0800

    add preload module
    Signed-off-by: jsy <jusiyao1@huawei.com>

--- interfaces/kits/native/ability/native/ability_runtime/js_ui_ability.h
+++ interfaces/kits/native/ability/native/ability_runtime/js_ui_ability.h
@@ -331,7 +331,7 @@ private:
     bool CheckSatisfyTargetAPIVersion(int32_t targetAPIVersion);
     bool BackPressDefaultValue();
     void UpdateAbilityObj(std::shared_ptr<AbilityInfo> abilityInfo,
-        const std::string &moduleName, const std::string &srcPath)
+        const std::string &moduleName, const std::string &srcPath);
 
     JsRuntime &jsRuntime_;
     std::shared_ptr<NativeReference> shellContextRef_;
