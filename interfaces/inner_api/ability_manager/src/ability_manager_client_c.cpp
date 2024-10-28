12903bbe640777255663adbd74c1a02c63eea82d
yangyang706 <yangyang706@h-partners.com>
2024-10-28 20:14:02 +0800

    fix compiler error
    Signed-off-by: yangyang706 <yangyang706@h-partners.com>
    Change-Id: I5db19ec4891621e9cdf8594e349914555635406e

--- interfaces/inner_api/ability_manager/src/ability_manager_client_c.cpp
+++ interfaces/inner_api/ability_manager/src/ability_manager_client_c.cpp
@@ -33,6 +33,6 @@ int RecordAppExitReason(int exitReason, const char *exitMsg)
     OHOS::AAFwk::ExitReason exitReasonData = { reason, exitMsgStr };
 
     auto instance = OHOS::AAFwk::AbilityManagerClient::GetInstance();
-    CHECK_POINTER_AND_RETURN_VALUE(instance, -1);
+    CHECK_POINTER_AND_RETURN(instance, -1);
     return instance->RecordAppExitReason(exitReasonData);
 }
