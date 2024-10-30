b0cc392fa9c4168fa73adb0601b9c72181e4cff1
yangyang706 <yangyang706@h-partners.com>
2024-10-30 19:03:21 +0800

    fix code
    Signed-off-by: yangyang706 <yangyang706@h-partners.com>
    Change-Id: I70809dd1d30b2a2cb2d3cd31e831552a8cd64292

--- services/appmgr/src/app_mgr_service_inner.cpp
+++ services/appmgr/src/app_mgr_service_inner.cpp
@@ -583,7 +583,7 @@ void AppMgrServiceInner::AfterLoadAbility(std::shared_ptr<AppRunningRecord> appR
     PerfProfile::GetInstance().Reset();
     appRecord->UpdateAbilityState(token, AbilityState::ABILITY_STATE_CREATE);
 
-    auto reportLoadTask = [appRecord]() {
+    auto reportLoadTask = [appRecord, loadParam]() {
         auto priorityObj = appRecord->GetPriorityObject();
         if (priorityObj) {
             auto timeOut = AbilityRuntime::GlobalConstant::GetLoadTimeOutBase() *
@@ -592,11 +592,9 @@ void AppMgrServiceInner::AfterLoadAbility(std::shared_ptr<AppRunningRecord> appR
                 timeOut = AbilityRuntime::GlobalConstant::GetLoadTimeOutInactive() *
                     AAFwk::AppUtils::GetInstance().GetTimeoutUnitTimeRatio();
             }
-            int64_t abilityRecordId = appRecord->GetAbilityRunningRecord()
-                ? static_cast<int64_t>(appRecord->GetAbilityRunningRecord()->GetAbilityRecordId()) : static_cast<int64_t>(loadParam->abilityRecordId);
 
             AAFwk::ResSchedUtil::GetInstance().ReportLoadingEventToRss(AAFwk::LoadingStage::LOAD_BEGIN,
-                priorityObj->GetPid(), appRecord->GetUid(), timeOut, abilityRecordId);
+                priorityObj->GetPid(), appRecord->GetUid(), timeOut, static_cast<int64_t>(loadParam->abilityRecordId));
         }
     };
     if (taskHandler_) {
