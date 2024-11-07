281572ca8bbf34ade397fbabd62de581b07d054a
sunxiaoqiang <sunxiaoqiang7@huawei.com>
2024-11-07 13:15:05 +0000

    update test/unittest/uri_permission_impl_test/mock/include/mock_storage_manager_service.h.
    Signed-off-by: sunxiaoqiang <sunxiaoqiang7@huawei.com>

--- test/unittest/uri_permission_impl_test/mock/include/mock_storage_manager_service.h
+++ test/unittest/uri_permission_impl_test/mock/include/mock_storage_manager_service.h
@@ -82,7 +82,8 @@ public:
         return E_OK;
     }
 
-    virtual int32_t GetBundleStats(std::string pkgName, BundleStats &bundleStats, int32_t index, uint32_t statFlag) override
+    virtual int32_t GetBundleStats(std::string pkgName, BundleStats &bundleStats,
+        int32_t index, uint32_t statFlag) override
     {
         return E_OK;
     }
