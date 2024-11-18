8f525fea212b60defb01adc2e946b0de21b5eb7f
zhoujing <zhoujing106@huawei.com>
2024-11-18 21:34:05 +0800

    [refactor]: Reduce public header files
    Signed-off-by: zhoujing <zhoujing106@huawei.com>
    Change-Id: I461bec0874b4c2520b31551400560c6bc8cf7ed6

--- frameworks/cj/ffi/application_context/include/cj_ability_lifecycle_callback.h
+++ frameworks/cj/ffi/application_context/include/cj_ability_lifecycle_callback.h
@@ -20,7 +20,6 @@
 #include <map>
 #include <memory>
 #include "cj_common_ffi.h"
-#include "ability_lifecycle_callback.h"
 
 using WindowStagePtr = void*;
 
@@ -219,67 +218,6 @@ public:
         WindowStagePtr windowStage) {}
     virtual void OnAbilityWillSaveState(const int64_t &ability) {}
     virtual void OnAbilitySaveState(const int64_t &ability) {}
-};
-
-class CjAbilityLifecycleCallbackImpl : public CjAbilityLifecycleCallback,
-                                       public std::enable_shared_from_this<CjAbilityLifecycleCallbackImpl> {
-public:
-    explicit CjAbilityLifecycleCallbackImpl() {};
-    virtual ~CjAbilityLifecycleCallbackImpl() {};
-
-    void OnAbilityCreate(const int64_t &ability) override;
-    void OnWindowStageCreate(const int64_t &ability, WindowStagePtr windowStage) override;
-    void OnWindowStageActive(const int64_t &ability, WindowStagePtr windowStage) override;
-    void OnWindowStageInactive(const int64_t &ability, WindowStagePtr windowStage) override;
-    void OnWindowStageDestroy(const int64_t &ability, WindowStagePtr windowStage) override;
-    void OnAbilityDestroy(const int64_t &ability) override;
-    void OnAbilityForeground(const int64_t &ability) override;
-    void OnAbilityBackground(const int64_t &ability) override;
-    void OnAbilityContinue(const int64_t &ability) override;
-    // optional callbacks
-    void OnAbilityWillCreate(const int64_t &ability) override;
-    void OnWindowStageWillCreate(const int64_t &ability, WindowStagePtr windowStage) override;
-    void OnWindowStageWillDestroy(const int64_t &ability, WindowStagePtr windowStage) override;
-    void OnAbilityWillDestroy(const int64_t &ability) override;
-    void OnAbilityWillForeground(const int64_t &ability) override;
-    void OnAbilityWillBackground(const int64_t &ability) override;
-    void OnNewWant(const int64_t &ability) override;
-    void OnWillNewWant(const int64_t &ability) override;
-    void OnAbilityWillContinue(const int64_t &ability) override;
-    void OnWindowStageWillRestore(const int64_t &ability, WindowStagePtr windowStage) override;
-    void OnWindowStageRestore(const int64_t &ability, WindowStagePtr windowStage) override;
-    void OnAbilityWillSaveState(const int64_t &ability) override;
-    void OnAbilitySaveState(const int64_t &ability) override;
-
-    int32_t Register(CArrI64 cFuncIds, bool isSync = false);
-    bool UnRegister(int32_t callbackId, bool isSync = false);
-    bool IsEmpty() const;
-    static int32_t serialNumber_;
-
-private:
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityCreatecallbacks_;
-    std::map<int32_t, std::function<void(int64_t, WindowStagePtr)>> onWindowStageCreatecallbacks_;
-    std::map<int32_t, std::function<void(int64_t, WindowStagePtr)>> onWindowStageActivecallbacks_;
-    std::map<int32_t, std::function<void(int64_t, WindowStagePtr)>> onWindowStageInactivecallbacks_;
-    std::map<int32_t, std::function<void(int64_t, WindowStagePtr)>> onWindowStageDestroycallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityDestroycallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityForegroundcallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityBackgroundcallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityContinuecallbacks_;
-    // optional callbacks
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityWillCreatecallbacks_;
-    std::map<int32_t, std::function<void(int64_t, WindowStagePtr)>> onWindowStageWillCreatecallbacks_;
-    std::map<int32_t, std::function<void(int64_t, WindowStagePtr)>> onWindowStageWillDestroycallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityWillForegroundcallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityWillDestroycallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityWillBackgroundcallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onWillNewWantcallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onNewWantcallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityWillContinuecallbacks_;
-    std::map<int32_t, std::function<void(int64_t, WindowStagePtr)>> onWindowStageWillRestorecallbacks_;
-    std::map<int32_t, std::function<void(int64_t, WindowStagePtr)>> onWindowStageRestorecallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onAbilityWillSaveStatecallbacks_;
-    std::map<int32_t, std::function<void(int64_t)>> onAbilitySaveStatecallbacks_;
 };
 }  // namespace AbilityRuntime
 }  // namespace OHOS
