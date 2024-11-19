534fe773eb64aee07485ad47c3c2772ac6f021ae
unknown <sijunjie@huawei.com>
2024-11-19 10:33:38 +0800

    fix upgrade
    Signed-off-by: unknown <sijunjie@huawei.com>

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -6671,7 +6671,8 @@ int32_t AbilityManagerService::UninstallAppInner(const std::string &bundleName, 
 
     CHECK_POINTER_AND_RETURN(subManagersHelper_, ERR_NULL_OBJECT);
     subManagersHelper_->UninstallApp(bundleName, uid);
-    int ret = DelayedSingleton<AppScheduler>::GetInstance()->KillApplicationByUid(bundleName, uid, "UninstallApp");
+    std::string killReason = isUpgrade ? "UpgradeApp" : "UninstallApp";
+    int ret = DelayedSingleton<AppScheduler>::GetInstance()->KillApplicationByUid(bundleName, uid, killReason);
     if (ret != ERR_OK) {
         return UNINSTALL_APP_FAILED;
     }
