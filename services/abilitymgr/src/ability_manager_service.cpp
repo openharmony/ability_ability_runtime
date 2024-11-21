d16d201fc9864bd83c55857e07380c1840118efb
zhang_hao_zheng <zhanghaozheng2@h-partners.com>
2024-11-21 16:17:03 +0800

    仅支持serviceExtension跨用户链接
    Signed-off-by: zhang_hao_zheng <zhanghaozheng2@h-partners.com>
    Change-Id: I7996099edbdbd7f07c0a578eb1a411c7475eced3

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -4201,8 +4201,7 @@ int32_t AbilityManagerService::ConnectLocalAbility(const Want &want, const int32
     TAG_LOGD(AAFwkTag::ABILITYMGR, "called");
     AbilityUtil::RemoveShowModeKey(const_cast<Want &>(want));
     bool isEnterpriseAdmin = AAFwk::UIExtensionUtils::IsEnterpriseAdmin(extensionType);
-    if (!(extensionType == AppExecFwk::ExtensionAbilityType::DATASHARE ||
-        extensionType == AppExecFwk::ExtensionAbilityType::SERVICE) &&
+    if (extensionType != AppExecFwk::ExtensionAbilityType::SERVICE &&
         !isEnterpriseAdmin && !JudgeMultiUserConcurrency(userId)) {
         TAG_LOGE(AAFwkTag::ABILITYMGR, "multi-user non-concurrent unsatisfied");
         return ERR_CROSS_USER;
