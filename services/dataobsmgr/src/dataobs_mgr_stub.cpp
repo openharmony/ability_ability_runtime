1149356e8a3f05d890446dfe19ed0df8128817d0
Axi_Beft <shuxin15@h-partners.com>
2024-11-25 22:05:54 +0800

    增加错误日志打印
    Signed-off-by: Axi_Beft <shuxin15@h-partners.com>

--- services/dataobsmgr/src/dataobs_mgr_stub.cpp
+++ services/dataobsmgr/src/dataobs_mgr_stub.cpp
@@ -145,6 +145,7 @@ int32_t DataObsManagerStub::NotifyChangeExtInner(MessageParcel &data, MessagePar
 {
     ChangeInfo changeInfo;
     if (!ChangeInfo::Unmarshalling(changeInfo, data)) {
+        TAG_LOGE(AAFwkTag::DBOBSMGR, "DataObsManagerStub::NotifyChangeExtInner: Failed to unmarshall changeInfo.");
         return IPC_STUB_INVALID_DATA_ERR;
     }
 
