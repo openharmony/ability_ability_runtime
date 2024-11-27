6a81af77aed67ea509765c26c02e43f5f6d4b237
chenzexin <chenzexin14@huawei.com>
2024-11-27 10:41:06 +0800

    also count EmbeddedUI inside UIability process
    Signed-off-by: chenzexin <chenzexin14@huawei.com>

--- services/appmgr/src/module_running_record.cpp
+++ services/appmgr/src/module_running_record.cpp
@@ -109,7 +109,8 @@ int32_t ModuleRunningRecord::GetPageAbilitySize()
     for (auto it : abilities_) {
         std::shared_ptr<AbilityRunningRecord> abilityRunningRecord = it.second;
         std::shared_ptr<AbilityInfo> abilityInfo = abilityRunningRecord->GetAbilityInfo();
-        if (abilityInfo && abilityInfo->type == AbilityType::PAGE) {
+        if (abilityInfo && (abilityInfo->type == AbilityType::PAGE ||
+            abilityInfo->extensionType == ExtensionAbilityType::EMBEDDED_UI)) {
             pageAbilitySize++;
         }
     }
