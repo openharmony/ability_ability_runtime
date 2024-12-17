7355e3059626b820efe3c5db14e270f267d4b490
郑冬冬冬 <zhengchengkai@huawei.com>
2024-12-17 10:03:53 +0800

    Message:fixCodes
    Signed-off-by: 郑冬冬冬 <zhengchengkai@huawei.com>

--- test/fuzztest/abilityappfreezemanager_fuzzer/abilityappfreezemanager_fuzzer.cpp
+++ test/fuzztest/abilityappfreezemanager_fuzzer/abilityappfreezemanager_fuzzer.cpp
@@ -70,7 +70,8 @@ bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
     freeze->ResetAppfreezeState(pid, bundleName);
     freeze->IsValidFreezeFilter(pid, bundleName);
     std::string stack(data, size);
-    freeze->GetBinderPeerPids(stack, pid);
+    std::set<int> asyncPids;
+    freeze->GetBinderPeerPids(stack, pid, asyncPids);
     std::string ret(data, size);
     freeze->FindStackByPid(ret, pid);
     std::string faultType(data, size);
