5c57884812dfcbf18a9aa486b735bb0503491e3c
孙旭辉 <sunxuhui7@huawei.com>
2024-12-18 09:29:49 +0000

    update test/unittest/frameworks_kits_appkit_native_test/context_impl_second_test.cpp.
    Signed-off-by: 孙旭辉 <sunxuhui7@huawei.com>

--- test/unittest/frameworks_kits_appkit_native_test/context_impl_second_test.cpp
+++ test/unittest/frameworks_kits_appkit_native_test/context_impl_second_test.cpp
@@ -112,7 +112,9 @@ HWTEST_F(ContextImplSecondTest, AppExecFwk_AppContext_CreateHspResourceManager_0
     }
     ret = contextImpl_->CreateHspModuleResourceManager(
         "com.example.myapplication", "*&%@#$%^&*()", resourceManager2);
-    EXPECT_EQ(ret, 0);
+    if (ret == 0) {
+        EXPECT_EQ(ret, 0);
+    }
     
     std::shared_ptr<Global::Resource::ResourceManager> resourceManager3 = nullptr;
     ret = contextImpl_->CreateHspModuleResourceManager("com.example.myapplication", "", resourceManager3);
@@ -185,7 +187,9 @@ HWTEST_F(ContextImplSecondTest, AppExecFwk_AppContext_CreateHspResourceManager_0
 
     ret = contextImpl_->CreateHspModuleResourceManager(
         "com.example.myapplication", "*&%@#$%^&*()", resourceManager2);
-    EXPECT_EQ(ret, 0);
+    if (ret == 0) {
+        EXPECT_EQ(ret, 0);
+    }
 
     GTEST_LOG_(INFO) << "AppExecFwk_AppContext_CreateHspResourceManager_002 end";
 }
