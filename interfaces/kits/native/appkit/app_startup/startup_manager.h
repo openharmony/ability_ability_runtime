61f1003bbe4e650422bf7ece102fe00bfe3671c7
liuzongze <liuzongze@huawei.com>
2024-12-20 15:34:40 +0800

    启动框架支持加载Har Hsp配置文件
    Signed-off-by: liuzongze <liuzongze@huawei.com>
    Change-Id: I2bf42dc2001d09e590d5104a3f2686b453401477

--- interfaces/kits/native/appkit/app_startup/startup_manager.h
+++ interfaces/kits/native/appkit/app_startup/startup_manager.h
@@ -25,9 +25,24 @@
 #include "startup_task.h"
 #include "startup_task_manager.h"
 #include "startup_utils.h"
+#include "hap_module_info.h"
+#include "native_engine/native_value.h"
+#include "js_runtime.h"
 
 namespace OHOS {
 namespace AbilityRuntime {
+
+struct AppPreloadHintStartupTask {
+    std::string name;
+    std::string srcEntry;
+    std::vector<std::string> dependencies;
+    bool excludeFromAutoStart;
+    std::string runOnThread;
+    std::string ohmurl;
+    std::string moduleType;
+    std::string taskType;
+};
+
 class StartupManager : public std::enable_shared_from_this<StartupManager> {
 DECLARE_DELAYED_SINGLETON(StartupManager)
 
@@ -54,17 +69,62 @@ public:
     int32_t IsInitialized(const std::string &name, bool &isInitialized);
 
     int32_t PostMainThreadTask(const std::function<void()> &task);
+    
+    int32_t GetStartupConfigString(const AppExecFwk::HapModuleInfo &info, std::string &config);
+
+    bool AnalyzeStartupConfig(
+        const std::string &startupConfig,
+        AppExecFwk::ModuleType moduleType,
+        JsRuntime &jsRuntime,
+        std::unique_ptr<NativeReference> &startupJsRef,
+        std::shared_ptr<NativeReference> &shellContextRef);
+
+    bool LoadJsStartupConfig(const std::string &srcEntry);
+
+    void LoadJsSrcEntry(const std::string &srcEntry);
+    
+    void LoadJsByOhmurl(const std::string &srcEntry);
 
 private:
     uint32_t startupTaskManagerId = 0;
     std::map<uint32_t, std::shared_ptr<StartupTaskManager>> startupTaskManagerMap_;
     // read only after initialization
     std::map<std::string, std::shared_ptr<StartupTask>> startupTasks_;
+    std::map<std::string, std::shared_ptr<AppPreloadHintStartupTask>> appPreloadHintStartupTask_;
     std::shared_ptr<StartupConfig> defaultConfig_;
     std::shared_ptr<AppExecFwk::EventHandler> mainHandler_;
 
     int32_t AddStartupTask(const std::string &name,
         std::map<std::string, std::shared_ptr<StartupTask>> &taskMap);
+
+    bool AddStartupTask(
+        nlohmann::json &startupConfigJson,
+        AppExecFwk::ModuleType moduleType,
+        JsRuntime &jsRuntime,
+        std::unique_ptr<NativeReference> &startupJsRef,
+        std::shared_ptr<NativeReference> &shellContextRef);
+    
+    bool AddPreloadHintStartupTask(nlohmann::json &startupConfigJson, AppExecFwk::ModuleType moduleType);
+
+    std::shared_ptr<StartupTask> AnalyzeStartupTasks(
+        const nlohmann::json &startupTaskJson,
+        AppExecFwk::ModuleType moduleType,
+        JsRuntime &jsRuntime,
+        std::unique_ptr<NativeReference> &startupJsRef,
+        std::shared_ptr<NativeReference> &shellContextRef);
+
+    std::shared_ptr<AppPreloadHintStartupTask> AnalyzeAppPreloadHintStartupTasks(
+        const nlohmann::json &preloadStartupTaskJson, AppExecFwk::ModuleType moduleType);
+
+    void SetOptionalParameters(
+        const nlohmann::json &module,
+        std::shared_ptr<StartupTask> startupTask,
+        AppExecFwk::ModuleType moduleType);
+
+    void SetOptionalParameters(
+        const nlohmann::json &module,
+        std::shared_ptr<AppPreloadHintStartupTask> appPreloadHintStartupTask,
+        AppExecFwk::ModuleType moduleType);
 };
 } // namespace AbilityRuntime
 } // namespace OHOS
