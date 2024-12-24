765af5c9152433aef85a4f5d384ea095c401f235
zhuhan <zhuhan10@huawei.com>
2024-12-24 15:24:56 +0800

    want fds
    Signed-off-by: zhuhan <zhuhan10@huawei.com>
    Change-Id: Iac5e2fc16b450632cd691d55559cfda164bd9501

--- frameworks/js/napi/inner/napi_common/napi_common_want.h
+++ frameworks/js/napi/inner/napi_common/napi_common_want.h
@@ -38,6 +38,8 @@ bool BlackListFilter(const std::string &strProName);
 napi_value WrapWant(napi_env env, const AAFwk::Want &want);
 bool UnwrapWant(napi_env env, napi_value param, AAFwk::Want &want);
 
+napi_value WrapWantParamsFD(napi_env env, const AAFwk::WantParams &wantParams);
+
 napi_value WrapAbilityResult(napi_env env, const int &resultCode, const AAFwk::Want &want);
 bool UnWrapAbilityResult(napi_env env, napi_value param, int &resultCode, AAFwk::Want &want);
 
