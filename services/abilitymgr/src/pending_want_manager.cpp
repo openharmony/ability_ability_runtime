6cabc3ca9153a28930769bad8991fa00c730f47e
linjunjie <linjunjie6@huawei.com>
2024-12-25 19:48:01 +0800

    修复系统应用无法cancel三方应用wantagent问题
    Signed-off-by: linjunjie <linjunjie6@huawei.com>

--- services/abilitymgr/src/pending_want_manager.cpp
+++ services/abilitymgr/src/pending_want_manager.cpp
@@ -216,7 +216,7 @@ int32_t PendingWantManager::SendWantSender(sptr<IWantSender> target, const Sende
     return record->SenderInner(info);
 }
 
-void PendingWantManager::CancelWantSender(const bool isSystemApp, const sptr<IWantSender> &sender)
+void PendingWantManager::CancelWantSender(const bool isSystemAppCall, const sptr<IWantSender> &sender)
 {
     TAG_LOGD(AAFwkTag::WANTAGENT, "begin");
 
@@ -226,7 +226,7 @@ void PendingWantManager::CancelWantSender(const bool isSystemApp, const sptr<IWa
     }
 
     auto isSaCall = AAFwk::PermissionVerification::GetInstance()->IsSACall();
-    if (!isSaCall && !isSystemApp) {
+    if (!isSaCall && !isSystemAppCall) {
         TAG_LOGE(AAFwkTag::WANTAGENT, "is not allowed to send");
         return;
     }
