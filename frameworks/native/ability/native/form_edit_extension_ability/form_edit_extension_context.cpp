d981436dd031e6624aaac09dd67b4cee324c3884
zhangying <42703582@qq.com>
2025-01-19 15:25:48 +0800

    卡片编辑新增FormEditExtensionAbility能力
    Signed-off-by: zhangying <42703582@qq.com>

--- frameworks/native/ability/native/form_edit_extension_ability/form_edit_extension_context.cpp
+++ frameworks/native/ability/native/form_edit_extension_ability/form_edit_extension_context.cpp
@@ -44,11 +44,11 @@ FormEditErrorCode FormEditExtensionContext::OnStartSceneBoard(const AAFwk::Want 
     wantToHost.SetAction(Constants::FORM_PUBLISH_ACTION);
 
     TAG_LOGI(AAFwkTag::UI_EXT, "OnStartSceneBoard wantToHost: %{public}s", wantToHost.ToString().c_str());
-    ErrCode err = AppExecFwk::FormMgr::GetInstance().StartAbilityByFms(wantToHost);
-    if (err != ERR_OK) {
-        TAG_LOGE(AAFwkTag::UI_EXT, "ret = %{public}d", err);
-        return FormEditErrorCode::ERROR_CODE_INTERNAL_ERROR;
-    }
+    // ErrCode err = AppExecFwk::FormMgr::GetInstance().StartAbilityByFms(wantToHost);
+    // if (err != ERR_OK) {
+    //     TAG_LOGE(AAFwkTag::UI_EXT, "ret = %{public}d", err);
+    //     return FormEditErrorCode::ERROR_CODE_INTERNAL_ERROR;
+    // }
     TAG_LOGI(AAFwkTag::UI_EXT, "OnStartSceneBoard success");
     return FormEditErrorCode::ERROR_OK;
 }
