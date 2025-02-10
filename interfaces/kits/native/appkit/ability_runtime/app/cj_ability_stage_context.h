b1aa1a039dd7dde189d996969fb8b888603f4db8
罗明 <luoming25@huawei.com>
2025-02-10 16:57:07 +0800

    消除cj_ability_ffi和cj_bundle_manager_ffi的依赖
    Signed-off-by: 罗明 <luoming25@huawei.com>

--- interfaces/kits/native/appkit/ability_runtime/app/cj_ability_stage_context.h
+++ interfaces/kits/native/appkit/ability_runtime/app/cj_ability_stage_context.h
@@ -18,7 +18,7 @@
 
 #include <cstdint>
 
-#include "bundle_manager_convert.h"
+#include "cj_bundle_info.h"
 #include "cj_common_ffi.h"
 #include "cj_utils_ffi.h"
 #include "cj_context.h"
@@ -30,7 +30,6 @@ namespace OHOS {
 namespace AbilityRuntime {
 
 class Context;
-using OHOS::CJSystemapi::BundleManager::RetHapModuleInfo;
 
 class CJAbilityStageContext : public FfiContext::CJContext {
 public:
@@ -48,6 +47,8 @@ public:
 private:
     std::weak_ptr<AbilityRuntime::Context> abilityStageContext_;
 };
+
+CConfiguration CallConvertConfig(std::shared_ptr<AppExecFwk::Configuration> configuration);
 }
 }
 #endif //OHOS_ABILITY_RUNTIME_CJ_ABILITY_STAGE_CONTEXT_H
