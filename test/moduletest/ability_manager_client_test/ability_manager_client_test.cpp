b7cd41fd460676cfdeeecd00b190cbdfd616d43d
chenliming <chenliming20@huawei-partners.com>
2025-02-13 15:10:41 +0800

    Js_Runtime AbilityContext Ability_Manager_client tdd 补充
    Signed-off-by: chenliming <chenliming20@huawei-partners.com>

--- test/moduletest/ability_manager_client_test/ability_manager_client_test.cpp
+++ test/moduletest/ability_manager_client_test/ability_manager_client_test.cpp
@@ -293,8 +293,8 @@ HWTEST_F(AbilityManagerClientTest, AbilityManagerClient_OpenLink, TestSize.Level
 }
 
 /**
- * @tc.name: StartSelfUIAbility_0100
- * @tc.desc: OpenLink
+ * @tc.name: AbilityManagerClient_StartSelfUIAbility_0100
+ * @tc.desc: StartSelfUIAbility
  * @tc.type: FUNC
  */
 HWTEST_F(AbilityManagerClientTest, StartSelfUIAbility_0100, TestSize.Level1)
@@ -310,8 +310,8 @@ HWTEST_F(AbilityManagerClientTest, StartSelfUIAbility_0100, TestSize.Level1)
 
 /**
  * @tc.name: AddQueryERMSObserver_0100
- * @tc.desc: OpenLink
- * @tc.type: FUNC
+ * @tc.name: AbilityManagerClient_AddQueryERMSObserver_0100
+ * @tc.desc: AddQueryERMSObserver
  */
 HWTEST_F(AbilityManagerClientTest, AddQueryERMSObserver_0100, TestSize.Level1)
 {
@@ -325,8 +325,8 @@ HWTEST_F(AbilityManagerClientTest, AddQueryERMSObserver_0100, TestSize.Level1)
 }
 
 /**
- * @tc.name: QueryAtomicServiceStartupRule_0100
- * @tc.desc: OpenLink
+  * @tc.name: AbilityManagerClient_QueryAtomicServiceStartupRule_0100
+ * @tc.desc: QueryAtomicServiceStartupRule
  * @tc.type: FUNC
  */
 HWTEST_F(AbilityManagerClientTest, QueryAtomicServiceStartupRule_0100, TestSize.Level1)
@@ -343,18 +343,30 @@ HWTEST_F(AbilityManagerClientTest, QueryAtomicServiceStartupRule_0100, TestSize.
 }
 
 /**
- * @tc.name: KillProcessWithReason_0100
- * @tc.desc: OpenLink
+ * @tc.name: AbilityManagerClient_KillProcessWithReason_0100
+ * @tc.name: AbilityManagerClient_KillProcessWithPrepareTerminateDone_0100
+ * @tc.desc: KillProcessWithReason and KillProcessWithPrepareTerminateDone
  * @tc.type: FUNC
  */
 HWTEST_F(AbilityManagerClientTest, KillProcessWithReason_0100, TestSize.Level1)
 {
     TAG_LOGI(AAFwkTag::TEST, "KillProcessWithReason_0100 start");
+
+    std::string moduleName = "com.ohos.example.moduleName";
+    int32_t prepareTermination = 1;
+    bool isExist = false;
+    AbilityManagerClient::GetInstance()->KillProcessWithPrepareTerminateDone(moduleName,
+        prepareTermination, isExist);
+
     int32_t pid = 1;
     AAFwk::ExitReason reason;
     std::shared_ptr<AbilityManagerClient> client = std::make_shared<AbilityManagerClient>();
     client->KillProcessWithReason(pid, reason);
     EXPECT_TRUE(client != nullptr);
+    auto result = AbilityManagerClient::GetInstance()->KillProcessWithReason(pid, reason);
+    EXPECT_EQ(result, ERR_OK);
+
+    TAG_LOGI(AAFwkTag::TEST, "KillProcessWithReason_0100 end");
 }
 }  // namespace AAFwk
 }  // namespace OHOS
