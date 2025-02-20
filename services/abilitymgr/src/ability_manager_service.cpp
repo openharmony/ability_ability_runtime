a07535da0f8cc254feca3371e82d144408b9ee80
yzx <yangzixuan12@huawei.com>
2025-02-20 02:57:31 +0000

    update services/abilitymgr/src/ability_manager_service.cpp.
    Signed-off-by: yzx <yangzixuan12@huawei.com>

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -1317,8 +1317,7 @@ int AbilityManagerService::StartAbilityInner(const Want &want, const sptr<IRemot
             abilityRequest.want.RemoveParam(DMS_CONTINUED_SESSION_ID);
             abilityRequest.want.RemoveParam(DMS_PERSISTENT_ID);
         }
-        if (abilityRecord != nullptr &&
-                PermissionVerification::GetInstance()->VerifyFusionAccessPermission() &&
+        if (PermissionVerification::GetInstance()->VerifyFusionAccessPermission() &&
                 abilityRequest.want.GetBoolParam(PARAM_RESV_ANCO_IS_NEED_UPDATE_NAME, false)) {
             abilityRequest.want.SetParam(Want::PARAM_RESV_CALLER_ABILITY_NAME, std::string(""));
             abilityRequest.want.SetParam(Want::PARAM_RESV_CALLER_BUNDLE_NAME, std::string(""));
