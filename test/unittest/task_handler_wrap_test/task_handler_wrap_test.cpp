e8dc23b07a2bbe2fa6072cb404dc201317807f1d
chenliming <chenliming20@huawei-partners.com>
2025-03-06 10:31:05 +0800

    add tdd
    Signed-off-by: chenliming <chenliming20@huawei-partners.com>

--- test/unittest/task_handler_wrap_test/task_handler_wrap_test.cpp
+++ test/unittest/task_handler_wrap_test/task_handler_wrap_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,7 +14,11 @@
  */
 
 #include <gtest/gtest.h>
+
+#include "ffrt_task_utils_wrap.h"
+#include "queue_task_handler_wrap.h"
 #include "task_handler_wrap.h"
+#include "task_utils_wrap.h"
 
 using namespace testing;
 using namespace testing::ext;
@@ -221,5 +225,86 @@ HWTEST_F(TaskHandlerWrapTest, FfrtTest_0040, TestSize.Level0)
     EXPECT_TRUE(result);
 }
 
+/**
+ * @tc.name: RemoveTask_0050
+ * @tc.desc: Test removing a task from the task handler after submission.
+ * @tc.type: FUNC
+ */
+HWTEST_F(TaskHandlerWrapTest, RemoveTask_0050, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "RemoveTask_0050 start";
+
+    EXPECT_NE(ffrtHandler_, nullptr);
+
+    const std::string name = "test";
+    TaskHandle taskHandle;
+    EXPECT_FALSE(ffrtHandler_->RemoveTask(name, taskHandle));
+
+    std::function<void()> taskWrap = []() {};
+    TaskAttribute taskAttr;
+    std::shared_ptr<InnerTaskHandle> innerTaskHandle = ffrtHandler_->SubmitTaskInner(std::move(taskWrap), taskAttr);
+    EXPECT_NE(innerTaskHandle, nullptr);
+    TaskHandle taskHandleTest(nullptr, innerTaskHandle);
+    ffrtHandler_->tasks_.emplace(name, taskHandleTest);
+    EXPECT_FALSE(ffrtHandler_->RemoveTask(name, taskHandle));
+
+    EXPECT_EQ(ffrtHandler_->tasks_.size(), 1);
+    EXPECT_TRUE(ffrtHandler_->RemoveTask(name, taskHandleTest));
+    EXPECT_EQ(ffrtHandler_->tasks_.size(), 0);
+
+    GTEST_LOG_(INFO) << "RemoveTask_0050 end";
+}
+
+/**
+ * @tc.name: Convert2FfrtQosAndConvert2FfrtPriority_0060
+ * @tc.desc: Verify the conversion of task QoS and priority to ffrt equivalents.
+ * @tc.type: FUNC
+ */
+HWTEST_F(TaskHandlerWrapTest, Convert2FfrtQosAndConvert2FfrtPriority_0060, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "Convert2FfrtQosAndConvert2FfrtPriority_0060 start";
+
+    EXPECT_EQ(Convert2FfrtQos(TaskQoS::INHERENT), ffrt::qos_inherit);
+    EXPECT_EQ(Convert2FfrtQos(TaskQoS::BACKGROUND), ffrt::qos_background);
+    EXPECT_EQ(Convert2FfrtQos(TaskQoS::UTILITY), ffrt::qos_utility);
+    EXPECT_EQ(Convert2FfrtQos(TaskQoS::DEFAULT), ffrt::qos_default);
+    EXPECT_EQ(Convert2FfrtQos(TaskQoS::USER_INITIATED), ffrt::qos_user_initiated);
+    EXPECT_EQ(Convert2FfrtQos(TaskQoS::DEADLINE_REQUEST), ffrt::qos_deadline_request);
+    EXPECT_EQ(Convert2FfrtQos(TaskQoS::USER_INTERACTIVE), ffrt::qos_user_interactive);
+    EXPECT_EQ(Convert2FfrtPriority(TaskQueuePriority::IMMEDIATE), ffrt_queue_priority_t::ffrt_queue_priority_immediate);
+    EXPECT_EQ(Convert2FfrtPriority(TaskQueuePriority::HIGH), ffrt_queue_priority_t::ffrt_queue_priority_high);
+    EXPECT_EQ(Convert2FfrtPriority(TaskQueuePriority::LOW), ffrt_queue_priority_t::ffrt_queue_priority_low);
+    EXPECT_EQ(Convert2FfrtPriority(TaskQueuePriority::IDLE), ffrt_queue_priority_t::ffrt_queue_priority_idle);
+
+    GTEST_LOG_(INFO) << "Convert2FfrtQosAndConvert2FfrtPriority_0060 end";
+}
+
+/**
+ * @tc.name: BuildFfrtTaskAttr_0070
+ * @tc.desc: Test building task attributes and ensure task execution occurs after delay.
+ * @tc.type: FUNC
+ */
+HWTEST_F(TaskHandlerWrapTest, BuildFfrtTaskAttr_0070, TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "BuildFfrtTaskAttr_0070 start";
+
+    EXPECT_NE(ffrtHandler_, nullptr);
+    std::shared_ptr<QueueTaskHandlerWrap> queueTaskHandlerWrap =
+        std::static_pointer_cast<QueueTaskHandlerWrap>(ffrtHandler_);
+    EXPECT_NE(queueTaskHandlerWrap, nullptr);
+    int ret = 0;
+    std::function<void()> task = [&ret]() { ret++; };
+    TaskAttribute taskAttr;
+    taskAttr.delayMillis_ = 1;
+    taskAttr.taskName_ = "test";
+    taskAttr.taskQos_ = TaskQoS::INHERENT;
+    taskAttr.taskPriority_ = TaskQueuePriority::IMMEDIATE;
+    taskAttr.timeoutMillis_ = 1;
+    auto innerTaskHandle = queueTaskHandlerWrap->SubmitTaskInner(std::move(task), taskAttr);
+    EXPECT_NE(innerTaskHandle, nullptr);
+    EXPECT_NE(ret, 1);
+
+    GTEST_LOG_(INFO) << "BuildFfrtTaskAttr_0070 end";
+}
 }  // namespace AAFwk
 }  // namespace OHOS
