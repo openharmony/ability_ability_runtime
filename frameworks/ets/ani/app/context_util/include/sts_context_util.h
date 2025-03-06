c2d70f54212e135f99711eb6e111c9c588c9dc4b
yuwenze <yuwenze1@huawei.com>
2025-03-06 11:45:01 +0800

    add resource manager
    Signed-off-by: yuwenze <yuwenze1@huawei.com>
    Change-Id: Ifb42a5ce5d7b107036128c7d23cf3e0bd7fe1262

---
+++ frameworks/ets/ani/app/context_util/include/sts_context_util.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_ABILITY_RUNTIME_STS_CONTEXT_UTIL_H
+#define OHOS_ABILITY_RUNTIME_STS_CONTEXT_UTIL_H
+
+#include "sts_runtime.h"
+#include "context.h"
+#include "extension_context.h"
+
+namespace OHOS {
+namespace AbilityRuntime {
+namespace ContextUtil {
+void BindApplicationCtx(ani_env* aniEnv, ani_class contextClass, ani_object contextObj,
+    void* applicationCtxRef);
+
+void SetArea([[maybe_unused]]ani_env *env, [[maybe_unused]]ani_object aniObj, ani_int value);
+
+void BindMethods(ani_env* aniEnv, ani_class contextClass);
+
+void BindApplicationInfo(ani_env* aniEnv, ani_class contextClass, ani_object contextObj,
+    std::shared_ptr<Context> context);
+
+void BindResourceManager(ani_env* aniEnv, ani_class contextClass, ani_object contextObj,
+    std::shared_ptr<Context> context);
+
+void BindParentProperty(ani_env* aniEnv, ani_class contextClass, ani_object contextObj,
+    std::shared_ptr<Context> context);
+
+void StsCreatContext(ani_env* aniEnv, ani_class contextClass, ani_object contextObj,
+    void* applicationCtxRef, std::shared_ptr<Context> context);
+
+void BindExtensionInfo(ani_env* aniEnv, ani_class contextClass, ani_object contextObj,
+    std::shared_ptr<AbilityRuntime::Context> context, std::shared_ptr<OHOS::AppExecFwk::AbilityInfo> abilityInfo);
+
+void StsCreatExtensionContext(ani_env* aniEnv, ani_class contextClass, ani_object contextObj,
+    void* applicationCtxRef, std::shared_ptr<AbilityRuntime::ExtensionContext> context);
+}
+} // namespace AbilityRuntime
+} // namespace OHOS
+#endif // OHOS_ABILITY_RUNTIME_STS_CONTEXT_UTIL_H
