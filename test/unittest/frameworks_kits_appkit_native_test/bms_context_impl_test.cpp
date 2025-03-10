ad5e65c66f042cb959c40d9671c50d4cbc275ba3
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-03-10 20:07:09 +0800

    独立编译整改
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- test/unittest/frameworks_kits_appkit_native_test/bms_context_impl_test.cpp
+++ test/unittest/frameworks_kits_appkit_native_test/bms_context_impl_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -29,6 +29,7 @@
 #include "mock_system_ability_manager.h"
 #include "sys_mgr_client.h"
 #include "system_ability_definition.h"
+#include "configuration.h"
 
 namespace OHOS {
 sptr<MockBundleInstallerService> mockBundleInstaller = new (std::nothrow) MockBundleInstallerService();
