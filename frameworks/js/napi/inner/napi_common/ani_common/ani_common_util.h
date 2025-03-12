96cfce8e748aa9f40cc529ce467115830765051b
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-03-12 16:09:05 +0800

    upd want
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- frameworks/js/napi/inner/napi_common/ani_common/ani_common_util.h
+++ frameworks/js/napi/inner/napi_common/ani_common/ani_common_util.h
@@ -21,7 +21,7 @@ namespace OHOS {
 namespace AppExecFwk {
 #define SETTER_METHOD_NAME(property) "<set>" #property
 
-bool GetIntOrUndefined(ani_env *env, ani_object param, const char *name, int &value);
+ani_int GetIntOrUndefined(ani_env *env, ani_object param, const char *name);
 double GetDoubleOrUndefined(ani_env *env, ani_object param, const char *name);
 bool GetBoolOrUndefined(ani_env *env, ani_object param, const char *name);
 bool GetStringOrUndefined(ani_env *env, ani_object param, const char *name, std::string &res);
