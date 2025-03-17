be46a167400ba10043415415732f695bf24f0189
wangzhen <wangzhen416@huawei.com>
2025-03-17 21:50:15 +0800

    Animation bug
    Signed-off-by: wangzhen <wangzhen416@huawei.com>

--- services/abilitymgr/src/scene_board/ui_ability_lifecycle_manager.cpp
+++ services/abilitymgr/src/scene_board/ui_ability_lifecycle_manager.cpp
@@ -3333,6 +3333,7 @@ void UIAbilityLifecycleManager::StartSpecifiedRequest(SpecifiedRequest &specifie
             sessionInfo->requestCode = request.requestCode;
             sessionInfo->userId = userId_;
             sessionInfo->requestId = specifiedRequest.requestId;
+            sessionInfo->isFromIcon = request.isFromIcon;
             TAG_LOGI(AAFwkTag::ABILITYMGR, "StartSpecifiedRequest cold");
             NotifySCBPendingActivation(sessionInfo, request);
             sessionInfo->want.RemoveAllFd();
