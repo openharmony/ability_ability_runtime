cca143e62aeae4ecb37502afc81cbd6f95c657e7
chenliming <chenliming20@huawei-partners.com>
2025-03-26 17:33:21 +0800

    TDD_2025_03_26
    Signed-off-by: chenliming <chenliming20@huawei-partners.com>

--- test/unittest/frameworks_kits_ability_native_test/ability_second_test.cpp
+++ test/unittest/frameworks_kits_ability_native_test/ability_second_test.cpp
@@ -15,12 +15,8 @@
 
 #include <gtest/gtest.h>
 
-#define private public
-#define protected public
 #include "ability.h"
 #include "data_ability_operation.h"
-#undef protected
-#undef private
 #include "ability_context_impl.h"
 #include "ability_handler.h"
 #include "ability_recovery.h"
@@ -30,6 +26,7 @@
 #include "ohos_application.h"
 #include "runtime.h"
 #include "values_bucket.h"
+#include "abs_shared_result_set.h"
 
 using namespace testing;
 using namespace testing::ext;
@@ -82,7 +79,7 @@ std::shared_ptr<DataAbilityOperation> CreateOperation(
     auto values = std::make_shared<OHOS::NativeRdb::ValuesBucket>();
     std::vector<uint8_t> value = {0x1, 0x2};
 
-    switch(type){
+    switch (type) {
         case Flagtype::TYPE_INT:
             values->PutInt(TEST_PHONE_NUMBER, TEST_NUMBER_100);
             break;
@@ -407,5 +404,252 @@ HWTEST_F(AbilitySecondTest, Ability_ParseValuesBucketReference_0100, TestSize.Le
 
     GTEST_LOG_(INFO) << "Ability_ParseValuesBucketReference_0100 end";
 }
+
+/**
+ * @tc.number: Ability_ChangeRef2Value_0100
+ * @tc.name: ChangeRef2Value
+ * @tc.desc: NA
+ */
+HWTEST_F(AbilitySecondTest, Ability_ChangeRef2Value_0100, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Ability_ChangeRef2Value_0100 start";
+
+    std::shared_ptr<Ability> ability = std::make_shared<Ability>();
+    ASSERT_NE(ability, nullptr);
+
+    std::vector<std::shared_ptr<DataAbilityResult>> results;
+    int numRefs = 0;
+    int index = 0;
+    auto ret = ability->ChangeRef2Value(results, numRefs, index);
+    EXPECT_EQ(ret, -1);
+
+    numRefs++;
+    ret = ability->ChangeRef2Value(results, numRefs, index);
+    EXPECT_EQ(ret, -1);
+
+    auto tmp = std::make_shared<DataAbilityResult>(0);
+    results.push_back(tmp);
+    ret = ability->ChangeRef2Value(results, numRefs, index);
+    EXPECT_EQ(ret, 0);
+
+    Uri uri("test");
+    tmp = std::make_shared<DataAbilityResult>(uri, 0);
+    results.push_back(tmp);
+    numRefs++;
+    index++;
+    ret = ability->ChangeRef2Value(results, numRefs, index);
+    EXPECT_EQ(ret, -1);
+
+    results.push_back(nullptr);
+    numRefs++;
+    index++;
+    ret = ability->ChangeRef2Value(results, numRefs, index);
+    EXPECT_EQ(ret, -1);
+
+    GTEST_LOG_(INFO) << "Ability_ChangeRef2Value_0100 end";
+}
+
+/**
+ * @tc.number: Ability_CheckAssertQueryResult_0100
+ * @tc.name: CheckAssertQueryResult
+ * @tc.desc: NA
+ */
+HWTEST_F(AbilitySecondTest, Ability_CheckAssertQueryResult_0100, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Ability_CheckAssertQueryResult_0100 start";
+
+    std::shared_ptr<Ability> ability = std::make_shared<Ability>();
+    ASSERT_NE(ability, nullptr);
+
+    std::shared_ptr<NativeRdb::AbsSharedResultSet> queryResult = nullptr;
+    auto ret = ability->CheckAssertQueryResult(queryResult, nullptr);
+    EXPECT_EQ(ret, true);
+
+    queryResult = std::make_shared<NativeRdb::AbsSharedResultSet>();
+    ret = ability->CheckAssertQueryResult(queryResult, nullptr);
+    EXPECT_EQ(ret, true);
+
+    auto operation = std::make_shared<DataAbilityOperation>();
+    ret = ability->CheckAssertQueryResult(queryResult, operation->GetValuesBucket());
+    EXPECT_EQ(ret, true);
+
+    GTEST_LOG_(INFO) << "Ability_CheckAssertQueryResult_0100 end";
+}
+
+/**
+ * @tc.number: Ability_SetMissionLabel_0100
+ * @tc.name: SetMissionLabel
+ * @tc.desc: NA
+ */
+HWTEST_F(AbilitySecondTest, Ability_SetMissionLabel_0100, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Ability_SetMissionLabel_0100 start";
+
+    std::shared_ptr<Ability> ability = std::make_shared<Ability>();
+    ASSERT_NE(ability, nullptr);
+
+    std::string label = "test";
+    auto ret = ability->SetMissionLabel(label);
+    EXPECT_EQ(ret, -1);
+
+    std::shared_ptr<AbilityInfo> abilityInfo = std::make_shared<AbilityInfo>();
+    abilityInfo->type = AppExecFwk::AbilityType::PAGE;
+    abilityInfo->isStageBasedModel = true;
+    auto eventRunner = EventRunner::Create(abilityInfo->name);
+    auto handler = std::make_shared<AbilityHandler>(eventRunner);
+    ability->Init(abilityInfo, nullptr, handler, nullptr);
+    ret = ability->SetMissionLabel(label);
+    EXPECT_EQ(ret, -1);
+
+    ability->scene_ = std::make_shared<Rosen::WindowScene>();
+    ret = ability->SetMissionLabel(label);
+    EXPECT_EQ(ret, -1);
+
+    int32_t displayId = 0;
+    std::shared_ptr<AbilityRuntime::Context> context = std::make_shared<AbilityRuntime::ContextImpl>();
+    sptr<Rosen::IWindowLifeCycle> listener = new Rosen::IWindowLifeCycle();
+    sptr<Rosen::WindowOption> option = new Rosen::WindowOption();
+    ability->scene_->Init(displayId, context, listener, option);
+    ret = ability->SetMissionLabel(label);
+    EXPECT_EQ(ret, -1);
+
+    ability->scene_->mainWindow_ = new Rosen::Window();
+    ret = ability->SetMissionLabel(label);
+    EXPECT_EQ(ret, ERR_OK);
+
+    abilityInfo->isStageBasedModel = false;
+    ability->Init(abilityInfo, nullptr, handler, nullptr);
+    ret = ability->SetMissionLabel(label);
+    EXPECT_EQ(ret, -1);
+
+    auto tmp = ability->abilityWindow_;
+    ability->abilityWindow_ = nullptr;
+    ret = ability->SetMissionLabel(label);
+    ability->abilityWindow_ = tmp;
+    EXPECT_EQ(ret, -1);
+
+    GTEST_LOG_(INFO) << "Ability_SetMissionLabel_0100 end";
+}
+
+/**
+ * @tc.number: Ability_SetMissionIcon_0100
+ * @tc.name: SetMissionIcon
+ * @tc.desc: NA
+ */
+HWTEST_F(AbilitySecondTest, Ability_SetMissionIcon_0100, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Ability_SetMissionIcon_0100 start";
+
+    std::shared_ptr<Ability> ability = std::make_shared<Ability>();
+    ASSERT_NE(ability, nullptr);
+
+    auto icon = std::make_shared<Media::PixelMap>();
+    auto ret = ability->SetMissionIcon(icon);
+    EXPECT_EQ(ret, -1);
+
+    std::shared_ptr<AbilityInfo> abilityInfo = std::make_shared<AbilityInfo>();
+    abilityInfo->type = AppExecFwk::AbilityType::PAGE;
+    abilityInfo->isStageBasedModel = true;
+    auto eventRunner = EventRunner::Create(abilityInfo->name);
+    auto handler = std::make_shared<AbilityHandler>(eventRunner);
+    ability->Init(abilityInfo, nullptr, handler, nullptr);
+    ret = ability->SetMissionIcon(icon);
+    EXPECT_EQ(ret, -1);
+
+    ability->scene_ = std::make_shared<Rosen::WindowScene>();
+    ret = ability->SetMissionIcon(icon);
+    EXPECT_EQ(ret, -1);
+
+    int32_t displayId = 0;
+    std::shared_ptr<AbilityRuntime::Context> context = std::make_shared<AbilityRuntime::ContextImpl>();
+    sptr<Rosen::IWindowLifeCycle> listener = new Rosen::IWindowLifeCycle();
+    sptr<Rosen::WindowOption> option = new Rosen::WindowOption();
+    ability->scene_->Init(displayId, context, listener, option);
+    ret = ability->SetMissionIcon(icon);
+    EXPECT_EQ(ret, -1);
+
+    ability->scene_->mainWindow_ = new Rosen::Window();
+    ret = ability->SetMissionIcon(icon);
+    EXPECT_EQ(ret, ERR_OK);
+
+    abilityInfo->isStageBasedModel = false;
+    ability->Init(abilityInfo, nullptr, handler, nullptr);
+    ret = ability->SetMissionIcon(icon);
+    EXPECT_EQ(ret, -1);
+
+    auto tmp = ability->abilityWindow_;
+    ability->abilityWindow_ = nullptr;
+    ret = ability->SetMissionIcon(icon);
+    ability->abilityWindow_ = tmp;
+    EXPECT_EQ(ret, -1);
+
+    GTEST_LOG_(INFO) << "Ability_SetMissionIcon_0100 end";
+}
+
+/**
+ * @tc.number: Ability_GetDisplayOrientation_0100
+ * @tc.name: GetDisplayOrientation
+ * @tc.desc: NA
+ */
+HWTEST_F(AbilitySecondTest, Ability_GetDisplayOrientation_0100, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Ability_GetDisplayOrientation_0100 start";
+
+    std::shared_ptr<Ability> ability = std::make_shared<Ability>();
+    ASSERT_NE(ability, nullptr);
+
+    auto ret = ability->GetDisplayOrientation();
+    EXPECT_EQ(ret, -1);
+
+    auto abilityInfo = std::make_shared<AbilityInfo>();
+    abilityInfo->type = AppExecFwk::AbilityType::PAGE;
+    auto eventRunner = EventRunner::Create(abilityInfo->name);
+    auto handler = std::make_shared<AbilityHandler>(eventRunner);
+    ability->Init(abilityInfo, nullptr, handler, nullptr);
+    ret = ability->GetDisplayOrientation();
+    EXPECT_EQ(ret, -1);
+
+    int32_t displayId = 0;
+    sptr<Rosen::WindowOption> option = new Rosen::WindowOption();
+    ability->InitWindow(displayId, option);
+    ability->SetDisplayOrientation(static_cast<int>(DisplayOrientation::LANDSCAPE));
+    ret = ability->GetDisplayOrientation();
+    EXPECT_EQ(ret, static_cast<int>(DisplayOrientation::LANDSCAPE));
+
+    ability->SetDisplayOrientation(static_cast<int>(DisplayOrientation::PORTRAIT));
+    ret = ability->GetDisplayOrientation();
+    EXPECT_EQ(ret, static_cast<int>(DisplayOrientation::PORTRAIT));
+
+    ability->SetDisplayOrientation(static_cast<int>(DisplayOrientation::FOLLOWRECENT));
+    ret = ability->GetDisplayOrientation();
+    EXPECT_EQ(ret, 0);
+
+    GTEST_LOG_(INFO) << "Ability_GetDisplayOrientation_0100 end";
+}
+
+/**
+ * @tc.number: Ability_CreateModalUIExtension_0100
+ * @tc.name: CreateModalUIExtension
+ * @tc.desc: NA
+ */
+HWTEST_F(AbilitySecondTest, Ability_CreateModalUIExtension_0100, Function | MediumTest | Level1)
+{
+    GTEST_LOG_(INFO) << "Ability_CreateModalUIExtension_0100 start";
+
+    auto ability = std::make_shared<Ability>();
+    ASSERT_NE(ability, nullptr);
+
+    Want want;
+    auto ret = ability->CreateModalUIExtension(want);
+    EXPECT_EQ(ret, ERR_INVALID_VALUE);
+
+    auto abilityContext = std::make_shared<AbilityRuntime::AbilityContextImpl>();
+    ability->AttachAbilityContext(abilityContext);
+    ret = ability->CreateModalUIExtension(want);
+    EXPECT_EQ(ret, ERR_INVALID_VALUE);
+
+    GTEST_LOG_(INFO) << "Ability_CreateModalUIExtension_0100 end";
+}
+
 }  // namespace AppExecFwk
 }  // namespace OHOS
