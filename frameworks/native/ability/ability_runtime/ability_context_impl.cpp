5a7f370a681754cdbcf9295a87cbf07a14391cc7
xiehd <xiehuandong@huawei.com>
2025-04-03 19:21:09 +0800

    fix
    Signed-off-by: xiehd <xiehuandong@huawei.com>

--- frameworks/native/ability/ability_runtime/ability_context_impl.cpp
+++ frameworks/native/ability/ability_runtime/ability_context_impl.cpp
@@ -44,6 +44,7 @@ constexpr const char* UIEXTENSION_TARGET_TYPE_KEY = "ability.want.params.uiExten
 constexpr const char* FLAG_AUTH_READ_URI_PERMISSION = "ability.want.params.uriPermissionFlag";
 constexpr const char* DISPOSED_PROHIBIT_BACK = "APPGALLERY_APP_DISPOSED_PROHIBIT_BACK";
 constexpr const char* IS_WINDOWMODE_FOLLOWHOST = "ohos.uec.params.isWindowModeFollowHost";
+constexpr const char* USE_GLOBAL_UICONTENT = "ohos.uec.params.useGlobalUIContent";
 
 struct RequestResult {
     int32_t resultCode {0};
@@ -1090,7 +1091,7 @@ ErrCode AbilityContextImpl::CreateModalUIExtensionWithApp(const AAFwk::Want &wan
         TAG_LOGE(AAFwkTag::CONTEXT, "null uiContent");
         return ERR_INVALID_VALUE;
     }
-    if (IsUIExtensionExist(want)) {
+    if (IsUIExtensionExist(want) && !want.GetBoolParam(USE_GLOBAL_UICONTENT, false)) {
         TAG_LOGD(AAFwkTag::CONTEXT, "exist uIExtension");
         return ERR_OK;
     }
