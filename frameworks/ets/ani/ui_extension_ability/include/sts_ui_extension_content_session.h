dc45ecca765f1d343871cf11fabcad230b552fcb
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-04-07 19:29:00 +0800

    uiExtension
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

---
+++ frameworks/ets/ani/ui_extension_ability/include/sts_ui_extension_content_session.h
@@ -0,0 +1,105 @@
+/*
+* Copyright (c) 2025 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef OHOS_ABILITY_RUNTIME_STS_UI_EXTENSION_CONTENT_SESSION_H
+#define OHOS_ABILITY_RUNTIME_STS_UI_EXTENSION_CONTENT_SESSION_H
+
+#include "session_info.h"
+#include "start_options.h"
+#include "window.h"
+#include "sts_runtime.h"
+
+namespace OHOS {
+namespace AbilityRuntime {
+[[maybe_unused]] static void NativeSendData(ani_env* env, ani_object obj, ani_object data);
+[[maybe_unused]] static void NativeLoadContent(ani_env* env, ani_object obj, ani_string path, ani_object storage);
+[[maybe_unused]] static void NativeTerminateSelf(ani_env* env, ani_object obj, [[maybe_unused]] ani_object callback);
+[[maybe_unused]] static void NativeSetWindowBackgroundColor(ani_env* env, ani_object obj, ani_string color);
+[[maybe_unused]] static int NativeTerminateSelfWithResult(ani_env* env, ani_object obj,
+    [[maybe_unused]] ani_object abilityResult, [[maybe_unused]] ani_object callback);
+[[maybe_unused]] static ani_object NativeSetReceiveDataCallback(ani_env* env, ani_object obj);
+
+using RuntimeTask = std::function<void(int, const AAFwk::Want&, bool)>;
+
+class StsAbilityResultListener {
+public:
+    StsAbilityResultListener() = default;
+    virtual ~StsAbilityResultListener() = default;
+    virtual void OnAbilityResult(int requestCode, int resultCode, const AAFwk::Want &resultData) = 0;
+    virtual bool IsMatch(int requestCode) = 0;
+};
+
+class StsAbilityResultListeners {
+public:
+    StsAbilityResultListeners() = default;
+    virtual ~StsAbilityResultListeners() = default;
+    void AddListener(const uint64_t &uiExtensionComponentId, std::shared_ptr<StsAbilityResultListener> listener) {}
+    void RemoveListener(const uint64_t &uiExtensionComponentId) {}
+    void OnAbilityResult(int requestCode, int resultCode, const AAFwk::Want &resultData) {}
+private:
+    std::map<uint64_t, std::shared_ptr<StsAbilityResultListener>> listeners_;
+};
+
+class StsUISessionAbilityResultListener : public StsAbilityResultListener {
+public:
+    StsUISessionAbilityResultListener() = default;
+    virtual ~StsUISessionAbilityResultListener() = default;
+    virtual void OnAbilityResult(int requestCode, int resultCode, const AAFwk::Want &resultData) {}
+    virtual bool IsMatch(int requestCode) {return true;}
+    void OnAbilityResultInner(int requestCode, int resultCode, const AAFwk::Want &resultData) {}
+    void SaveResultCallbacks(int requestCode, RuntimeTask&& task) {}
+private:
+    std::map<int, RuntimeTask> resultCallbacks_;
+};
+
+class StsUIExtensionContentSession {
+private:
+    class CallbackWrapper;
+public:
+    StsUIExtensionContentSession(sptr<AAFwk::SessionInfo> sessionInfo,
+        sptr<Rosen::Window> uiWindow, std::weak_ptr<AbilityRuntime::Context>& context,
+        std::shared_ptr<StsAbilityResultListeners>& abilityResultListeners);
+    StsUIExtensionContentSession(sptr<AAFwk::SessionInfo> sessionInfo,
+        sptr<Rosen::Window> uiWindow);
+    virtual ~StsUIExtensionContentSession() = default;
+    static ani_object CreateStsUIExtensionContentSession(ani_env* env,
+        sptr<AAFwk::SessionInfo> sessionInfo, sptr<Rosen::Window> uiWindow,
+        std::weak_ptr<AbilityRuntime::Context> context,
+        std::shared_ptr<StsAbilityResultListeners>& abilityResultListeners,
+        std::shared_ptr<StsUIExtensionContentSession> contentSessionPtr);
+    void SendData(ani_env* env, ani_object object, ani_object data);
+    void LoadContent(ani_env* env, ani_object object, ani_string path, ani_object storage);
+    void TerminateSelf();
+    int32_t TerminateSelfWithResult();
+    void SetWindowBackgroundColor(ani_env* env, ani_string color);
+    ani_object GetUIExtensionHostWindowProxy(ani_env* env, ani_object object);
+    ani_object SetReceiveDataCallback(ani_env* env, ani_object object);
+
+private:
+    sptr<AAFwk::SessionInfo> sessionInfo_;
+    sptr<Rosen::Window> uiWindow_;
+    std::weak_ptr<AbilityRuntime::Context> context_;
+    std::shared_ptr<CallbackWrapper> receiveDataCallback_;
+    bool isRegistered = false;
+    std::shared_ptr<CallbackWrapper> receiveDataForResultCallback_;
+    bool isSyncRegistered = false;
+    std::shared_ptr<StsUISessionAbilityResultListener> listener_;
+    bool isFirstTriggerBindModal_ = true;
+};
+
+StsUIExtensionContentSession* GetStsContentSession(ani_env* env, ani_object obj);
+}  // namespace AbilityRuntime
+}  // namespace OHOS
+#endif  // OHOS_ABILITY_RUNTIME_STS_UI_EXTENSION_CONTENT_SESSION_H
---
+++ frameworks/ets/ani/ui_extension_ability/src/sts_ui_extension_context.cpp
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "common_fun_ani.h"
+#include "sts_ui_extension_context.h"
+#include "ui_extension_context.h"
+#include "ani_common_want.h"
+#include "ability_manager_client.h"
+#include "sts_context_utils.h"
+#include "sts_error_utils.h"
+#include "sts_ui_extension_common.h"
+
+const char *INVOKE_METHOD_NAME = "invoke";
+static void TerminateSelfSync([[maybe_unused]] ani_env *env, [[maybe_unused]] ani_object obj,
+    [[maybe_unused]] ani_object callback)
+{
+    TAG_LOGD(AAFwkTag::UI_EXT, "called");
+    ani_class cls = nullptr;
+    ani_long nativeContextLong;
+    ani_field contextField = nullptr;
+    ani_status status = ANI_ERROR;
+    OHOS::ErrCode ret = OHOS::ERR_INVALID_VALUE;
+    if ((status = env->FindClass("Lapplication/UIExtensionContext/UIExtensionContext;", &cls)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return;
+    }
+    if ((status = env->Class_FindField(cls, "nativeUIExtensionContext", &contextField)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return;
+    }
+    if ((status = env->Object_GetField_Long(obj, contextField, &nativeContextLong)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return;
+    }
+    ret = ((OHOS::AbilityRuntime::UIExtensionContext*)nativeContextLong)->TerminateSelf();
+    OHOS::AbilityRuntime::StsUIExtensionCommon::AsyncCallback(env, callback,
+        OHOS::AbilityRuntime::CreateStsErrorByNativeErr(env, static_cast<int32_t>(ret)), nullptr);
+}
+static void TerminateSelfWithResultSync([[maybe_unused]] ani_env *env, [[maybe_unused]] ani_object obj,
+    [[maybe_unused]] ani_object abilityResult, [[maybe_unused]] ani_object callback)
+{
+    TAG_LOGD(AAFwkTag::UI_EXT, "called");
+    ani_class cls = nullptr;
+    ani_long nativeContextLong;
+    ani_field contextField = nullptr;
+    ani_status status = ANI_ERROR;
+    OHOS::ErrCode ret = OHOS::ERR_INVALID_VALUE;
+    if ((status = env->FindClass("Lapplication/UIExtensionContext/UIExtensionContext;", &cls)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return;
+    }
+    if ((status = env->Class_FindField(cls, "nativeUIExtensionContext", &contextField)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return;
+    }
+    if ((status = env->Object_GetField_Long(obj, contextField, &nativeContextLong)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return;
+    }
+    auto context = ((OHOS::AbilityRuntime::UIExtensionContext*)nativeContextLong);
+    if (!context) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "context is released");
+        return;
+    }
+
+    OHOS::AAFwk::Want want;
+    int resultCode = 0;
+    OHOS::AppExecFwk::UnWrapAbilityResult(env, abilityResult, resultCode, want);
+    auto token = context->GetToken();
+    OHOS::AAFwk::AbilityManagerClient::GetInstance()->TransferAbilityResultForExtension(token, resultCode, want);
+    ret = context->TerminateSelf();
+    if (ret != 0) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "TerminateSelf failed, errorCode is %{public}d", ret);
+        return;
+    }
+    OHOS::AbilityRuntime::StsUIExtensionCommon::AsyncCallback(env, callback,
+        OHOS::AbilityRuntime::CreateStsErrorByNativeErr(env, static_cast<int32_t>(ret)), nullptr);
+}
+
+void BindExtensionInfo(ani_env* aniEnv, ani_class contextClass, ani_object contextObj,
+    std::shared_ptr<OHOS::AbilityRuntime::Context> context, std::shared_ptr<OHOS::AppExecFwk::AbilityInfo> abilityInfo)
+{
+    TAG_LOGD(AAFwkTag::UI_EXT, "called");
+    auto hapModuleInfo = context->GetHapModuleInfo();
+    ani_status status = ANI_OK;
+    if (abilityInfo && hapModuleInfo) {
+        auto isExist = [&abilityInfo](const OHOS::AppExecFwk::ExtensionAbilityInfo& info) {
+            TAG_LOGE(AAFwkTag::UI_EXT, "%{public}s, %{public}s", info.bundleName.c_str(), info.name.c_str());
+            return info.bundleName == abilityInfo->bundleName && info.name == abilityInfo->name;
+        };
+        auto infoIter = std::find_if(
+            hapModuleInfo->extensionInfos.begin(), hapModuleInfo->extensionInfos.end(), isExist);
+        if (infoIter == hapModuleInfo->extensionInfos.end()) {
+            TAG_LOGE(AAFwkTag::UI_EXT, "set extensionAbilityInfo fail");
+            return;
+        }
+        ani_field extensionAbilityInfoField;
+        status = aniEnv->Class_FindField(contextClass, "extensionAbilityInfo", &extensionAbilityInfoField);
+        if (status != ANI_OK) {
+            TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+            return;
+        }
+        ani_object extAbilityInfoObj = OHOS::AppExecFwk::CommonFunAni::ConvertExtensionInfo(aniEnv, *infoIter);
+        status = aniEnv->Object_SetField_Ref(contextObj, extensionAbilityInfoField,
+            reinterpret_cast<ani_ref>(extAbilityInfoObj));
+        if (status != ANI_OK) {
+            TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+            return;
+        }
+    }
+}
+
+void StsCreatExtensionContext(ani_env* aniEnv, ani_class contextClass, ani_object contextObj,
+    void* applicationCtxRef, std::shared_ptr<OHOS::AbilityRuntime::ExtensionContext> context)
+{
+    OHOS::AbilityRuntime::ContextUtil::StsCreatContext(aniEnv, contextClass, contextObj, applicationCtxRef, context);
+    BindExtensionInfo(aniEnv, contextClass, contextObj, context, context->GetAbilityInfo());
+}
+
+ani_object CreateStsUIExtensionContext(ani_env *env,
+    std::shared_ptr<OHOS::AbilityRuntime::UIExtensionContext> context,
+    const std::shared_ptr<OHOS::AppExecFwk::OHOSApplication> &application)
+{
+    TAG_LOGD(AAFwkTag::UI_EXT, "called");
+    ani_class cls = nullptr;
+    ani_status status = ANI_ERROR;
+    ani_method method = nullptr;
+    ani_field field = nullptr;
+    ani_object contextObj = nullptr;
+    if ((env->FindClass("Lapplication/UIExtensionContext/UIExtensionContext;", &cls)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return nullptr;
+    }
+    std::array functions = {
+        ani_native_function { "terminateSelfSync", nullptr, reinterpret_cast<ani_int*>(TerminateSelfSync) },
+        ani_native_function { "terminateSelfWithResultSync", nullptr,
+            reinterpret_cast<ani_int*>(TerminateSelfWithResultSync) },
+    };
+    if ((status = env->Class_BindNativeMethods(cls, functions.data(), functions.size())) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return nullptr;
+    }
+    if ((status = env->Class_FindMethod(cls, "<ctor>", ":V", &method)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return nullptr;
+    }
+    if ((status = env->Object_New(cls, method, &contextObj)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return nullptr;
+    }
+    if ((status = env->Class_FindField(cls, "nativeUIExtensionContext", &field)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return nullptr;
+    }
+    ani_long nativeContextLong = (ani_long)context.get();
+    if ((status = env->Object_SetField_Long(contextObj, field, nativeContextLong)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "status: %{public}d", status);
+        return nullptr;
+    }
+
+    // bind parent context
+    if (application == nullptr) {
+        TAG_LOGE(AAFwkTag::UI_EXT, "application null");
+        return nullptr;
+    }
+    StsCreatExtensionContext(env, cls, contextObj, application->GetApplicationCtxObjRef(), context);
+    return contextObj;
+}
