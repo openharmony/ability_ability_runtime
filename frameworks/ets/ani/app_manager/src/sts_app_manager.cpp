f9ab7abf8b8b7f39480806d5b4bcc29403d3be24
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-04-10 14:16:17 +0800

    fix ani app manager
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- frameworks/ets/ani/app_manager/src/sts_app_manager.cpp
+++ frameworks/ets/ani/app_manager/src/sts_app_manager.cpp
@@ -18,6 +18,7 @@
 #include "hilog_tag_wrapper.h"
 #include "ability_manager_client.h"
 #include "ability_manager_interface.h"
+#include "ani_common_util.h"
 #include "app_mgr_constants.h"
 #include "app_mgr_interface.h"
 #include "if_system_ability_manager.h"
@@ -36,48 +37,53 @@ OHOS::sptr<OHOS::AppExecFwk::IAppMgr> GetAppManagerInstance()
     return OHOS::iface_cast<OHOS::AppExecFwk::IAppMgr>(appObject);
 }
 
-static void PreloadApplication(ani_env *env, [[maybe_unused]]ani_class aniClass,
-    ani_string bundleName, ani_int userId, ani_int mode, ani_int appIndex)
+static void PreloadApplication(
+    ani_env *env, ani_string bundleName, ani_double userId, ani_int mode, ani_double appIndex)
 {
     std::string bundleNameStr;
-    ani_size sz {};
-    env->String_GetUTF8Size(bundleName, &sz);
-    bundleNameStr.resize(sz + 1);
-    env->String_GetUTF8SubString(bundleName, 0, sz, bundleNameStr.data(), bundleNameStr.size(), &sz);
+    if (!AppExecFwk::GetStdString(env, bundleName, bundleNameStr)) {
+        TAG_LOGE(AAFwkTag::APPKIT, "GetStdString failed");
+        return;
+    }
+    TAG_LOGD(AAFwkTag::APPKIT, "PreloadApplication bundleName %{public}s", bundleNameStr.c_str());
+    TAG_LOGD(AAFwkTag::APPKIT, "PreloadApplication userId %{public}d", static_cast<int32_t>(userId));
+    TAG_LOGD(AAFwkTag::APPKIT, "PreloadApplication appIndex %{public}d", static_cast<int32_t>(appIndex));
 
     sptr<OHOS::AppExecFwk::IAppMgr> appMgr = GetAppManagerInstance();
     if (appMgr == nullptr) {
-        TAG_LOGE(AAFwkTag::DELEGATOR, "appMgr is null");
+        TAG_LOGE(AAFwkTag::APPKIT, "appMgr is null");
         return;
     }
-    appMgr->PreloadApplication(bundleNameStr, userId, static_cast<AppExecFwk::PreloadMode>(mode), appIndex);
+    appMgr->PreloadApplication(bundleNameStr, static_cast<int32_t>(userId), AppExecFwk::PreloadMode::PRESS_DOWN,
+        static_cast<int32_t>(appIndex));
 }
 
 void StsAppManagerRegistryInit(ani_env *env)
 {
     ani_status status = ANI_ERROR;
     if (env->ResetError() != ANI_OK) {
-        TAG_LOGE(AAFwkTag::STSRUNTIME, "ResetError failed");
+        TAG_LOGE(AAFwkTag::APPKIT, "ResetError failed");
     }
 
     ani_namespace ns;
     status = env->FindNamespace("L@ohos/app/ability/appManager/appManager;", &ns);
     if (status != ANI_OK) {
-        TAG_LOGE(AAFwkTag::DELEGATOR, "FindNamespace appManager failed status : %{public}d", status);
+        TAG_LOGE(AAFwkTag::APPKIT, "FindNamespace appManager failed status : %{public}d", status);
         return;
     }
 
     std::array kitFunctions = {
-        ani_native_function {"nativePreloadApplication", nullptr, reinterpret_cast<void *>(PreloadApplication)},
+        ani_native_function {
+            "nativePreloadApplication", nullptr, reinterpret_cast<void *>(PreloadApplication)},
     };
 
     status = env->Namespace_BindNativeFunctions(ns, kitFunctions.data(), kitFunctions.size());
     if (status != ANI_OK) {
-        TAG_LOGE(AAFwkTag::DELEGATOR, "Namespace_BindNativeFunctions failed status : %{public}d", status);
+        TAG_LOGE(AAFwkTag::APPKIT, "Namespace_BindNativeFunctions failed status : %{public}d", status);
     }
 
     if (env->ResetError() != ANI_OK) {
-        TAG_LOGE(AAFwkTag::STSRUNTIME, "ResetError failed");
+        TAG_LOGE(AAFwkTag::APPKIT, "ResetError failed");
     }
 }
 } // namespace AbilityDelegatorSts
