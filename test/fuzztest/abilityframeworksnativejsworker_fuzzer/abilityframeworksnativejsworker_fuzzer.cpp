25996576b8fab979ad8a192bdd7642ade0fa5c48
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-04-14 17:01:18 +0800

    独立编译整改解耦
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- test/fuzztest/abilityframeworksnativejsworker_fuzzer/abilityframeworksnativejsworker_fuzzer.cpp
+++ test/fuzztest/abilityframeworksnativejsworker_fuzzer/abilityframeworksnativejsworker_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -26,6 +26,7 @@
 
 #include "ability_record.h"
 #include "securec.h"
+#include "worker_info.h"
 
 using namespace OHOS::AAFwk;
 using namespace OHOS::AppExecFwk;
