cdc05aa80216340ba4cbce8a024fba1edd9a358f
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-04-15 20:44:47 +0800

    upd config
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- frameworks/ets/ani/ani_common/include/ani_common_util.h
+++ frameworks/ets/ani/ani_common/include/ani_common_util.h
@@ -19,7 +19,6 @@
 #include "sts_runtime.h"
 namespace OHOS {
 namespace AppExecFwk {
-#define SETTER_METHOD_NAME(property) "<set>" #property
 
 bool GetDoubleOrUndefined(ani_env *env, ani_object param, const char *name, ani_double &value);
 bool GetBoolOrUndefined(ani_env *env, ani_object param, const char *name);
@@ -34,14 +33,14 @@ ani_array_ref GetAniArrayString(ani_env *env, const std::vector<std::string> &va
 bool GetRefFieldByName(ani_env *env, ani_object param, const char *name, ani_ref &ref);
 
 ani_object createDouble(ani_env *env, ani_double value);
+ani_object createBoolean(ani_env *env, ani_boolean value);
+
 bool SetFieldString(ani_env *env, ani_class cls, ani_object object, const std::string &fieldName, const std::string &value);
 bool SetFieldDouble(ani_env *env, ani_class cls, ani_object object, const std::string &fieldName, double value);
 bool SetFieldBoolean(ani_env *env, ani_class cls, ani_object object, const std::string &fieldName, bool value);
 bool SetFieldInt(ani_env *env, ani_class cls, ani_object object, const std::string &fieldName, int value);
 bool SetFieldArrayString(ani_env *env, ani_class cls, ani_object object, const std::string &fieldName, const std::vector<std::string> &values);
 bool SetFieldRef(ani_env *env, ani_class cls, ani_object object, const std::string &fieldName, ani_ref value);
-
-void ClassSetter(ani_env* env, ani_class cls, ani_object object, const char* setterName, ...);
 }  // namespace AppExecFwk
 }  // namespace OHOS
 #endif  // OHOS_ABILITY_RUNTIME_ANI_COMMON_UTIL_H
