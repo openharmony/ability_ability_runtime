10775ab46cbf85b2f1a8ceec69dfefd517a1d02f
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-04-16 11:14:20 +0800

    add wantagent
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- frameworks/native/ability/native/sts_free_install_observer.cpp
+++ frameworks/native/ability/native/sts_free_install_observer.cpp
@@ -12,14 +12,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#include "sts_free_install_observer.h"
+
 #include "ability_business_error.h"
 #include "ani_common_ability_result.h"
 #include "ani_common_configuration.h"
 #include "ani_common_start_options.h"
 #include "ani_common_want.h"
+#include "ani_common_util.h"
 #include "hilog_tag_wrapper.h"
 #include "hitrace_meter.h"
-#include "sts_free_install_observer.h"
 
 namespace OHOS {
 namespace AbilityRuntime {
@@ -77,106 +80,11 @@ void StsFreeInstallObserver::CallCallback(ani_object callback, int32_t resultCod
     if ((etsVm_->AttachCurrentThread(&aniArgs, ANI_VERSION_1, &env)) != ANI_OK) {
         TAG_LOGE(AAFwkTag::STSRUNTIME, "status : %{public}d", status);
     }
-    AsyncCallback(env, callback, WrapBusinessError(env, resultCode), nullptr);
-}
-
-bool StsFreeInstallObserver::AsyncCallback(ani_env *env, ani_object call, ani_object error, ani_object result)
-{
-    ani_status status = ANI_ERROR;
-    ani_class clsCall {};
-
-    if ((status = env->FindClass("Lapplication/UIAbilityContext/AsyncCallbackWrapper;", &clsCall)) != ANI_OK) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "status : %{public}d", status);
-        return false;
+    ani_object aniObject = CreateStsError(env, AbilityErrorCode::ERROR_OK);
+    if (resultCode != ERR_OK) {
+        aniObject = CreateStsErrorByNativeErr(env, resultCode);
     }
-    ani_method method {};
-    if ((status = env->Class_FindMethod(clsCall, "invoke", "L@ohos/base/BusinessError;Lstd/core/Object;:V", &method)) !=
-        ANI_OK) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "status : %{public}d", status);
-        return false;
-    }
-    if (result == nullptr) {
-        ani_ref nullRef = nullptr;
-        env->GetNull(&nullRef);
-        result = reinterpret_cast<ani_object>(nullRef);
-    }
-    if ((status = env->Object_CallMethod_Void(call, method, error, result)) != ANI_OK) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "status : %{public}d", status);
-        return false;
-    }
-    return true;
-}
-
-ani_object StsFreeInstallObserver::WrapError(ani_env *env, const std::string &msg)
-{
-    ani_class cls {};
-    ani_method method {};
-    ani_object obj = nullptr;
-    ani_status status = ANI_ERROR;
-    if (env == nullptr) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "null env");
-        return nullptr;
-    }
-    ani_string aniMsg = AppExecFwk::GetAniString(env, msg);
-
-    ani_ref undefRef;
-    env->GetUndefined(&undefRef);
-
-    if ((status = env->FindClass("Lescompat/Error;", &cls)) != ANI_OK) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "status : %{public}d", status);
-        return nullptr;
-    }
-    if ((status = env->Class_FindMethod(cls, "<ctor>", "Lstd/core/String;Lescompat/ErrorOptions;:V", &method)) !=
-        ANI_OK) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "status : %{public}d", status);
-        return nullptr;
-    }
-
-    if ((status = env->Object_New(cls, method, &obj, aniMsg, undefRef)) != ANI_OK) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "status : %{public}d", status);
-        return nullptr;
-    }
-    return obj;
-}
-
-std::string StsFreeInstallObserver::GetErrMsg(int32_t err, const std::string &permission)
-{
-    auto errCode = GetJsErrorCodeByNativeError(err);
-    auto errMsg = (errCode == AbilityErrorCode::ERROR_CODE_PERMISSION_DENIED && !permission.empty())
-                      ? GetNoPermissionErrorMsg(permission)
-                      : GetErrorMsg(errCode);
-    return errMsg;
-}
-
-ani_object StsFreeInstallObserver::WrapBusinessError(ani_env *env, int32_t resultCode)
-{
-    ani_class cls {};
-    ani_method method {};
-    ani_object obj = nullptr;
-    ani_status status = ANI_ERROR;
-    if (env == nullptr) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "null env");
-        return nullptr;
-    }
-    if ((status = env->FindClass("L@ohos/base/BusinessError;", &cls)) != ANI_OK) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "status : %{public}d", status);
-        return nullptr;
-    }
-    if ((status = env->Class_FindMethod(cls, "<ctor>", "DLescompat/Error;:V", &method)) != ANI_OK) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "status : %{public}d", status);
-        return nullptr;
-    }
-    ani_object error = WrapError(env, GetErrMsg(resultCode));
-    if (error == nullptr) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "error nulll");
-        return nullptr;
-    }
-    ani_double dCode(resultCode);
-    if ((status = env->Object_New(cls, method, &obj, dCode, error)) != ANI_OK) {
-        TAG_LOGE(AAFwkTag::UIABILITY, "status : %{public}d", status);
-        return nullptr;
-    }
-    return obj;
+    AppExecFwk::AsyncCallback(env, callback, aniObject, nullptr);
 }
 
 void StsFreeInstallObserver::AddStsObserverObject(ani_env *env, const std::string &bundleName,
