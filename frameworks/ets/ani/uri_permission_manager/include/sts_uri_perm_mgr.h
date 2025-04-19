6d0c72e21e670b8db18f19f9e9755f189002e6a5
zhang_hao_zheng <zhanghaozheng2@h-partners.com>
2025-04-19 22:01:37 +0800

    错误码整改
    Signed-off-by: zhang_hao_zheng <zhanghaozheng2@h-partners.com>
    Change-Id: Id7e8663e9f5642e9737a673e6c5feb5fca422c9e

--- frameworks/ets/ani/uri_permission_manager/include/sts_uri_perm_mgr.h
+++ frameworks/ets/ani/uri_permission_manager/include/sts_uri_perm_mgr.h
@@ -11,7 +11,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */1
 
 #ifndef OHOS_ABILITY_RUNTIME_SIMULATOR_STS_ABILITY_CONTEXT_H
 #define OHOS_ABILITY_RUNTIME_SIMULATOR_STS_ABILITY_CONTEXT_H
