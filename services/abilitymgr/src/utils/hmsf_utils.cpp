00a496464eb52301ef6b4a66ba35f10bdfe41a31
wangkailong <wangkailong6@huawei.com>
2025-04-25 11:28:33 +0800

    bug
    Signed-off-by: wangkailong <wangkailong6@huawei.com>
    Change-Id: I10cb1d89b95e9c9a8312499504c6bb5b7f3275c9

--- services/abilitymgr/src/utils/hmsf_utils.cpp
+++ services/abilitymgr/src/utils/hmsf_utils.cpp
@@ -1,12 +1,4 @@
 /*
- * @Author: wangkailong wangkailong6@huawei.com
- * @Date: 2025-04-23 12:33:58
- * @LastEditors: wangkailong wangkailong6@huawei.com
- * @LastEditTime: 2025-04-24 11:19:41
- * @FilePath: /xts/home/wangkailong/ohos/master/foundation/ability/ability_runtime/services/abilitymgr/src/utils/hmsf_utils.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-/*
  * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
