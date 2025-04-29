e89baec0c40a177b1c48d746535a506a667f0030
cxy251 <caixinyu9@h-partners.com>
2025-04-29 16:19:03 +0800

    onstop002
    Signed-off-by: cxy251 <caixinyu9@h-partners.com>

--- frameworks/ets/ani/form_extension_ability/src/sts_form_extension.cpp
+++ frameworks/ets/ani/form_extension_ability/src/sts_form_extension.cpp
@@ -626,7 +626,8 @@ void STSFormExtension::OnStop()
     }
 
     ani_ref nameRef;
-    ani_status status = env->Object_GetFieldByName_Ref(static_cast<ani_object>(stsAbilityObj_->aniRef), "onStop", &nameRef);
+    ani_status status = env->Object_GetFieldByName_Ref(
+		static_cast<ani_object>(stsAbilityObj_->aniRef), "onStop", &nameRef);
     if (status != ANI_OK) {
         TAG_LOGE(AAFwkTag::FORM_EXT, "Object_GetFieldByName status: %{public}d, %{public}p, %{public}p",
             status, stsAbilityObj_->aniRef, stsAbilityObj_->aniObj);
