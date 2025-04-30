70d4063ec242e5ae99658724feac74a36c9e7434
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-04-30 18:59:47 +0800

    startOption add more
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- frameworks/ets/ani/ani_common/include/ani_common_start_options.h
+++ frameworks/ets/ani/ani_common/include/ani_common_start_options.h
@@ -18,12 +18,26 @@
 
 #include "start_options.h"
 #include "ani_common_util.h"
+#ifdef START_WINDOW_OPTIONS_WITH_PIXELMAP
+#include "pixel_map_ani.h"
+#include "image_ani_utils.h"
+#endif
 
 namespace OHOS {
 namespace AppExecFwk {
 
 bool UnwrapStartOptionsWithProcessOption(ani_env *env, ani_object param, AAFwk::StartOptions &startOptions);
 bool UnwrapStartOptions(ani_env *env, ani_object param, AAFwk::StartOptions &startOptions);
+bool UnwrapProcessOptions(ani_env *env, ani_object param, std::shared_ptr<AAFwk::ProcessOptions> &processOptions);
+
+#ifdef START_WINDOW_OPTIONS_WITH_PIXELMAP
+bool UnwrapPixelMapByPropertyName(
+    ani_env *env, ani_object EnvObject, const char *propertyName, std::shared_ptr<Media::PixelMap> &value);
+bool UnwrapPixelMapFromAni(ani_env *env, ani_object param, std::shared_ptr<Media::PixelMap> &value);
+#endif
+
+bool UnwrapStartWindowOption(ani_env *env, ani_object param,
+    std::shared_ptr<AAFwk::StartWindowOption> &startWindowOption);
 }  // namespace AppExecFwk
 }  // namespace OHOS
 #endif  // OHOS_ABILITY_RUNTIME_NAPI_COMMON_START_OPTIONS_H
