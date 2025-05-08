4f84a16142f4204ba81139db5d5c71f46c9aa219
xiehd <xiehuandong@huawei.com>
2025-05-08 14:41:19 +0800

    Fix the package name of the East Lake scenario.
    Signed-off-by: xiehd <xiehuandong@huawei.com>

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -11954,7 +11954,12 @@ int32_t AbilityManagerService::GetUIExtensionRootHostInfo(const sptr<IRemoteObje
     }
 
     hostInfo.elementName_ = callerRecord->GetElementName();
-    TAG_LOGD(AAFwkTag::UI_EXT, "uri: %{public}s.", hostInfo.elementName_.GetURI().c_str());
+    auto sessionInfo = callerRecord->GetSessionInfo();
+    if (sessionInfo != nullptr) {
+        hostInfo.sessionName = sessionInfo->sessionName;
+    }
+    TAG_LOGD(AAFwkTag::UI_EXT, "uri: %{public}s, sessionName: %{public}s.",
+             hostInfo.elementName_.GetURI().c_str(), hostInfo.sessionName.c_str());
     return ERR_OK;
 }
 
