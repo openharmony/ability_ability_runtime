a614a01d6a836e5f825136e8b049624f417def2c
zhang_hao_zheng <zhanghaozheng2@h-partners.com>
2025-05-15 23:05:36 +0800

    testMergeAppServiceExtensionAbility
    Signed-off-by: zhang_hao_zheng <zhanghaozheng2@h-partners.com>
    Change-Id: I8d63a401a14f7ea95a833b398090a7fbde91c57b

---
+++ interfaces/kits/native/ability/native/app_service_extension.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_ABILITY_RUNTIME_APP_SERVICE_EXTENSION_H
+#define OHOS_ABILITY_RUNTIME_APP_SERVICE_EXTENSION_H
+
+#include "extension_base.h"
+
+namespace OHOS {
+namespace AbilityRuntime {
+class AppServiceExtensionContext;
+class Runtime;
+/**
+ * @brief Basic service components.
+ */
+class AppServiceExtension : public ExtensionBase<AppServiceExtensionContext> {
+public:
+    AppServiceExtension() = default;
+    virtual ~AppServiceExtension() = default;
+
+    /**
+     * @brief Create and init context.
+     *
+     * @param record the extension record.
+     * @param application the application info.
+     * @param handler the extension handler.
+     * @param token the remote token.
+     * @return The created context.
+     */
+    virtual std::shared_ptr<AppServiceExtensionContext> CreateAndInitContext(
+        const std::shared_ptr<AbilityLocalRecord> &record,
+        const std::shared_ptr<OHOSApplication> &application,
+        std::shared_ptr<AbilityHandler> &handler,
+        const sptr<IRemoteObject> &token) override;
+
+    /**
+     * @brief Init the extension.
+     *
+     * @param record the extension record.
+     * @param application the application info.
+     * @param handler the extension handler.
+     * @param token the remote token.
+     */
+    virtual void Init(const std::shared_ptr<AbilityLocalRecord> &record,
+        const std::shared_ptr<OHOSApplication> &application,
+        std::shared_ptr<AbilityHandler> &handler,
+        const sptr<IRemoteObject> &token) override;
+
+    /**
+     * @brief Create Extension.
+     *
+     * @param runtime The runtime.
+     * @return The AppServiceExtension instance.
+     */
+    static AppServiceExtension* Create(const std::unique_ptr<Runtime>& runtime);
+
+    /**
+     * @brief Called when the system configuration is updated.
+     *
+     * @param configuration Indicates the updated configuration information.
+     */
+    void OnConfigurationUpdated(const AppExecFwk::Configuration &configuration) override;
+};
+}  // namespace AbilityRuntime
+}  // namespace OHOS
+#endif  // OHOS_ABILITY_RUNTIME_APP_SERVICE_EXTENSION_H
