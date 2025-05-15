c0ceb26072627564e5e7b98605e841727928a05d
Cathy <houxi3@huawei.com>
2025-05-15 20:59:35 +0800

    support onDestory set suggestCache flag
    Signed-off-by: Cathy <houxi3@huawei.com>

--- test/unittest/cache_process_manager_test/cache_process_manager_test.cpp
+++ test/unittest/cache_process_manager_test/cache_process_manager_test.cpp
@@ -368,6 +368,7 @@ HWTEST_F(CacheProcessManagerTest, CacheProcessManager_IsAppShouldCache_0100, Tes
     cacheProcMgr->maxProcCacheNum_ = 2;
     auto appRecord = MockAppRecord();
     EXPECT_NE(appRecord, nullptr);
+    appRecord->procCacheSupportState_ = SupportProcessCacheState::SUPPORT;
     cacheProcMgr->cachedAppRecordQueue_.push_back(appRecord);
     EXPECT_EQ(cacheProcMgr->IsAppShouldCache(appRecord), true);
 
