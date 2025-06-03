c0565ab44c844c8244857165bd3004231672f24c
oh_ci <maguangsheng1@huawei.com>
2025-06-03 01:24:16 +0000

    回退 'Pull Request !14596 : 修复RestartSelfAtomicService接口3秒内连续调用没有返回错误码'

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -14278,7 +14278,8 @@ int32_t AbilityManagerService::RestartSelfAtomicService(sptr<IRemoteObject> call
     }
 
     RestartAppKeyType key(processInfo.instanceKey, callerUid);
-    int64_t now = time(nullptr);
+    int64_t now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::
+        system_clock::now().time_since_epoch()).count();
     if (RestartAppManager::GetInstance().IsRestartAppFrequent(key, now)) {
         return AAFwk::ERR_RESTART_APP_FREQUENT;
     }
