4e86215fa87559383dfa0e2c9be3a63feee1e2cb
HengBai <baiheng5@huawei.com>
2025-06-06 04:14:49 +0000

    update services/abilitymgr/src/ability_manager_service.cpp.
    Signed-off-by: HengBai <baiheng5@huawei.com>

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -2923,7 +2923,7 @@ void AbilityManagerService::ReportAbilityStartInfoToRSS(const AppExecFwk::Abilit
             if (info.uid_ == abilityInfo.applicationInfo.uid &&
                 info.processType_ == AppExecFwk::ProcessType::NORMAL &&
                 std::find(info.bundleNames.begin(), info.bundleNames.end(),
-                abilityInfo.applicationInfo.bundleName) !=  info.bundleNames.end()){
+                abilityInfo.applicationInfo.bundleName) != info.bundleNames.end()){
                 isColdStart = false;
                 pid = info.pid_;
                 warmStartType = static_cast<int32_t>(info.preloadMode_);
