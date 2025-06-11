b6b9badeca684e41b7332ce3126c26af070050e3
吴浩 <wuhao378@huawei.com>
2025-06-11 07:02:38 +0000

    update services/appmgr/src/modal_system_app_freeze_uiextension.cpp.
    Signed-off-by: 吴浩 <wuhao378@huawei.com>

--- services/appmgr/src/modal_system_app_freeze_uiextension.cpp
+++ services/appmgr/src/modal_system_app_freeze_uiextension.cpp
@@ -73,8 +73,8 @@ void ModalSystemAppFreezeUIExtension::ProcessAppFreeze(bool focusFlag, const Fau
             .count();
     bool timeout = now - lastFreezeTime > TIMEOUT_INTERVAL_MS;
     TAG_LOGI(AAFwkTag::ABILITYMGR,
-        "%{public}s is %{public}s.pid:%{public}s lastFreezePid:%{public}s,timeout %{public}lu", bundleName.c_str(),
-        focusFlag ? "focus" : "not focus", pid.c_str(), lastFreezePid.c_str(), now - lastFreezeTime);
+        "%{public}s is %{public}s.pid:%{public}s lastFreezePid:%{public}s", bundleName.c_str(),
+        focusFlag ? "focus" : "not focus", pid.c_str(), lastFreezePid.c_str());
     bool isPullUpBox =
         isAppFreezeDialog && (pid != lastFreezePid || (pid == lastFreezePid && timeout && !isDialogExist));
     bool updateTypeName = name == AppFreezeType::THREAD_BLOCK_6S || name == AppFreezeType::BUSSINESS_THREAD_BLOCK_6S;
