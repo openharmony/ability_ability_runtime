14004c289dbdc1c32d5b9f43ff92c830440f59a0
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-06-12 18:40:30 +0800

    add UriUtils  unittest cases for ability_runtime
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

---
+++ test/unittest/uri_utils_second_test/mock/src/mock_my_flag.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mock_my_flag.h"
+
+namespace OHOS {
+namespace AAFwk {
+int MyFlag::flag_ = 0;
+bool MyFlag::permissionFileAccessManager_ = false;
+bool MyFlag::permissionWriteImageVideo_ = false;
+bool MyFlag::permissionReadImageVideo_ = false;
+bool MyFlag::permissionAllMedia_ = false;
+bool MyFlag::permissionWriteAudio_ = false;
+bool MyFlag::permissionReadAudio_ = false;
+bool MyFlag::permissionProxyAuthorization_ = false;
+bool MyFlag::permissionAll_ = false;
+bool MyFlag::permissionPrivileged_ = false;
+bool MyFlag::permissionReadWriteDownload_ = false;
+bool MyFlag::permissionReadWriteDesktop_ = false;
+bool MyFlag::permissionReadWriteDocuments_ = false;
+bool MyFlag::IsSystempAppCall_ = false;
+bool MyFlag::permissionFileAccessPersist_ = false;
+bool MyFlag::isSAOrSystemAppCall_ = false;
+bool MyFlag::isSystemAppCall_ = false;
+bool MyFlag::isUriTypeValid_ = false;
+bool MyFlag::isDocsCloudUri_ = false;
+int32_t MyFlag::getTokenIdByBundleNameStatus_ = 0;
+std::string MyFlag::bundleName_ = "";
+TokenInfoMap MyFlag::tokenInfos = {};
+} // namespace AAFwk
+} // namespace OHOS
