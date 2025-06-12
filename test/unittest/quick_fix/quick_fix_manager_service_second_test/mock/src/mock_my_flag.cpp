ab39b9d305111a0d0ddffd32bacc7f8db0ee200b
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-06-12 13:46:45 +0800

    add DataObsMgrService unittest cases for ability_runtime
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

---
+++ test/unittest/quick_fix/quick_fix_manager_service_second_test/mock/src/mock_my_flag.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "../../mock/include/mock_my_flag.h"
+
+namespace OHOS {
+namespace AAFwk {
+int MyFlag::flag_ = 0;
+bool MyFlag::isAllowedToUseSystemAPIFlag_ = false;
+bool MyFlag::verifyCallingPermissionFlag_ = false;
+bool MyFlag::checkSpecificFlag_ = false;
+int MyFlag::usedCheckSpecificTimes_ = 0;
+bool MyFlag::isVerifyInstallBundlePermission_ = false;
+bool MyFlag::isVerifyPrivilegedPermission_ = false;
+} // namespace AAFwk
+} // namespace OHOS
