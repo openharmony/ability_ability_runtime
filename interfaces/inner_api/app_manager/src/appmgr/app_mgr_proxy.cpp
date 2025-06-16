7c0677253983eed9521803189bbabb4453f408d2
wangbing <wangbing175@huawei-partners.com>
2025-06-16 09:17:23 +0800

    Add code
    Signed-off-by: wangbing <wangbing175@huawei-partners.com>

--- interfaces/inner_api/app_manager/src/appmgr/app_mgr_proxy.cpp
+++ interfaces/inner_api/app_manager/src/appmgr/app_mgr_proxy.cpp
@@ -18,6 +18,7 @@
 #include "ability_manager_errors.h"
 #include "appexecfwk_errors.h"
 #include "hilog_tag_wrapper.h"
+#include "hitrace_chain_utils.h"
 #include "hitrace_meter.h"
 #include "ipc_capacity_wrap.h"
 #include "ipc_types.h"
@@ -943,6 +944,7 @@ int AppMgrProxy::GetRenderProcessTerminationStatus(pid_t renderPid, int &status)
 
 int32_t AppMgrProxy::UpdateConfiguration(const Configuration &config, const int32_t userId)
 {
+    Ability_MANAGER_HITRACE_CHAIN_NAME("UpdateConfiguration", HITRACE_FLAG_INCLUDE_ASYNC);
     TAG_LOGI(AAFwkTag::APPMGR, "AppMgrProxy UpdateConfiguration");
     MessageParcel data;
     MessageParcel reply;
@@ -1010,6 +1012,7 @@ int32_t AppMgrProxy::UpdateConfigurationForBackgroundApp(const std::vector<Backg
 int32_t AppMgrProxy::UpdateConfigurationByBundleName(const Configuration &config, const std::string &name,
     int32_t appIndex)
 {
+    Ability_MANAGER_HITRACE_CHAIN_NAME("UpdateConfigurationByBundleName", HITRACE_FLAG_INCLUDE_ASYNC);
     TAG_LOGI(AAFwkTag::APPMGR, "AppMgrProxy UpdateConfigurationByBundleName");
     MessageParcel data;
     if (!WriteInterfaceToken(data)) {
