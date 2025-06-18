6fdfb8e41aae646a73d4b048318cac1f94e529c3
huangshiwei <huangshiwei4@huawei.com>
2025-06-18 11:37:53 +0800

    huangshiwei4@huawei.com
    Signed-off-by: huangshiwei <huangshiwei4@huawei.com>

---
+++ test/fuzztest/abilityautostartupclientfirst_fuzzer/abilityautostartupclientfirst_fuzzer.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "abilityautostartupclientfirst_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <fuzzer/FuzzedDataProvider.h>
+
+#define private public
+#include "ability_auto_startup_client.h"
+#undef private
+
+#include "securec.h"
+
+using namespace OHOS::AAFwk;
+using namespace OHOS::AppExecFwk;
+
+namespace OHOS {
+namespace {
+constexpr size_t U32_AT_SIZE = 4;
+constexpr size_t STRING_MAX_LENGTH = 128;
+}
+
+bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
+{
+    auto abilityAutoStartupClient = AbilityAutoStartupClient::GetInstance();
+    abilityAutoStartupClient->Connect();
+    AutoStartupInfo info;
+    FuzzedDataProvider fdp(data, size);
+    info.appCloneIndex = fdp.ConsumeIntegralInRange<int32_t>(0, U32_AT_SIZE);
+    info.userId = fdp.ConsumeIntegralInRange<int32_t>(0, U32_AT_SIZE);
+    info.retryCount = fdp.ConsumeIntegralInRange<int32_t>(0, U32_AT_SIZE);
+    info.bundleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    info.abilityName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    info.moduleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    info.abilityTypeName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    info.accessTokenId = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    bool boolParam = fdp.ConsumeBool();
+    abilityAutoStartupClient->SetApplicationAutoStartupByEDM(info, boolParam);
+    return true;
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    // Run your code on data.
+    OHOS::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
