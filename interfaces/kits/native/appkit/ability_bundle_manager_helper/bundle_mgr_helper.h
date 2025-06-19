82ee02a2e728ee1c418d9b4faf04717e87ecec76
lanhaoyu <lanhaoyu3@huawei-partners.com>
2025-06-19 15:11:06 +0800

    idl
    Signed-off-by: lanhaoyu <lanhaoyu3@huawei-partners.com>

--- interfaces/kits/native/appkit/ability_bundle_manager_helper/bundle_mgr_helper.h
+++ interfaces/kits/native/appkit/ability_bundle_manager_helper/bundle_mgr_helper.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -60,7 +60,6 @@ public:
     ErrCode GetDependentBundleInfo(const std::string &sharedBundleName, BundleInfo &sharedBundleInfo,
         GetDependentBundleInfoFlag flag = GetDependentBundleInfoFlag::GET_APP_CROSS_HSP_BUNDLE_INFO);
     bool GetGroupDir(const std::string &dataGroupId, std::string &dir);
-    sptr<IOverlayManager> GetOverlayManagerProxy();
     bool QueryAbilityInfo(const Want &want, AbilityInfo &abilityInfo);
     bool QueryAbilityInfo(const Want &want, int32_t flags, int32_t userId, AbilityInfo &abilityInfo);
     bool GetBundleInfos(int32_t flags,
