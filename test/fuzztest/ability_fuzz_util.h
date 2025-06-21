4ef5b9791191ef5284b3639886468c8c82aa03bc
huangshiwei <huangshiwei4@huawei.com>
2025-06-21 13:37:58 +0800

    huangshiwei4@huawei.com
    Signed-off-by: huangshiwei <huangshiwei4@huawei.com>

--- test/fuzztest/ability_fuzz_util.h
+++ test/fuzztest/ability_fuzz_util.h
@@ -24,10 +24,13 @@
 #include "auto_startup_info.h"
 #include "bundle_info.h"
 #include "bundle_user_info.h"
+#include "keep_alive_process_manager.h"
 
 namespace OHOS {
 namespace AppExecFwk {
 namespace AbilityFuzzUtil {
+constexpr size_t CODE_TWO = 2;
+constexpr size_t CODE_MAX = 99;
 constexpr size_t STRING_MAX_LENGTH = 128;
 std::vector<std::string> GenerateStringArray(FuzzedDataProvider& fdp, size_t arraySizeMax = STRING_MAX_LENGTH,
     size_t stringSize = STRING_MAX_LENGTH)
@@ -105,6 +108,36 @@ void GetRandomBundleInfo(FuzzedDataProvider& fdp, BundleInfo& info)
     info.moduleResPaths = GenerateStringArray(fdp);
     info.reqPermissions = GenerateStringArray(fdp);
     info.defPermissions = GenerateStringArray(fdp);
+}
+
+void GetRandomKeepAliveInfo(FuzzedDataProvider& fdp, KeepAliveInfo& info)
+{
+    info.userId = fdp.ConsumeIntegral<int32_t>();
+    info.setterId = fdp.ConsumeIntegral<int32_t>();
+    info.appType = static_cast<KeepAliveAppType>(fdp.ConsumeIntegralInRange<int32_t>(0, CODE_TWO));
+    info.setter = static_cast<KeepAliveSetter>(fdp.ConsumeIntegralInRange<int32_t>(0, CODE_TWO));
+    info.policy = static_cast<KeepAlivePolicy>(fdp.ConsumeIntegralInRange<int32_t>(0, CODE_TWO));
+    info.bundleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+}
+
+void GetRandomKeepAliveAbilityInfo(FuzzedDataProvider& fdp, KeepAliveAbilityInfo& info)
+{
+    info.userId = fdp.ConsumeIntegral<int32_t>();
+    info.appCloneIndex = fdp.ConsumeIntegral<int32_t>();
+    info.uid = fdp.ConsumeIntegral<int32_t>();
+    info.bundleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    info.moduleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    info.abilityName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+}
+
+void GetRandomKeepAliveAppInfo(FuzzedDataProvider& fdp, AppInfo& info)
+{
+    info.processName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    info.state = static_cast<AppState>(fdp.ConsumeIntegralInRange<int32_t>(0, CODE_TWO));
+    info.pid = fdp.ConsumeIntegral<uint32_t>();
+    info.appIndex = fdp.ConsumeIntegral<int32_t>();
+    info.instanceKey = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    info.bundleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
 }
 }  // namespace AbilityFuzzUtil
 }  // namespace AppExecFwk
