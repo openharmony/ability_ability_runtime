c52e44c0a9930f531e394d677c9d895729d1caf3
yangzk <yangzhongkai@huawei.com>
2025-07-01 19:01:18 +0800

    Description: add native startup mgr IssueNo: Sig: SIG_ApplicationFramework Feature or Bugfix: Feature Binary Source: No
    Signed-off-by: yangzk <yangzhongkai@huawei.com>
    Change-Id: I114ed4f373985ab827c296bcddf3e8388360a10f

---
+++ interfaces/kits/native/appkit/app_startup/ext_native_startup_manager.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_ABILITY_RUNTIME_EXT_NATIVE_STARTUP_MANAGER_H
+#define OHOS_ABILITY_RUNTIME_EXT_NATIVE_STARTUP_MANAGER_H
+
+#include <map>
+#include <memory>
+
+#include "ext_native_startup_task.h"
+#include "startup_task.h"
+
+namespace OHOS {
+namespace AbilityRuntime {
+class ExtNativeStartupManager {
+public:
+    static void LoadExtStartupTask();
+
+    static int32_t BuildExtStartupTask(const std::shared_ptr<ExtNativeStartupTask> &extNativeStartupTask,
+        std::shared_ptr<StartupTask> &startupTask);
+
+    static int32_t RunNativeStartupTask(const std::map<std::string, std::shared_ptr<StartupTask>> &nativeStartupTask);
+};
+} // namespace AbilityRuntime
+} // namespace OHOS
+#endif // OHOS_ABILITY_RUNTIME_EXT_NATIVE_STARTUP_MANAGER_H
---
+++ test/new_test/mock/app_startup/startup_utils/startup_utils.h
@@ -0,0 +1,55 @@
+/*
+* Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_ABILITY_RUNTIME_MOCK_STARTUP_UTILS_H
+#define OHOS_ABILITY_RUNTIME_MOCK_STARTUP_UTILS_H
+
+#include <string>
+#include <gmock/gmock.h>
+
+#include "errors.h"
+
+namespace OHOS {
+namespace AbilityRuntime {
+enum {
+    ERR_STARTUP_INVALID_VALUE = 401,
+    ERR_STARTUP_INTERNAL_ERROR = 16000050,
+    ERR_STARTUP_DEPENDENCY_NOT_FOUND = 28800001,
+    ERR_STARTUP_CIRCULAR_DEPENDENCY = 28800002,
+    ERR_STARTUP_FAILED_TO_EXECUTE_STARTUP = 28800003,
+    ERR_STARTUP_TIMEOUT = 28800004,
+    ERR_STARTUP_CONFIG_NOT_FOUND = 28800005,
+    ERR_STARTUP_CONFIG_PATH_ERROR = 28800006,
+    ERR_STARTUP_CONFIG_PARSE_ERROR = 28800007,
+};
+
+class StartupUtils {
+public:
+    static StartupUtils &GetInstance()
+    {
+        static StartupUtils instance;
+        return instance;
+    }
+
+    static std::string GetErrorMessage(int32_t errCode)
+    {
+        return GetInstance().MockGetErrorMessage(errCode);
+    }
+
+    MOCK_METHOD(std::string, MockGetErrorMessage, (int32_t), (const));
+};
+} // namespace AbilityRuntime
+} // namespace OHOS
+#endif // OHOS_ABILITY_RUNTIME_MOCK_STARTUP_UTILS_H
