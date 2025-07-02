dc3411ecd1706f0a32d34ad1ba14cb09c216ca03
xiehd <xiehuandong@huawei.com>
2025-07-02 19:28:22 +0800

    fix abilityStage attch
    Signed-off-by: xiehd <xiehuandong@huawei.com>

--- frameworks/native/appkit/ability_runtime/app/js_ability_stage.cpp
+++ frameworks/native/appkit/ability_runtime/app/js_ability_stage.cpp
@@ -906,13 +906,11 @@ void JsAbilityStage::SetJsAbilityStage(const std::shared_ptr<Context> &context)
     if (obj != nullptr) {
         napi_set_named_property(env, obj, "context", contextObj);
     }
-    TAG_LOGD(AAFwkTag::APPKIT, "Set ability stage context");
     napi_status status = napi_wrap(env, contextObj, workContext,
         [](napi_env, void* data, void*) {
             TAG_LOGD(AAFwkTag::APPKIT, "Finalizer for weak_ptr ability stage context is called");
             delete static_cast<std::weak_ptr<AbilityRuntime::Context>*>(data);
-        },
-        nullptr, nullptr);
+        }, nullptr, nullptr);
     if (status != napi_ok && workContext != nullptr) {
         TAG_LOGD(AAFwkTag::APPKIT, "napi_wrap Failed: %{public}d", status);
         delete workContext;
