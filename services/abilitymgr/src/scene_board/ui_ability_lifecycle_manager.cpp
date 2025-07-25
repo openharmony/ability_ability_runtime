e7bda6f8ca0e54ab60d49cc1aaf3f53cce5d5af1
duansizhao <duansizhao@huawei.com>
2025-07-25 11:04:54 +0800

    修复backToCaller弹窗问题
    Signed-off-by: duansizhao <duansizhao@huawei.com>

--- services/abilitymgr/src/scene_board/ui_ability_lifecycle_manager.cpp
+++ services/abilitymgr/src/scene_board/ui_ability_lifecycle_manager.cpp
@@ -1852,6 +1852,7 @@ int32_t UIAbilityLifecycleManager::BackToCallerAbilityWithResultLocked(sptr<Sess
 
     auto currentSession = iface_cast<Rosen::ISession>(currentSessionInfo->sessionToken);
     callerSessionInfo->isBackTransition = true;
+    callerSessionInfo->want.SetParam(ServerConstant::IS_CALL_BY_SCB, false);
     callerSessionInfo->scenarios = ServerConstant::SCENARIO_BACK_TO_CALLER_ABILITY_WITH_RESULT;
     auto ret = static_cast<int>(currentSession->PendingSessionActivation(callerSessionInfo));
     callerSessionInfo->isBackTransition = false;
