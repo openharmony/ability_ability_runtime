f3b2ddbd6cf15f5a84adb777e1a7407289fef8dd
wangzhen <wangzhen416@huawei.com>
2025-07-29 14:27:56 +0800

    Add trace
    Signed-off-by: wangzhen <wangzhen416@huawei.com>
    Change-Id: If3d6f5d650d250a4d213b08d8b3f40e2c84f5a3c

--- services/appmgr/src/app_mgr_service_inner.cpp
+++ services/appmgr/src/app_mgr_service_inner.cpp
@@ -2965,6 +2965,7 @@ void AppMgrServiceInner::GetChildrenProcesses(const std::shared_ptr<AppRunningRe
 
 int32_t AppMgrServiceInner::KillProcessByPid(const pid_t pid, const std::string& reason)
 {
+    HITRACE_METER_NAME(HITRACE_TAG_APP, __PRETTY_FUNCTION__);
     if (!ProcessUtil::ProcessExist(pid)) {
         TAG_LOGI(AAFwkTag::APPMGR, "null killProcessByPid, pid: %{public}d", pid);
         return AAFwk::ERR_KILL_PROCESS_NOT_EXIST;
@@ -3478,6 +3479,7 @@ int32_t AppMgrServiceInner::KillProcessesInBatch(const std::vector<int32_t> &pid
 int32_t AppMgrServiceInner::KillSubProcessBypidInner(const pid_t pid, const std::string &reason,
     AAFwk::EventInfo &eventInfo)
 {
+    HITRACE_METER_NAME(HITRACE_TAG_APP, __PRETTY_FUNCTION__);
     int32_t ret = -1;
     if (!ProcessUtil::ProcessExist(pid) || pid <= 0) {
         TAG_LOGE(AAFwkTag::APPMGR, "invalid pid: %{public}d", pid);
@@ -3505,6 +3507,7 @@ int32_t AppMgrServiceInner::KillSubProcessBypidInner(const pid_t pid, const std:
 
 int32_t AppMgrServiceInner::KillSubProcessBypid(const pid_t pid, const std::string &reason)
 {
+    HITRACE_METER_NAME(HITRACE_TAG_APP, __PRETTY_FUNCTION__);
     int32_t ret = ERR_OK;
     if (!appRunningManager_) {
         TAG_LOGE(AAFwkTag::APPMGR, "appRunningManager_ null");
@@ -4985,6 +4988,7 @@ void AppMgrServiceInner::GetRunningProcessInfoByToken(
 int32_t AppMgrServiceInner::GetRunningProcessInfoByPid(const pid_t pid,
     OHOS::AppExecFwk::RunningProcessInfo &info) const
 {
+    HITRACE_METER_NAME(HITRACE_TAG_APP, __PRETTY_FUNCTION__);
     TAG_LOGD(AAFwkTag::APPMGR, "called");
     if (!CheckGetRunningInfoPermission()) {
         return ERR_PERMISSION_DENIED;
