4f955b08e7e5e7f7537dc18cfefb38d5e73fdf00
zhang_hao_zheng <zhanghaozheng2@h-partners.com>
2025-07-30 14:53:09 +0800

    挑单0728 eventhub
    Signed-off-by: zhang_hao_zheng <zhanghaozheng2@h-partners.com>
    Change-Id: Id2bb86fcf7f51ec6fd884a77cfd7b5bdbf19baee

--- frameworks/ets/ani/ani_common/src/ets_context_utils.cpp
+++ frameworks/ets/ani/ani_common/src/ets_context_utils.cpp
@@ -24,6 +24,7 @@
 #include "ets_application_context_utils.h"
 #include "ets_error_utils.h"
 #include "ets_native_reference.h"
+#include "event_hub.h"
 #include "hilog_tag_wrapper.h"
 #include "ipc_skeleton.h"
 #include "resourceManager.h"
@@ -342,6 +343,21 @@ void CreateEtsBaseContext(ani_env *aniEnv, ani_class contextClass, ani_object co
     }
     BindParentProperty(aniEnv, contextClass, contextObj, context);
     BindNativeFunction(aniEnv);
+    // set eventHub context
+    ani_ref eventHubRef = nullptr;
+    ani_status status = ANI_OK;
+    if ((status = aniEnv->Object_GetFieldByName_Ref(contextObj, "eventHub", &eventHubRef)) != ANI_OK) {
+        TAG_LOGE(AAFwkTag::APPKIT, "Object_GetFieldByName_Ref failed status: %{public}d", status);
+        return;
+    }
+
+    auto workContext = new (std::nothrow) std::weak_ptr<AbilityRuntime::Context>(context);
+    if (workContext == nullptr) {
+        TAG_LOGE(AAFwkTag::APPKIT, "workContext null");
+        return;
+    }
+    ani_long nativeContextLong = (ani_long)workContext;
+    AbilityRuntime::EventHub::SetEventHubContext(aniEnv, eventHubRef, nativeContextLong);
 }
 
 std::shared_ptr<Context> GetBaseContext(ani_env *env, ani_object aniObj)
--- frameworks/js/napi/app/context/context.js
+++ frameworks/js/napi/app/context/context.js
@@ -16,9 +16,14 @@
 class EventHub {
   constructor() {
     this.eventMap = {};
+    this.nativeEventHubRef = null;
     this.contextIndex = -1;
     this.emitMultiThreadingEnabled = false;
     this.emitter = undefined;
+  }
+
+  setNativeEventHubRef(ref) {
+    this.nativeEventHubRef = ref;
   }
 
   onEmitterFunction(eventData = {'data':{}}) {
@@ -91,6 +96,26 @@ class EventHub {
     }
   }
 
+  offByNativeContext(event, callback) {
+    if (typeof (event) !== 'string') {
+      return;
+    }
+    if (this.eventMap[event]) {
+      if (callback) {
+        let cbArray = this.eventMap[event];
+        let index = cbArray.indexOf(callback);
+        if (index > -1) {
+          for (; index + 1 < cbArray.length; index++) {
+            cbArray[index] = cbArray[index + 1];
+          }
+          cbArray.pop();
+        }
+      } else {
+        delete this.eventMap[event];
+      }
+    }
+  }
+
   off(event, callback) {
     if (typeof (event) !== 'string') {
       return;
@@ -107,6 +132,14 @@ class EventHub {
         }
       } else {
         delete this.eventMap[event];
+      }
+    }
+    if (this.nativeEventHubRef != null) {
+      // call native eventHub off
+      if (callback) {
+        this.nativeEventHubRef.offByDynamicContext(event, callback);
+      } else {
+        this.nativeEventHubRef.offByDynamicContext(event);
       }
     }
     if (this.emitMultiThreadingEnabled === true && this.emitter !== undefined) {
@@ -129,6 +162,22 @@ class EventHub {
   }
 
   emitInner(event, ...args) {
+    if (typeof (event) !== 'string') {
+      return;
+    }
+    if (this.eventMap[event]) {
+      const cloneArray = [...this.eventMap[event]];
+      const len = cloneArray.length;
+      for (let i = 0; i < len; ++i) {
+        cloneArray[i].apply(this, args);
+      }
+    }
+    if (this.nativeEventHubRef != null) {
+      this.nativeEventHubRef.emitByDynamicContext(event, ...args);
+    }
+  }
+
+  emitByNativeContext(event, ...args) {
     if (typeof (event) !== 'string') {
       return;
     }
