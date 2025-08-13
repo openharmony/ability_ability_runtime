3d64ed05652cbe2a399e35963ca0b8236214ca71
zhaoyrr <zhaoyuran@h-partners.com>
2025-08-13 17:21:47 +0800

    aa命令问题修复
    Signed-off-by: zhaoyrr <zhaoyuran@h-partners.com>

--- tools/aa/include/ability_command.h
+++ tools/aa/include/ability_command.h
@@ -63,7 +63,8 @@ const std::string HELP_MSG_START =
     "usage: aa start <options>\n"
     "options list:\n"
     "  -h, --help                                                   list available commands\n"
-    "  [-d <device-id>] [-a <ability-name> -b <bundle-name>] [-m <module-name>] [-p <perf-cmd>] [-D] [-E] [-S] [-N]"
+    "  [-d <device-id>] [-a <ability-name> -b <bundle-name>] [-m <module-name>] [-p <perf-cmd>] "
+    "  [-C] [-D] [-E] [-S] [-N] "
     "  [-R] [-c] [--ps <key> <string-value>] "
     "  [--pi <key> <integer-value>] "
     "  [--pb <key> <boolean-value>] "
@@ -272,6 +273,7 @@ private:
     sptr<IAbilityManager> GetAbilityManagerService();
 
     ErrCode MakeWantFromCmd(Want& want, std::string& windowMode);
+    ErrCode MakeWantFromCmdForStopService(Want& want);
     ErrCode MakeWantForProcess(Want& want);
     ErrCode RunAsTestCommand();
     ErrCode TestCommandError(const std::string& info);
