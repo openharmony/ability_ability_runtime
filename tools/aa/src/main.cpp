df223902b042843b1951e78c1250862d9e2caa7e
unknown <sijunjie@huawei.com>
2025-08-18 14:46:43 +0800

    add TDD
    Signed-off-by: unknown <sijunjie@huawei.com>

--- tools/aa/src/main.cpp
+++ tools/aa/src/main.cpp
@@ -58,13 +58,6 @@ int main(int argc, char* argv[])
         CommandTimer commandTimer("ability::aa_command", COMMAND_TIME_OUT, operation);
         OHOS::AAFwk::AbilityManagerShellCommand cmd(argc, argv);
         std::cout << cmd.ExecCommand();
-    } else {
-#ifdef A11Y_ENABLE
-        if (strstr(argv[0], "accessibility") != nullptr) {
-            OHOS::AAFwk::AccessibilityAbilityShellCommand cmd(argc, argv);
-            std::cout << cmd.ExecCommand();
-        }
-#endif // A11Y_ENABLE
     }
     fflush(stdout);
     _exit(0);
