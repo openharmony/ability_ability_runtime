0a0d5a18b6a28990bf090d6d31a1bcf3a33759d8
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-08-21 10:28:40 +0800

    change ui_extension_provider_bundle ohos_hap to ohos_app
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- test/moduletest/ui_extension_ability_test/ui_extension_provider_bundle/entry/src/main/ets/IdlServiceExt/idl_service_ext_stub.ts
+++ test/moduletest/ui_extension_ability_test/ui_extension_provider_bundle/entry/src/main/ets/IdlServiceExt/idl_service_ext_stub.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,48 +13,52 @@
  * limitations under the License.
  */
 
-import {processDataCallback} from "./i_idl_service_ext";
-import {insertDataToMapCallback} from "./i_idl_service_ext";
+import { processDataCallback } from "./i_idl_service_ext";
+import { insertDataToMapCallback } from "./i_idl_service_ext";
 import IIdlServiceExt from "./i_idl_service_ext";
 import rpc from "@ohos.rpc";
 
 export default class IdlServiceExtStub extends rpc.RemoteObject implements IIdlServiceExt {
-    constructor(des: string) {
-        super(des);
+  constructor(des: string) {
+    super(des);
+  }
+
+  async onRemoteMessageRequest(code: number, data: rpc.MessageSequence, reply: rpc.MessageSequence,
+    option: rpc.MessageOption): Promise<boolean> {
+    console.log("onRemoteMessageRequest called, code = " + code);
+    switch (code) {
+      case IdlServiceExtStub.COMMAND_PROCESS_DATA: {
+        let _data = data.readInt();
+        this.processData(_data, (errCode, returnValue) => {
+          reply.writeInt(errCode);
+          if (errCode == 0) {
+            reply.writeInt(returnValue);
+          }
+        });
+        return true;
+      }
+      case IdlServiceExtStub.COMMAND_INSERT_DATA_TO_MAP: {
+        let _key = data.readString();
+        let _val = data.readInt();
+        this.insertDataToMap(_key, _val, (errCode) => {
+          reply.writeInt(errCode);
+        });
+        return true;
+      }
+      default: {
+        console.log("invalid request code" + code);
+        break;
+      }
     }
+    return false;
+  }
 
-    async onRemoteMessageRequest(code: number, data, reply, option): Promise<boolean> {
-        console.log("onRemoteMessageRequest called, code = " + code);
-        switch(code) {
-            case IdlServiceExtStub.COMMAND_PROCESS_DATA: {
-                let _data = data.readInt();
-                this.processData(_data, (errCode, returnValue) => {
-                    reply.writeInt(errCode);
-                    if (errCode == 0) {
-                        reply.writeInt(returnValue);
-                    }
-                });
-                return true;
-            }
-            case IdlServiceExtStub.COMMAND_INSERT_DATA_TO_MAP: {
-                let _key = data.readString();
-                let _val = data.readInt();
-                this.insertDataToMap(_key, _val, (errCode) => {
-                    reply.writeInt(errCode);
-                });
-                return true;
-            }
-            default: {
-                console.log("invalid request code" + code);
-                break;
-            }
-        }
-        return false;
-    }
+  processData(data: number, callback: processDataCallback): void {
+  }
 
-    processData(data: number, callback: processDataCallback): void{}
-    insertDataToMap(key: string, val: number, callback: insertDataToMapCallback): void{}
+  insertDataToMap(key: string, val: number, callback: insertDataToMapCallback): void {
+  }
 
-    static readonly COMMAND_PROCESS_DATA = 1;
-    static readonly COMMAND_INSERT_DATA_TO_MAP = 2;
+  static readonly COMMAND_PROCESS_DATA = 1;
+  static readonly COMMAND_INSERT_DATA_TO_MAP = 2;
 }
