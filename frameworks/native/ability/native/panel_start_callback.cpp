b03106d1344bad9281c371c414f28d0b5c118960
番茄鱼 <qyifang0623@gmail.com>
2025-08-26 22:20:21 +0800

    napi for start vertical panel
    Signed-off-by: 番茄鱼 <qyifang0623@gmail.com>
    Signed-off-by: Dylan余 <dylanyuhong@163.com>

---
+++ frameworks/native/ability/native/panel_start_callback.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "panel_start_callback.h"
+
+#include "hilog_tag_wrapper.h"
+#ifdef SUPPORT_SCREEN
+#include "ui_content.h"
+#include "ws_common.h"
+#endif // SUPPORT_SCREEN
+
+namespace OHOS {
+namespace AbilityRuntime {
+void PanelStartCallback::SetSessionId(int32_t sessionId)
+{
+    sessionId_ = sessionId;
+}
+#ifdef SUPPORT_SCREEN
+void PanelStartCallback::SetUIContent(Ace::UIContent* uiContent)
+{
+    uiContent_ = uiContent;
+}
+
+void PanelStartCallback::OnRelease(int32_t code)
+{
+    TAG_LOGI(AAFwkTag::VERTICAL_PANEL, "call, code:%{public}d", code);
+    CloseModalUIExtension();
+}
+
+void PanelStartCallback::CloseModalUIExtension()
+{
+    if (uiContent_ == nullptr) {
+        TAG_LOGE(AAFwkTag::VERTICAL_PANEL, "null uiContent_");
+        return;
+    }
+    uiContent_->CloseModalUIExtension(sessionId_);
+}
+#endif // SUPPORT_SCREEN
+} // namespace AbilityRuntime
+} // namespace OHOS
