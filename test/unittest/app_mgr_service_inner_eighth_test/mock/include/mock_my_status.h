fff8e1686ecba89391ce35d92edb1d7fcea6c08f
zhang_hao_zheng <zhanghaozheng2@h-partners.com>
2025-08-28 15:52:04 +0800

    add tdd
    Signed-off-by: zhang_hao_zheng <zhanghaozheng2@h-partners.com>
    Change-Id: I4c36ae1c83a2f6f89ee3826236ffbdd38c83562d

--- test/unittest/app_mgr_service_inner_eighth_test/mock/include/mock_my_status.h
+++ test/unittest/app_mgr_service_inner_eighth_test/mock/include/mock_my_status.h
@@ -92,7 +92,7 @@ public:
     AppExecFwk::ApplicationInfo applicationInfo_;
     int getOverlayCall_ = 0;
     sptr<AppExecFwk::IOverlayManager> getOverlay_ = nullptr;
-    int getBaseSharedBundleInfos_ = 0;
+    ErrCode getBaseSharedBundleInfos_ = ERR_OK;
     std::vector<AppExecFwk::BaseSharedBundleInfo> baseSharedBundleInfos_;
     bool queryDataGroupInfos_ = false;
     std::vector<AppExecFwk::DataGroupInfo> queryData_;
