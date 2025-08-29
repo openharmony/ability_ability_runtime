7f2279c3f3f99fa207503beb98fc40fc5829a51c
任国军 <renguojun1@h-partners.com>
2025-08-29 16:07:44 +0800

    add ReasonMessage_Notification
    Signed-off-by: 任国军 <renguojun1@h-partners.com>

--- services/common/src/app_utils.cpp
+++ services/common/src/app_utils.cpp
@@ -667,6 +667,7 @@ bool AppUtils::IsSystemReasonMessage(const std::string &reasonMessage)
     const std::unordered_set<std::string> systemReasonMessagesSet = {
         "ReasonMessage_SystemShare",
         "ReasonMessage_DesktopShortcut",
+        "ReasonMessage_Notification",
     };
     return systemReasonMessagesSet.find(reasonMessage) != systemReasonMessagesSet.end();
 }
