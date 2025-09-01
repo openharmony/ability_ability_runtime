f9449512434ff05252ddd9bf20beb6a78eeedf99
wangzhen <wangzhen416@huawei.com>
2025-09-01 10:06:44 +0800

    Optimization for specified process
    Signed-off-by: wangzhen <wangzhen416@huawei.com>
    Change-Id: I85ecdebb08311c32b263de6cb8204fa7be17100e

--- test/unittest/ability_manager_service_thirteenth_test/mock/src/mock_ui_ability_lifecycle_manager.cpp
+++ test/unittest/ability_manager_service_thirteenth_test/mock/src/mock_ui_ability_lifecycle_manager.cpp
@@ -718,7 +718,8 @@ std::shared_ptr<SpecifiedRequest> UIAbilityLifecycleManager::PopAndGetNextSpecif
     return nullptr;
 }
 
-bool UIAbilityLifecycleManager::IsSpecifiedModuleLoaded(const AbilityRequest &abilityRequest, bool &isDebug)
+bool UIAbilityLifecycleManager::IsSpecifiedModuleLoaded(const AbilityRequest &abilityRequest,
+    bool isSpecifiedProcess, bool &isDebug)
 {
     return true;
 }
