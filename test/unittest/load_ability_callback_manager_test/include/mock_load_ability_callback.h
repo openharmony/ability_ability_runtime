7f2233904a992ac5e0f2b4ab307465ee89702601
Nathan Yang <yangxuguang3@huawei.com>
2025-09-14 15:46:04 +0800

    TDD: StartSelfUIAbilityWithPidResult
    Signed-off-by: Nathan Yang <yangxuguang3@huawei.com>

---
+++ test/unittest/load_ability_callback_manager_test/include/mock_load_ability_callback.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ABILITY_RUNTIME_MOCK_LOAD_ABILITY_CALLBACK_H
+#define ABILITY_RUNTIME_MOCK_LOAD_ABILITY_CALLBACK_H
+
+#include <functional>
+
+#include "load_ability_callback_stub.h"
+
+namespace OHOS {
+namespace AbilityRuntime {
+using OHOS::AppExecFwk::LoadAbilityCallbackStub;
+using OnFinishTask = std::function<void(int32_t)>;
+class MockLoadAbilityCallback : public LoadAbilityCallbackStub {
+public:
+    explicit MockLoadAbilityCallback(OnFinishTask &&task) : task_(task) {}
+    virtual ~MockLoadAbilityCallback() = default;
+
+    /**
+     * Callback to return pid.
+     *
+     * @param pid Process id.
+     */
+    virtual void OnFinish(int32_t pid) override;
+
+private:
+    OnFinishTask task_;
+};
+}
+}
+#endif // ABILITY_RUNTIME_MOCK_LOAD_ABILITY_CALLBACK_H
