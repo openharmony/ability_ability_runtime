2def7efd7101a5d0831dcb8d8483a9c46116ed20
查维 <zhawei12@huawei-partners.com>
2025-09-15 16:19:32 +0800

    解决ability_manager_service_first_test&ability_manager_sixth_test测试套重复符号问题
    Signed-off-by: 查维 <zhawei12@huawei-partners.com>

--- test/unittest/ability_manager_service_first_test/mock/src/mock_permission_verification.cpp
+++ test/unittest/ability_manager_service_first_test/mock/src/mock_permission_verification.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 - 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,120 +13,284 @@
  * limitations under the License.
  */
 
-#include "mock_permission_verification.h"
+#include "ability_manager_errors.h"
+#include "permission_verification.h"
+#include "hilog_tag_wrapper.h"
+#include "mock_my_flag.h"
 
 namespace OHOS {
 namespace AAFwk {
 
-bool PermissionVerification::VerifyCallingPermission(const std::string &permissionName) const
+bool PermissionVerification::VerifyPermissionByTokenId(const int &tokenId, const std::string &permissionName) const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
+
+bool PermissionVerification::VerifyCallingPermission(
+    const std::string &permissionName, const uint32_t specifyTokenId) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return !!(MyFlag::flag_);
+}
+
 bool PermissionVerification::IsSACall() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return (MyFlag::flag_ & MyFlag::FLAG::IS_SA_CALL);
 }
+
+bool PermissionVerification::IsSACallByTokenId(uint32_t callerTokenId) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return !!(MyFlag::flag_);
+}
+
 bool PermissionVerification::IsShellCall() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return (MyFlag::flag_ & MyFlag::FLAG::IS_SHELL_CALL);
 }
-bool PermissionVerification::CheckSpecificSystemAbilityAccessPermission() const
+
+bool PermissionVerification::IsShellCallByTokenId(uint32_t callerTokenId) const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
+
+bool PermissionVerification::CheckSpecificSystemAbilityAccessPermission(const std::string &processName) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return !!(MyFlag::flag_);
+}
+
+bool PermissionVerification::CheckObserverCallerPermission() const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return !!(MyFlag::flag_);
+}
+
 bool PermissionVerification::VerifyRunningInfoPerm() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
+
+bool PermissionVerification::VerifyCustomSandbox(uint32_t accessTokenId) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return !!(MyFlag::flag_);
+}
+
 bool PermissionVerification::VerifyControllerPerm() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
+
 #ifdef WITH_DLP
 bool PermissionVerification::VerifyDlpPermission(Want &want) const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
 #endif // WITH_DLP
 int PermissionVerification::VerifyAccountPermission() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return MyFlag::flag_;
 }
+
 bool PermissionVerification::VerifyMissionPermission() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
+
 int PermissionVerification::VerifyAppStateObserverPermission() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return MyFlag::flag_;
 }
+
 int32_t PermissionVerification::VerifyUpdateConfigurationPerm() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return static_cast<int32_t>(MyFlag::flag_);
 }
+
 bool PermissionVerification::VerifyInstallBundlePermission() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
+
 bool PermissionVerification::VerifyGetBundleInfoPrivilegedPermission() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
+
+bool PermissionVerification::VerifyStartRecentAbilityPermission() const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return !!(MyFlag::flag_);
+}
+
 int PermissionVerification::CheckCallDataAbilityPermission(const VerificationInfo &verificationInfo, bool isShell) const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return MyFlag::flag_;
 }
+
 int PermissionVerification::CheckCallServiceAbilityPermission(const VerificationInfo &verificationInfo) const
 {
-    return MyFlag::flag_;
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return ERR_OK;
 }
-int PermissionVerification::CheckCallAbilityPermission(const VerificationInfo &verificationInfo) const
+
+int PermissionVerification::CheckCallAbilityPermission(const VerificationInfo &verificationInfo,
+    bool isCallByShortcut) const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return MyFlag::flag_;
 }
+
 int PermissionVerification::CheckCallServiceExtensionPermission(const VerificationInfo &verificationInfo) const
 {
-    return MyFlag::flag_;
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return ERR_OK;
 }
+
 int PermissionVerification::CheckStartByCallPermission(const VerificationInfo &verificationInfo) const
 {
-    return MyFlag::flag_;
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return ERR_OK;
 }
+
 unsigned int PermissionVerification::GetCallingTokenID() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return static_cast<unsigned int>(MyFlag::flag_);
 }
-bool PermissionVerification::JudgeStartInvisibleAbility(const uint32_t accessTokenId, const bool visible) const
+
+bool PermissionVerification::JudgeStartInvisibleAbility(const uint32_t accessTokenId, const bool visible,
+    const uint32_t specifyTokenId) const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
+
 bool PermissionVerification::JudgeStartAbilityFromBackground(const bool isBackgroundCall) const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
+
 bool PermissionVerification::JudgeAssociatedWakeUp(const uint32_t accessTokenId, const bool associatedWakeUp) const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return !!(MyFlag::flag_);
 }
-int PermissionVerification::JudgeInvisibleAndBackground(const VerificationInfo &verificationInfo) const
+
+int PermissionVerification::JudgeInvisibleAndBackground(const VerificationInfo &verificationInfo,
+    bool isCallByShortcut) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return MyFlag::flag_;
+}
+
+bool PermissionVerification::JudgeCallerIsAllowedToUseSystemAPI() const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return true;
+}
+
+bool PermissionVerification::VerifyPrepareTerminatePermission() const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return true;
+}
+
+bool PermissionVerification::VerifyPrepareTerminatePermission(const int &tokenId) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return true;
+}
+
+bool PermissionVerification::VerifyShellStartExtensionType(int32_t type) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return MyFlag::flag_;
+}
+
+bool PermissionVerification::VerifyPreloadApplicationPermission() const
 {
     return MyFlag::flag_;
 }
-bool PermissionVerification::JudgeCallerIsAllowedToUseSystemAPI() const
+
+bool PermissionVerification::VerifyPreStartAtomicServicePermission() const
 {
-    return true;
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return MyFlag::flag_;
 }
-bool PermissionVerification::VerifyPrepareTerminatePermission() const
-{
-    return true;
-}
+
 bool PermissionVerification::IsSystemAppCall() const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return true;
+}
+
+bool PermissionVerification::IsSystemAppCallByTokenId(uint32_t callerTokenId) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return true;
+}
+
+bool PermissionVerification::VerifyBackgroundCallPermission(const bool isBackgroundCall) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return MyFlag::flag_;
+}
+
+bool PermissionVerification::VerifyKillProcessDependedOnWebPermission() const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return true;
+}
+
+bool PermissionVerification::VerifyBlockAllAppStartPermission() const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return true;
+}
+
+bool PermissionVerification::VerifyStartUIAbilityToHiddenPermission() const
 {
     return true;
 }
 
-bool PermissionVerification::VerifySetProcessCachePermission() const
+bool PermissionVerification::VerifySuperviseKiaServicePermission() const
 {
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return true;
+}
+
+bool PermissionVerification::VerifyStartLocalDebug(int32_t tokenId) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return true;
+}
+
+bool PermissionVerification::VerifyStartSelfUIAbility(int tokenId) const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
+    return true;
+}
+
+bool PermissionVerification::VerifyFusionAccessPermission() const
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s enter", __func__);
     return true;
 }
 }  // namespace AAFwk
