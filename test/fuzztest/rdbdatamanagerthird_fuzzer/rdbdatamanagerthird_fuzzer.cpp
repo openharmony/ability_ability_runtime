780196410020a92cb513689e139c89bc6311f9c6
y30069878 <y30069878@notesmail.huawei.com/>
2025-09-23 14:54:45 +0800

    add rdbdatamanager fuzz
    Change-Id: I14ef9b1ed3a99a3c5ce1be65bca1ad14b442f4bb
    Signed-off-by: hy051901341 <hy05190134@qq.com>

---
+++ test/fuzztest/rdbdatamanagerthird_fuzzer/rdbdatamanagerthird_fuzzer.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rdbdatamanagerthird_fuzzer.h"
+
+#include <charconv>
+#include <fuzzer/FuzzedDataProvider.h>
+#include "hilog_tag_wrapper.h"
+#include "parser_util.h"
+
+#define private public
+#include "ability_resident_process_rdb.h"
+#include "rdb_data_manager.h"
+#undef private
+
+using namespace OHOS::AAFwk;
+using namespace OHOS::AbilityRuntime;
+
+namespace OHOS {
+namespace {
+constexpr size_t STRING_MAX_LENGTH = 128;
+const std::string INTENT_KEY = "INTENT_KEY";
+}
+
+bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
+{
+    struct AmsRdbConfig amsRdbConfig;
+    amsRdbConfig.tableName = "resident_process_list";
+    AmsResidentProcessRdbCallBack amsCallback(amsRdbConfig);
+    std::unique_ptr<RdbDataManager> rdbMgr =
+        std::make_unique<RdbDataManager>(amsRdbConfig);
+    rdbMgr->Init(amsCallback);
+    FuzzedDataProvider fdp(data, size);
+    NativeRdb::AbsRdbPredicates absRdbPredicates(amsRdbConfig.tableName);
+    absRdbPredicates.EqualTo(INTENT_KEY, fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH));
+    rdbMgr->QueryData(absRdbPredicates);
+    return true;
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    // Run your code on data.
+    OHOS::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
+
