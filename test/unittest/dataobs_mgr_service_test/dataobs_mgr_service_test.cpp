10ec5798e7ec71b25f3bbb335e45f62150ebc1bb
Cui-Ziyuan1128 <cuiziyuan1@h-partners.com>
2025-09-23 17:36:12 +0800

    debug
    Signed-off-by: Cui-Ziyuan1128 <cuiziyuan1@h-partners.com>

--- test/unittest/dataobs_mgr_service_test/dataobs_mgr_service_test.cpp
+++ test/unittest/dataobs_mgr_service_test/dataobs_mgr_service_test.cpp
@@ -703,15 +703,15 @@ HWTEST_F(DataObsMgrServiceTest, AaFwk_DataObsMgrServiceTest_CheckSystemCallingPe
     DataObsOption opt;
     bool ret = 0;
     opt.isSystem = false;
-    ret = dataObsMgrServer->IsCallingPermissionValid(opt, IDataObsMgr::DATAOBS_DEFAULT_CURRENT_USER, -1);
-    EXPECT_EQ(ret, false);
-    ret = dataObsMgrServer->IsCallingPermissionValid(opt, IDataObsMgr::DATAOBS_DEFAULT_CURRENT_USER, 100);
-    EXPECT_EQ(ret, true);
-    ret = dataObsMgrServer->IsCallingPermissionValid(opt, 100, 101);
-    EXPECT_EQ(ret, false);
+    auto ret1 = dataObsMgrServer->IsCallingPermissionValid(opt, IDataObsMgr::DATAOBS_DEFAULT_CURRENT_USER, -1);
+    EXPECT_EQ(ret1, false);
+    auto ret2 = dataObsMgrServer->IsCallingPermissionValid(opt, IDataObsMgr::DATAOBS_DEFAULT_CURRENT_USER, 100);
+    EXPECT_EQ(ret2, true);
+    auto ret3 = dataObsMgrServer->IsCallingPermissionValid(opt, 100, 101);
+    EXPECT_EQ(ret3, false);
     opt.isSystem = true;
-    ret = dataObsMgrServer->IsCallingPermissionValid(opt, IDataObsMgr::DATAOBS_DEFAULT_CURRENT_USER, 100);
-    EXPECT_EQ(ret, false);
+    auto ret4 = dataObsMgrServer->IsCallingPermissionValid(opt, IDataObsMgr::DATAOBS_DEFAULT_CURRENT_USER, 100);
+    EXPECT_EQ(ret4, false);
 
     SetSelfTokenID(originalToken);
     TAG_LOGI(AAFwkTag::DBOBSMGR, "AaFwk_DataObsMgrServiceTest_CheckSystemCallingPermission_0200 end");
