249a2d088de116dfdcbe8bd09f22073f19aecbb3
zhongshield1 <zhangzezhong8@huawei-partners.com>
2025-09-24 17:08:41 +0800

    fix rpc taihe
    Signed-off-by: zhongshield1 <zhangzezhong8@huawei-partners.com>

--- frameworks/ets/ani/ui_extension_ability/src/ets_ui_extension_context.cpp
+++ frameworks/ets/ani/ui_extension_ability/src/ets_ui_extension_context.cpp
@@ -19,7 +19,7 @@
 #include "ani_common_start_options.h"
 #include "ani_common_want.h"
 #include "ani_enum_convert.h"
-#include "ani_remote_object.h"
+#include "remote_object_taihe_ani.h"
 #include "common_fun_ani.h"
 #include "ets_context_utils.h"
 #include "ets_error_utils.h"
