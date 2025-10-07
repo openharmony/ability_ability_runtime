a2fd90fbf2c8389d3f82074465e46b5061cd50f5
RuiChen_01 <chenrui193@huawei.com>
2025-10-07 16:00:29 +0800

    add fuzz2
    Signed-off-by: RuiChen_01 <chenrui193@huawei.com>

---
+++ test/fuzztest/abilitymgrecologicalrulemgrserviceparamfirst_fuzzer/abilitymgrecologicalrulemgrserviceparamfirst_fuzzer.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "abilitymgrecologicalrulemgrserviceparamfirst_fuzzer.h"
+#include "ability_record.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <fuzzer/FuzzedDataProvider.h>
+
+#define private public
+#include "ecological_rule/ability_ecological_rule_mgr_service_param.h"
+#include "ecological_rule/ability_ecological_rule_mgr_service_interface.h"
+#undef private
+#include "securec.h"
+
+using namespace OHOS::AAFwk;
+using namespace OHOS::AppExecFwk;
+
+namespace OHOS {
+bool DoSomethingInterestingWithMyAPI(FuzzedDataProvider *fdp)
+{
+    EcologicalRuleMgrService::AbilityExperienceRule abilityExperienceRule;
+    Parcel parcel;
+    parcel.WriteString(fdp->ConsumeRandomLengthString());
+    abilityExperienceRule.Marshalling(parcel);
+    abilityExperienceRule.Unmarshalling(parcel);
+
+    EcologicalRuleMgrService::AbilityCallerInfo abilityCallerInfo;
+    abilityCallerInfo.ReadFromParcel(parcel);
+    abilityCallerInfo.Marshalling(parcel);
+    abilityCallerInfo.Unmarshalling(parcel);
+    return true;
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    FuzzedDataProvider fdp(data, size);
+    OHOS::DoSomethingInterestingWithMyAPI(&fdp);
+    return 0;
+}
+
