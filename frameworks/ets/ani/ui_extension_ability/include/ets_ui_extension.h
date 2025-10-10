683360d7bfecc7e8db70f6916c97a3eb171cf57a
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-10-10 19:33:22 +0800

    transfer windowstage and uicontentsession
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- frameworks/ets/ani/ui_extension_ability/include/ets_ui_extension.h
+++ frameworks/ets/ani/ui_extension_ability/include/ets_ui_extension.h
@@ -36,6 +36,10 @@ namespace OHOS {
 namespace AbilityRuntime {
 class UIExtension;
 class UIExtensionContext;
+struct ContentSessionType {
+    ani_ref etsContentSession = nullptr;
+    std::shared_ptr<NativeReference> jsContentSession = nullptr;
+};
 
 /**
  * @brief Basic ui extension components.
@@ -147,7 +151,8 @@ private:
     ani_status CallOnDisconnect(const AAFwk::Want &want, bool withResult = false);
     void DestroyWindow(const sptr<AAFwk::SessionInfo> &sessionInfo) override;
     bool ForegroundWindowInitInsightIntentExecutorInfo(const AAFwk::Want &want,
-        const sptr<AAFwk::SessionInfo> &sessionInfo, InsightIntentExecutorInfo &executorInfo);
+        const sptr<AAFwk::SessionInfo> &sessionInfo, InsightIntentExecutorInfo &executorInfo,
+        const std::string &arkTSMode);
     bool ForegroundWindowWithInsightIntent(const AAFwk::Want &want, const sptr<AAFwk::SessionInfo> &sessionInfo,
         bool needForeground) override;
     bool HandleSessionCreate(const AAFwk::Want &want, const sptr<AAFwk::SessionInfo> &sessionInfo) override;
@@ -165,7 +170,7 @@ private:
     std::shared_ptr<AppExecFwk::ETSNativeReference> etsObj_ = nullptr;
     std::shared_ptr<AppExecFwk::ETSNativeReference> shellContextRef_ = nullptr;
     std::mutex uiWindowMutex_;
-    std::map<uint64_t, ani_ref> contentSessions_;
+    std::map<uint64_t, ContentSessionType> contentSessions_;
     int32_t screenMode_ = AAFwk::IDLE_SCREEN_MODE;
     std::shared_ptr<int32_t> screenModePtr_;
     sptr<IRemoteObject> token_ = nullptr;
