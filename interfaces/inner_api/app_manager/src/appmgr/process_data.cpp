d7db8f9ab6c06473030d0fa2b2d64bd49b855ac2
jusiyao <jusiyao1@huawei.com>
2025-11-14 09:48:15 +0800

    add preload mode tag
    Signed-off-by: jusiyao <jusiyao1@huawei.com>

--- interfaces/inner_api/app_manager/src/appmgr/process_data.cpp
+++ interfaces/inner_api/app_manager/src/appmgr/process_data.cpp
@@ -32,7 +32,7 @@ bool ProcessData::Marshalling(Parcel &parcel) const
         && parcel.WriteInt32(renderUid) && parcel.WriteUint32(accessTokenId) &&
         parcel.WriteBool(isTestMode) && parcel.WriteInt32(exitReason) && parcel.WriteString16(Str8ToStr16(exitMsg)) &&
         parcel.WriteInt32(childUid) && parcel.WriteBool(isPreload)  && parcel.WriteBool(isPreloadModule) &&
-        parcel.WriteBool(isPrelaunch) && parcel.WriteInt32(callerPid) && parcel.WriteInt32(callerUid) &&
+        parcel.WriteInt32(callerPid) && parcel.WriteInt32(callerUid) &&
         parcel.WriteString(killReason) && parcel.WriteBool(isFromWindowFocusChanged) &&
         parcel.WriteInt32(preloadMode));
 }
@@ -61,7 +61,6 @@ bool ProcessData::ReadFromParcel(Parcel &parcel)
     childUid = parcel.ReadInt32();
     isPreload = parcel.ReadBool();
     isPreloadModule = parcel.ReadBool();
-    isPrelaunch = parcel.ReadBool();
     callerPid = parcel.ReadInt32();
     callerUid = parcel.ReadInt32();
     killReason = parcel.ReadString();
