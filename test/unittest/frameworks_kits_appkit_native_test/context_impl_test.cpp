d8c5a9b1367f6a31055190879e0735cc928033dc
wangzhen <wangzhen416@huawei.com>
2025-12-02 15:45:23 +0800

    Clean el1 and el2
    Signed-off-by: wangzhen <wangzhen416@huawei.com>

--- test/unittest/frameworks_kits_appkit_native_test/context_impl_test.cpp
+++ test/unittest/frameworks_kits_appkit_native_test/context_impl_test.cpp
@@ -558,6 +558,33 @@ HWTEST_F(ContextImplTest, GetBaseDir_0100, TestSize.Level1)
 }
 
 /**
+ * @tc.name: GetAllTempBase_0100
+ * @tc.desc: Switch temp bases test.
+ * @tc.type: FUNC
+ * @tc.require: issueI61P7Y
+ */
+HWTEST_F(ContextImplTest, GetAllTempBase_0100, TestSize.Level1)
+{
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s start.", __func__);
+    auto contextImpl = std::make_shared<AbilityRuntime::ContextImpl>();
+    EXPECT_NE(contextImpl, nullptr);
+    contextImpl->applicationInfo_ = nullptr;
+    std::vector<std::string> tempBases;
+    contextImpl->GetAllTempBase(tempBases);
+    EXPECT_TRUE(tempBases.empty());
+
+    contextImpl->applicationInfo_ = std::make_shared<AppExecFwk::ApplicationInfo>();
+    AppExecFwk::ModuleInfo moduleInfo;
+    moduleInfo.moduleName = "testModuleName";
+    contextImpl->applicationInfo_->moduleInfos.push_back(moduleInfo);
+    contextImpl->GetAllTempBase(tempBases);
+    constexpr int32_t SING_MODULE_TEMP_BASE_SIZE = 4;
+    EXPECT_EQ(tempBases.size(), SING_MODULE_TEMP_BASE_SIZE);
+
+    TAG_LOGI(AAFwkTag::TEST, "%{public}s end.", __func__);
+}
+
+/**
  * @tc.name: SwitchArea_0100
  * @tc.desc: Switch area basic test.
  * @tc.type: FUNC
