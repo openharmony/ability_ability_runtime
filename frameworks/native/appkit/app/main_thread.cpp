fd22407e0994b5e1bbf681a9412d3639e109c78a
StreamBuns <zhaotong42@huawei.com>
2025-12-02 14:15:59 +0800

    fix word
    Signed-off-by: StreamBuns <zhaotong42@huawei.com>

--- frameworks/native/appkit/app/main_thread.cpp
+++ frameworks/native/appkit/app/main_thread.cpp
@@ -4330,7 +4330,7 @@ void MainThread::SleepCleanKill()
             TAG_LOGE(AAFwkTag::APPKIT, "null appThread");
             return ;
         }
-        if (AbilityRuntime::TimeUtil::SystemTimeMillisecond() - beginTime < AppExecFwk::SLEEP_CLEAN_TIME_OUT &&
+        if (AbilityRuntime::TimeUtil::SystemTimeMillisecond() - beginTime < AppExecFwk::SLEEP_CLEAN_TIMEOUT &&
             appThread->applicationImpl_->GetState() != ApplicationImpl::APP_STATE_FOREGROUND) {
             AbilityManagerClient::GetInstance()->RecordAppExitReason({ REASON_RESOURCE_CONTROL,
                 "Js_Heap_Sleep_Clean_Kill" });
