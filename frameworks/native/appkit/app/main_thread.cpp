8038c848f3979bddf0996a685d36c15d28699304
StreamBuns <zhaotong42@huawei.com>
2025-12-02 09:38:15 +0800

    dix bug
    Signed-off-by: StreamBuns <zhaotong42@huawei.com>

--- frameworks/native/appkit/app/main_thread.cpp
+++ frameworks/native/appkit/app/main_thread.cpp
@@ -4329,8 +4329,11 @@ void MainThread::SleepCleanKill()
             TAG_LOGE(AAFwkTag::APPKIT, "null appThread");
             return ;
         }
-        AbilityManagerClient::GetInstance()->RecordAppExitReason({ REASON_SIGNAL, "Sleep_Clean_Kill" });
-        _exit(0);
+        if(appThread->applicationImpl_ &&
+            appThread->applicationImpl_->GetState() == ApplicationImpl::APP_STATE_BACKGROUND){
+            AbilityManagerClient::GetInstance()->RecordAppExitReason({ REASON_RESOURCE_CONTROL, "Js_Heap_Sleep_Clean_Kill" });
+            _exit(0);            
+        }
     };
     mainHandler_->PostTask(task, "Sleep Clean:Over HeapSize", AppExecFwk::SLEEP_CLEAN_DELAY_TIME);
 }
