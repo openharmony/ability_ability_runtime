8e3071831d31190a0e0e751973e658b74182dd53
hanchen45 <wanghanchen1@huawei.com>
2025-12-22 09:35:17 +0800

    openlink放开host校验
    Signed-off-by: hanchen45 <wanghanchen1@huawei.com>

--- frameworks/ets/ani/ui_extension_ability/src/ets_ui_extension_context.cpp
+++ frameworks/ets/ani/ui_extension_ability/src/ets_ui_extension_context.cpp
@@ -74,7 +74,7 @@ static bool CheckUrl(std::string &urlValue)
         return false;
     }
     Uri uri = Uri(urlValue);
-    if (uri.GetScheme().empty() || uri.GetHost().empty()) {
+    if (uri.GetScheme().empty()) {
         return false;
     }
     return true;
