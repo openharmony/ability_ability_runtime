b50c9d32fc91c03a9168158e24d34f91444316fa
zhangzezhong <zhangzezhong8@huawei-partners.com>
2026-01-04 11:19:15 +0800

    Revise Review Comments
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

--- test/mock/services_abilitymgr_test/libs/appexecfwk_core/src/appmgr/app_state_callback_proxy.cpp
+++ test/mock/services_abilitymgr_test/libs/appexecfwk_core/src/appmgr/app_state_callback_proxy.cpp
@@ -43,5 +43,9 @@ void AppStateCallbackProxy::NotifyStartResidentProcess(std::vector<AppExecFwk::B
 
 void AppStateCallbackProxy::NotifyStartKeepAliveProcess(std::vector<AppExecFwk::BundleInfo> &bundleInfos)
 {}
+
+void AppStateCallbackProxy::RecordAppExitSignalReason(int32_t pid, int32_t uid, int32_t signal,
+    std::string &bundleName)
+{}
 }  // namespace AppExecFwk
 }  // namespace OHOS
