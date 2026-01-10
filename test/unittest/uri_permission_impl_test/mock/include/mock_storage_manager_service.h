8ab99dcc2e29d0881ea5a8a9ae0afe86e443d1f7
dsz2025 <duansizhao@huawei.com>
2026-01-10 13:08:20 +0800

    适配dec
    Signed-off-by: dsz2025 <duansizhao@huawei.com>

--- test/unittest/uri_permission_impl_test/mock/include/mock_storage_manager_service.h
+++ test/unittest/uri_permission_impl_test/mock/include/mock_storage_manager_service.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -405,17 +405,17 @@ public:
         return E_OK;
     }
 
+    virtual int32_t UpdateUserPublicDirPolicy(uint32_t userId) override
+    {
+        return E_OK;
+    }
+
     virtual int32_t RegisterUeceActivationCallback(const sptr<IUeceActivationCallback> &ueceCallback) override
     {
         return E_OK;
     }
 
     virtual int32_t UnregisterUeceActivationCallback() override
-    {
-        return E_OK;
-    }
-
-    virtual int32_t UpdateUserPublicDirPolicy(uint32_t userId) override
     {
         return E_OK;
     }
@@ -435,6 +435,11 @@ public:
         return E_OK;
     }
 
+    virtual int32_t EraseAllUserEncryptedKeys() override
+    {
+        return E_OK;
+    }
+
     virtual int32_t SetExtBundleStats(uint32_t userId, const ExtBundleStats &stats) override
     {
         return E_OK;
@@ -446,11 +451,6 @@ public:
     }
 
     virtual int32_t GetAllExtBundleStats(uint32_t userId, std::vector<ExtBundleStats> &statsVec) override
-    {
-        return E_OK;
-    }
-
-    virtual int32_t EraseAllUserEncryptedKeys() override
     {
         return E_OK;
     }
