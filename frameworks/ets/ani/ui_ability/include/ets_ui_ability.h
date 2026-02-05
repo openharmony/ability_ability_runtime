f55de0fc1b83c2529b1ba8c16b0a7e1e25bf1e3c
zhengzhuolan <zhengzhuolan@huawei-partners.com>
2026-02-05 17:21:14 +0800

    add log lifecycle
    Signed-off-by: zhengzhuolan <zhengzhuolan@huawei-partners.com>

--- frameworks/ets/ani/ui_ability/include/ets_ui_ability.h
+++ frameworks/ets/ani/ui_ability/include/ets_ui_ability.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -351,6 +351,7 @@ private:
     void RemoveShareRouterByBundleType(const Want &want);
     bool CheckSatisfyTargetAPIVersion(int32_t targetAPIVersion);
     bool BackPressDefaultValue();
+    void WriteLifecycleSwitchLog(const std::string lifecycleName);
     int32_t CallSaveState(ani_value args[], WantParams &wantParams, AppExecFwk::StateReason stateReason,
         AppExecFwk::AbilityTransactionCallbackInfo<AppExecFwk::OnSaveStateResult> *callbackInfo);
 
