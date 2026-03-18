9d7480efb1840a93f783517e94b8981c8ab921d7
renjianhao <renjianhao@h-partners.com>
2026-03-18 09:52:16 +0800

    target uid
    Signed-off-by: renjianhao <renjianhao@h-partners.com>

--- services/abilitymgr/src/ability_record.cpp
+++ services/abilitymgr/src/ability_record.cpp
@@ -2750,6 +2750,7 @@ void AbilityRecord::GrantUriPermission(Want &want, std::string targetBundleName,
     grantInfo.userId = GetOwnerMissionUserId();
     grantInfo.flag = want.GetFlags();
     grantInfo.isNotifyCollaborator = isNotifyCollaborator;
+    grantInfo.targetUid = uid_;
     grantInfo.callerUid = want.GetIntParam(Want::PARAM_RESV_CALLER_UID, IPCSkeleton::GetCallingUid());
     if (!isNotifyCollaborator) {
         grantInfo.targetAbilityName = abilityInfo_.name;
