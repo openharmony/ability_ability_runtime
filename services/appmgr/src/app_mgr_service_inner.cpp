7ceec9e0dbbbdc4dbfebe8fcb77fd95114fed51f
jusiyao <jusiyao1@huawei.com>
2026-03-23 20:17:54 +0800

    clonenotpreload
    Signed-off-by: jusiyao <jusiyao1@huawei.com>

--- services/appmgr/src/app_mgr_service_inner.cpp
+++ services/appmgr/src/app_mgr_service_inner.cpp
@@ -610,7 +610,7 @@ int32_t AppMgrServiceInner::PreloadApplication(const std::string &bundleName, in
     }
     if (appIndex != 0) {
         TAG_LOGE(AAFwkTag::APPMGR, "not support clone app preload");
-        return ERR_PERMISSION_DENIED;
+        return ERR_INVALID_VALUE;
     }
     return PreloadApplication(bundleName, userId, appIndex, preloadMode, AppExecFwk::PreloadPhase::UNSPECIFIED);
 }
