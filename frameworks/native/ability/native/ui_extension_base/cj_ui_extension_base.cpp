4530a443fc3820a59c051462d3c585693dc61615
glb0 <gaolibin3@h-partners.com>
2026-03-31 16:30:30 +0800

    原能力适配车机多进程
    Signed-off-by: glb0 <gaolibin3@h-partners.com>

--- frameworks/native/ability/native/ui_extension_base/cj_ui_extension_base.cpp
+++ frameworks/native/ability/native/ui_extension_base/cj_ui_extension_base.cpp
@@ -348,7 +348,13 @@ bool CJUIExtensionBase::HandleSessionCreate(const AAFwk::Want &want, const sptr<
         {
             HITRACE_METER_NAME(HITRACE_TAG_APP, "Rosen::Window::Create");
             option->SetDisplayId(sessionInfo->displayId);
-            uiWindow = Rosen::Window::Create(option, context_, sessionInfo->sessionToken);
+            uiWindow = Rosen::Window::Create(option,
+                context_,
+                sessionInfo->sessionToken,
+            Rosen::DefaultCreateErrCode,
+            "",
+            false,
+            sessionInfo->renderSession);
         }
         if (uiWindow == nullptr) {
             TAG_LOGE(AAFwkTag::UI_EXT, "null uiWindow");
