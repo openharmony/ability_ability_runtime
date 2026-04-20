0a5f12ca5f20ee8957775885bde7ea058ae5c325
xhz-sz <xiehuandong@huawei.com>
2026-04-20 14:43:46 +0800

    support request modal with accountId
    Signed-off-by: xhz-sz <xiehuandong@huawei.com>
    Co-Authored-By: Agent

--- interfaces/inner_api/ability_manager/include/ability_manager_client.h
+++ interfaces/inner_api/ability_manager/include/ability_manager_client.h
@@ -417,6 +417,15 @@ public:
     ErrCode RequestModalUIExtension(const Want &want);
 
     /**
+     * Request modal UIExtension with account id.
+     *
+     * @param want, the want of the modal UIExtension to request.
+     * @param accountId, the account id for multi-user scenario.
+     * @return Returns ERR_OK on success, others on failure.
+     */
+    ErrCode RequestModalUIExtensionWithAccount(const Want &want, int32_t accountId);
+
+    /**
      * Preload UIExtension with want, send want to ability manager service.
      *
      * @param want, the want of the ability to start.
--- interfaces/kits/native/ability/native/ability_runtime/js_ability_context.h
+++ interfaces/kits/native/ability/native/ability_runtime/js_ability_context.h
@@ -67,6 +67,7 @@ public:
     static napi_value SetMissionContinueState(napi_env env, napi_callback_info info);
     static napi_value StartAbilityByType(napi_env env, napi_callback_info info);
     static napi_value RequestModalUIExtension(napi_env env, napi_callback_info info);
+    static napi_value RequestModalUIExtensionWithAccount(napi_env env, napi_callback_info info);
     static napi_value ShowAbility(napi_env env, napi_callback_info info);
     static napi_value HideAbility(napi_env env, napi_callback_info info);
     static napi_value MoveAbilityToBackground(napi_env env, napi_callback_info info);
@@ -142,6 +143,7 @@ private:
     napi_value OnSetMissionContinueState(napi_env env, NapiCallbackInfo& info);
     napi_value OnStartAbilityByType(napi_env env, NapiCallbackInfo& info);
     napi_value OnRequestModalUIExtension(napi_env env, NapiCallbackInfo& info);
+    napi_value OnRequestModalUIExtensionWithAccount(napi_env env, NapiCallbackInfo& info);
     napi_value OnShowAbility(napi_env env, NapiCallbackInfo& info);
     napi_value OnHideAbility(napi_env env, NapiCallbackInfo& info);
     napi_value ChangeAbilityVisibility(napi_env env, NapiCallbackInfo& info, bool isShow);
