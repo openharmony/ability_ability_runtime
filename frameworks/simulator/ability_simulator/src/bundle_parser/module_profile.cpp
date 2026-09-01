0452f81be88f2c6c91f4694c1f99ff0062de15b4
beauty-bag <huwei169@huawei.com>
2026-09-01 16:27:38 +0800

    UPDATE
    Signed-off-by: beauty-bag <huwei169@huawei.com>

--- frameworks/simulator/ability_simulator/src/bundle_parser/module_profile.cpp
+++ frameworks/simulator/ability_simulator/src/bundle_parser/module_profile.cpp
@@ -110,7 +110,7 @@ const std::map<std::string, uint32_t> BACKGROUND_MODES_MAP = {
     {ProfileReader::KEY_AV_PLAYBACK_AND_RECORD, ProfileReader::VALUE_AV_PLAYBACK_AND_RECORD},
     {ProfileReader::KEY_SPECIAL_SCENARIO_PROCESSING, ProfileReader::VALUE_SPECIAL_SCENARIO_PROCESSING},
     {ProfileReader::KEY_NEARLINK, ProfileReader::VALUE_NEARLINK},
-    {ProfileReader::KEY_USB, ProfileReader::VALUE_USB}
+    {ProfileReader::KEY_USB_CONNECTION, ProfileReader::VALUE_USB_CONNECTION}
 };
 
 const std::set<std::string> GRANT_MODE_SET = {
