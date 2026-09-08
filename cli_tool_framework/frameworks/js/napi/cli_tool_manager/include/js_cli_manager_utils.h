06d5e37b40c089836dd1799d3e4946a7b6af783f
yijobs <yisilong@h-partners.com>
2026-09-08 10:24:33 +0800

    update: 更新文件 js_cli_manager_utils.h
    Signed-off-by: yijobs <yisilong@h-partners.com>

--- cli_tool_framework/frameworks/js/napi/cli_tool_manager/include/js_cli_manager_utils.h
+++ cli_tool_framework/frameworks/js/napi/cli_tool_manager/include/js_cli_manager_utils.h
@@ -106,14 +106,6 @@ napi_value CreateJsToolInfo(napi_env env, const ToolInfo &tool);
  * @return Returns the JavaScript object.
  */
 napi_value CreateJsToolSummary(napi_env env, const ToolSummary &summary);
-
-/**
- * @brief Create JavaScript SessionStatus enum object.
- * @param env The N-API environment.
- * @return Returns the JavaScript object representing SessionStatus enum.
- */
-napi_value CreateJsSessionStatus(napi_env env);
-
 } // namespace CliTool
 } // namespace OHOS
 
