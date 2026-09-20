772ac83bceec8db79b7043573c24126cf9f70276
Leal <tanqingliu@h-partners.com>
2026-09-20 21:00:07 +0800

    update: 更新文件 ability_manager_service_thirteenth_test.cpp
    Signed-off-by: Leal <tanqingliu@h-partners.com>

--- test/unittest/ability_manager_service_thirteenth_test/ability_manager_service_thirteenth_test.cpp
+++ test/unittest/ability_manager_service_thirteenth_test/ability_manager_service_thirteenth_test.cpp
@@ -563,44 +563,6 @@ HWTEST_F(AbilityManagerServiceThirteenthTest, CheckCallerInfoQueryExtensionPermi
 
 /*
  * Feature: AbilityManagerService
- * Name: CheckUkeyAuthExtensionPermission_001
- * Function: CheckUkeyAuthExtensionPermission
- * SubFunction: NA
- * FunctionPoints: AbilityManagerService CheckUkeyAuthExtensionPermission
- */
-HWTEST_F(AbilityManagerServiceThirteenthTest, CheckUkeyAuthExtensionPermission_001, TestSize.Level1)
-{
-    TAG_LOGI(AAFwkTag::TEST, "AbilityManagerServiceThirteenthTest CheckUkeyAuthExtensionPermission_001 start");
-    auto abilityMs_ = std::make_shared<AbilityManagerService>();
-    EXPECT_NE(abilityMs_, nullptr);
-    MyStatus::GetInstance().permPermission_ = 0;
-    AbilityRequest abilityRequest;
-    int result = abilityMs_->CheckUkeyAuthExtensionPermission(abilityRequest);
-    EXPECT_EQ(result, CHECK_PERMISSION_FAILED);
-    TAG_LOGI(AAFwkTag::TEST, "AbilityManagerServiceThirteenthTest CheckUkeyAuthExtensionPermission_001 end");
-}
-
-/*
- * Feature: AbilityManagerService
- * Name: CheckUkeyAuthExtensionPermission_002
- * Function: CheckUkeyAuthExtensionPermission
- * SubFunction: NA
- * FunctionPoints: AbilityManagerService CheckUkeyAuthExtensionPermission
- */
-HWTEST_F(AbilityManagerServiceThirteenthTest, CheckUkeyAuthExtensionPermission_002, TestSize.Level1)
-{
-    TAG_LOGI(AAFwkTag::TEST, "AbilityManagerServiceThirteenthTest CheckUkeyAuthExtensionPermission_002 start");
-    auto abilityMs_ = std::make_shared<AbilityManagerService>();
-    EXPECT_NE(abilityMs_, nullptr);
-    MyStatus::GetInstance().permPermission_ = 1;
-    AbilityRequest abilityRequest;
-    int result = abilityMs_->CheckUkeyAuthExtensionPermission(abilityRequest);
-    EXPECT_EQ(result, ERR_OK);
-    TAG_LOGI(AAFwkTag::TEST, "AbilityManagerServiceThirteenthTest CheckUkeyAuthExtensionPermission_002 end");
-}
-
-/*
- * Feature: AbilityManagerService
  * Name: CheckCallOtherExtensionPermission_UkeyAuth_001
  * Function: CheckCallOtherExtensionPermission
  * SubFunction: NA
