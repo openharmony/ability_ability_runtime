428d4208d175a0abcb571b31a8c5e67c337a2a25
yijobs <yisilong@h-partners.com>
2026-09-21 21:29:06 +0800

    update: 更新文件 mock_start_ability_utils.cpp
    Signed-off-by: yijobs <yisilong@h-partners.com>

--- test/unittest/start_options_utils_test/mock/src/mock_start_ability_utils.cpp
+++ test/unittest/start_options_utils_test/mock/src/mock_start_ability_utils.cpp
@@ -20,7 +20,7 @@
 namespace OHOS {
 namespace AAFwk {
 
-bool StartAbilityUtils::GetAppIndex(const Want &want, sptr<IRemoteObject> callerToken callerToken,
+bool StartAbilityUtils::GetAppIndex(const Want &want, sptr<IRemoteObject> callerToken,
                                     int userId, int32_t &appIndex)
 {
     appIndex = 0;
