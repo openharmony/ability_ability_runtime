59791839d2feb1e5876df9fdc1019b5513b7c6a9
GroupM <maqun4@huawei.com>
2026-09-21 20:37:22 +0800

    update: 更新文件 js_runtime.cpp
    Signed-off-by: GroupM <maqun4@huawei.com>

--- frameworks/native/runtime/js_runtime.cpp
+++ frameworks/native/runtime/js_runtime.cpp
@@ -989,9 +989,9 @@ void JsRuntime::CreatePluginDefaultNamespace(const std::string &lddictionaries)
     if (result != Security::AccessToken::PermissionState::PERMISSION_GRANTED &&
         resultWeb != Security::AccessToken::PermissionState::PERMISSION_GRANTED &&
         resultLoadCertSignLib != Security::AccessToken::PermissionState::PERMISSION_GRANTED) {
-        TAG_LOGE(AAFwkTag::JSRUNTIME, "verify access token failed: result = %{public}d, 
-        resultWeb = %{public}d, resultLoadCertSignLib = %{public}d", 
-        result, resultWeb, resultLoadCertSignLib);
+        TAG_LOGE(AAFwkTag::JSRUNTIME, "verify access token failed: result = %{public}d,
+        resultWeb = %{public}d, resultLoadCertSignLib = %{public}d",
+        result, resultWeb, resultLoadCertSignLi);
         return;
     }
     auto moduleManager = NativeModuleManager::GetInstance();
