b74c14e2735a739a22128fc2066e34abaf7ad28b
MMMMCCCC <937296505@qq.com>
2026-09-22 22:14:18 +0800

    update: 更新文件 ui_ability_lifecycle_manager.cpp
    Signed-off-by: MMMMCCCC <937296505@qq.com>

--- services/abilitymgr/src/scene_board/ui_ability_lifecycle_manager.cpp
+++ services/abilitymgr/src/scene_board/ui_ability_lifecycle_manager.cpp
@@ -1903,9 +1903,9 @@ int UIAbilityLifecycleManager::CallAbilityLocked(const AbilityRequest &abilityRe
             }
             uiAbilityRecord->SetPendingState(AbilityState::FOREGROUND);
             ForegroundOptions options;
-            options.callerUid = abilityRequest->GetWant().GetIntParam(Want::PARAM_RESV_CALLER_UID, -1);
+            options.callerUid = abilityRequest.want.GetIntParam(Want::PARAM_RESV_CALLER_UID, -1);
             options.callerBundleName =
-                abilityRequest->GetWant().GetStringParam(Want::PARAM_RESV_CALLER_BUNDLE_NAME);
+                abilityRequest.want.GetStringParam(Want::PARAM_RESV_CALLER_BUNDLE_NAME);
             options.sceneFlag = SCENE_FLAG_BYCALL;
             uiAbilityRecord->ProcessForegroundAbility(sessionInfo->callingTokenId, options,
                 abilityRequest.want.GetBoolParam(ServerConstant::IS_CALL_BY_SCB, false));
